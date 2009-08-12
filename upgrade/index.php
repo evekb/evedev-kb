756692feb10ecee2bb1c61993900defe0a231b49
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2009-08-12 14:14:28 +0000

    Upgrade now clears template cache as well as sql cache by default.

--- upgrade/index.php
+++ upgrade/index.php
@@ -102,6 +102,7 @@ function updateDB()
 {
 // if update nesseary run updates
 	killCache();
+	removeOld(0,'cache/templates_c', false);
 	chdir('upgrade');
 	$dir = opendir('.');
 	$updatedirs = array();
@@ -176,3 +177,28 @@ function killCache()
 	}
 }
 
+function removeOld($hours, $dir, $recurse = false)
+{
+	if(!session::isAdmin()) return false;
+	if(strpos($dir, '.') !== false) return false;
+	//$dir = KB_CACHEDIR.'/'.$dir;
+	if(!is_dir($dir)) return false;
+	if(substr($dir,-1) != '/') $dir = $dir.'/';
+	$seconds = $hours*60*60;
+	$files = scandir($dir);
+
+	foreach ($files as $num => $fname)
+	{
+		if (file_exists("{$dir}{$fname}") && !is_dir("{$dir}{$fname}") && substr($fname,0,1) != "." && ((time() - filemtime("{$dir}{$fname}")) > $seconds))
+		{
+			$mod_time = filemtime("{$dir}{$fname}");
+			if (unlink("{$dir}{$fname}")) $del = $del + 1;
+		}
+		if ($recurse && file_exists("{$dir}{$fname}") && is_dir("{$dir}{$fname}")
+			 && substr($fname,0,1) != "." && $fname !== ".." )
+		{
+			$del = $del + admin_acache::remove_old($hours, $dir.$fname."/");
+		}
+	}
+	return $del;
+}
