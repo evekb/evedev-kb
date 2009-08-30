a989c2d6239918fcb2c5a582a06488815a780e13
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2009-08-30 11:32:43 +0000

    Improve handling of pilot and corp external ids. Name changes caught when updating external ID.

--- common/includes/class.corp.php
+++ common/includes/class.corp.php
@@ -5,37 +5,37 @@ require_once('class.pilot.php');
 //! Creates a new Corporation or fetches an existing one from the database.
 class Corporation
 {
-    //! Create a new Corporation object from the given $id.
-    
+//! Create a new Corporation object from the given $id.
+
     /*!
      * \param $id The corporation ID.
      */
-    function Corporation($id = 0)
-    {
-        $this->id_ = $id;
-        $this->executed_ = false;
-    }
-    //! Return whether this corporation is an NPC corporation.
-    function isNPCCorp()
-    {
-        global $corp_npc;
-        if (in_array($this->getName(), $corp_npc))
-            return true;
+	function Corporation($id = 0)
+	{
+		$this->id_ = $id;
+		$this->executed_ = false;
+	}
+	//! Return whether this corporation is an NPC corporation.
+	function isNPCCorp()
+	{
+		global $corp_npc;
+		if (in_array($this->getName(), $corp_npc))
+			return true;
 		if($this->externalid_ > 1000001 && $this->externalid_ < 1000183)
 			return true;
-    }
+	}
 
-    //! Return the corporation name stripped of all non-ASCII non-alphanumeric characters.
-    function getUnique()
-    {
+	//! Return the corporation name stripped of all non-ASCII non-alphanumeric characters.
+	function getUnique()
+	{
 		if(!$this->name_) $this->execQuery();
 		return preg_replace('/[^a-z0-9]/', '', strtolower($this->getName()));
-    }
-    //! Return a URL for the icon of this corporation.
-    function getPortraitURL($size = 64)
-    {
-        if ($this->isNPCCorp() || file_exists('img/corps/'.$this->getUnique().'.jpg'))
-        {
+	}
+	//! Return a URL for the icon of this corporation.
+	function getPortraitURL($size = 64)
+	{
+		if ($this->isNPCCorp() || file_exists('img/corps/'.$this->getUnique().'.jpg'))
+		{
 			if($size == 128)
 			{
 				if($this->externalid_ > 1000001 && $this->externalid_ < 1000183)
@@ -45,15 +45,15 @@ class Corporation
 			if($this->externalid_ > 1000001 && $this->externalid_ < 1000183)
 				return '?a=thumb&amp;type=corp&amp;id=c'.$this->externalid_.'&amp;size='.$size;
 			else return '?a=thumb&amp;type=corp&amp;id='.$this->getUnique().'&amp;size='.$size;
-        }
-        return '?a=thumb&amp;type=corp&amp;id='.$this->id_.'&amp;size='.$size;
-    }
+		}
+		return '?a=thumb&amp;type=corp&amp;id='.$this->id_.'&amp;size='.$size;
+	}
 
-    //! Return the corporation CCP ID.
+	//! Return the corporation CCP ID.
 	function getExternalID()
 	{
 		if($this->externalid_) return $this->externalid_;
-        $this->execQuery();
+		$this->execQuery();
 		if($this->externalid_) return $this->externalid_;
 
 		$corpname = str_replace(" ", "%20", $this->getName() );
@@ -68,70 +68,70 @@ class Corporation
 	}
 
 	//! Return the corporation ID.
-    function getID()
-    {
-        return $this->id_;
-    }
-    //! Return the corporation name.
-    function getName()
-    {
+	function getID()
+	{
+		return $this->id_;
+	}
+	//! Return the corporation name.
+	function getName()
+	{
 		if(!$this->name_) $this->execQuery();
-        return $this->name_;
-    }
+		return $this->name_;
+	}
 
-    //! Return an alliance object for the alliance this corporation belongs to.
-    function getAlliance()
-    {
+	//! Return an alliance object for the alliance this corporation belongs to.
+	function getAlliance()
+	{
 		if(!$this->alliance_) $this->execQuery();
-        return new Alliance($this->alliance_);
-    }
-    //! Lookup a corporation name and set this object to use the details found.
-    
+		return new Alliance($this->alliance_);
+	}
+	//! Lookup a corporation name and set this object to use the details found.
+
     /*!
      * \param $name The corporation name to look up.
      */
-    function lookup($name)
-    {
-        $qry = new DBQuery();
-        $qry->execute("select * from kb3_corps
+	function lookup($name)
+	{
+		$qry = new DBQuery();
+		$qry->execute("select * from kb3_corps
                        where crp_name = '".slashfix($name)."'");
-        $row = $qry->getRow();
-        if ($row['crp_id']) $this->id_ = $row['crp_id'];
+		$row = $qry->getRow();
+		if ($row['crp_id']) $this->id_ = $row['crp_id'];
 		$this->name_ = $row['crp_name'];
 		$this->externalid_ = intval($row['crp_external_id']);
 		$this->alliance_ = $row['crp_all_id'];
-    }
-    //! Search the database for the corporation details for this object.
-    function execQuery()
-    {
-        if (!$this->executed_)
-        {
-            $qry = new DBQuery();
-            $qry->execute("select * from kb3_corps
+	}
+	//! Search the database for the corporation details for this object.
+	function execQuery()
+	{
+		if (!$this->executed_)
+		{
+			$qry = new DBQuery();
+			$qry->execute("select * from kb3_corps
 	  	                   where crp_id = ".$this->id_);
-            $row = $qry->getRow();
+			$row = $qry->getRow();
 			$this->id_ = $row['crp_id'];
 			$this->name_ = $row['crp_name'];
 			$this->externalid_ = $row['crp_external_id'];
 			$this->alliance_ = $row['crp_all_id'];
-        }
-    }
-    //! Add a new corporation to the database or update the details of an existing one.
-    
+		}
+	}
+	//! Add a new corporation to the database or update the details of an existing one.
+
     /*!
      * \param $name The name of the new corporation.
      * \param $alliance The alliance this corporation belongs to.
      * \param $timestamp The timestamp the corporation's details were updated.
      * \param $externalid The external CCP ID for the alliance.
      */
-    function add($name, $alliance, $timestamp, $externalid = 0)
-    {
-        $qry = new DBQuery();
-        $qry->execute("select * from kb3_corps
+	function add($name, $alliance, $timestamp, $externalid = 0)
+	{
+		$qry = new DBQuery();
+		$qry->execute("select * from kb3_corps
 		               where crp_name = '".slashfix($name)."'");
 		// If the corp name is not present in the db add it.
-        if ($qry->recordCount() == 0)
-        {
+		if ($qry->recordCount() == 0)
+		{
 			$externalid = intval($externalid);
 			// If no external id is given then look it up.
 			if(!$externalid)
@@ -177,26 +177,26 @@ class Corporation
 			}
 			// Neither corp name or external id was found so add this corp as new
 			if($externalid) $qry->execute("insert into kb3_corps ".
-				"(crp_id, crp_name, crp_all_id, crp_external_id, crp_updated) ".
-				"values ( null, '".slashfix($name)."',".$alliance->getID().
-				", ".$externalid.", date_format('".$timestamp.
-				"','%Y.%m.%d %H:%i:%s'))");
-            else $qry->execute("insert into kb3_corps ".
-				"(crp_id, crp_name, crp_all_id, crp_updated) ".
-				"values ( null,'".slashfix($name)."',".$alliance->getID().
-				",date_format('".$timestamp."','%Y.%m.%d %H:%i:%s'))");
-            $this->id_ = $qry->getInsertID();
-        }
-        else
-        {
-            $row = $qry->getRow();
-            $this->id_ = $row['crp_id'];
+					"(crp_name, crp_all_id, crp_external_id, crp_updated) ".
+					"values ('".slashfix($name)."',".$alliance->getID().
+					", ".$externalid.", date_format('".$timestamp.
+					"','%Y.%m.%d %H:%i:%s'))");
+			else $qry->execute("insert into kb3_corps ".
+					"(crp_name, crp_all_id, crp_updated) ".
+					"values ('".slashfix($name)."',".$alliance->getID().
+					",date_format('".$timestamp."','%Y.%m.%d %H:%i:%s'))");
+			$this->id_ = $qry->getInsertID();
+		}
+		else
+		{
+			$row = $qry->getRow();
+			$this->id_ = $row['crp_id'];
 			$this->name = $row['crp_name'];
 			$this->externalid_ = $row['crp_external_id'];
 			$this->alliance_ = $row['crp_all_id'];
 
-            if ($row['crp_all_id'] != $alliance->getID() && $this->isUpdatable($timestamp))
-            {
+			if ($row['crp_all_id'] != $alliance->getID() && $this->isUpdatable($timestamp))
+			{
 				$sql = 'update kb3_corps
 	                           set crp_all_id = '.$alliance->getID().', ';
 				if(intval(externalid))
@@ -204,39 +204,61 @@ class Corporation
 				$sql .= "crp_updated = date_format( '".
 					$timestamp."','%Y.%m.%d %H:%i:%s') ".
 					"where crp_id = ".$this->id_;
-                $qry->execute($sql);
+				$qry->execute($sql);
 				$this->alliance_ = $alliance;
-            }
-        }
+			}
+		}
 
-        return $this->id_;
-    }
-    //! Return whether this corporation was updated before the given timestamp.
-    
+		return $this->id_;
+	}
+	//! Return whether this corporation was updated before the given timestamp.
+
     /*!
      * \param $timestamp A timestamp to compare this corporation's details with.
      */
-    function isUpdatable($timestamp)
-    {
+	function isUpdatable($timestamp)
+	{
 		if(isset($this->updated_))
 			if(is_null($this->updated_) || strtotime($timestamp." UTC") > $this->updated_) return true;
 			else return false;
-        $qry = new DBQuery();
-        $qry->execute("select crp_id from kb3_corps
+		$qry = new DBQuery();
+		$qry->execute("select crp_id from kb3_corps
 		               where crp_id = ".$this->id_."
 		               and ( crp_updated < date_format( '".$timestamp."', '%Y.%m.%d %H:%i' )
 			           or crp_updated is null )");
-        return $qry->recordCount() == 1;
-    }
+		return $qry->recordCount() == 1;
+	}
 
 	//! Set the CCP external ID for this corporation.
+
+	//! \param $externalid The new external id to set for this corp.
+	//! If the same externalid already exists then that corp name is changed to
+	//! the new one.
 	function setExternalID($externalid)
 	{
 		$externalid = intval($externalid);
 		if($externalid && $this->id_)
 		{
 			$this->execQuery();
-			$qry = new DBQuery();
+			$qry = new DBQuery(true);
+			$qry->execute("SELECT crp_id FROM kb3_corps WHERE crp_external_id = ".$externalid." AND crp_id <> ".$this->id_);
+			if($qry->recordCount())
+			{
+				$result = $qry->getRow();
+				$old_id = $result['crp_id'];
+				$qry->autocommit(false);
+				$qry->execute("UPDATE kb3_pilots SET crp_id = ".$old_id." WHERE crp_id = ".$this->id_);
+				$qry->execute("UPDATE kb3_kills SET kll_crp_id = ".$old_id." WHERE kll_crp_id = ".$this->id_);
+				$qry->execute("UPDATE kb3_inv_detail SET ind_crp_id = ".$old_id." WHERE ind_crp_id = ".$this->id_);
+				$qry->execute("UPDATE kb3_inv_crp SET inc_crp_id = ".$old_id." WHERE inc_crp_id = ".$this->id_);
+				$qry->execute("UPDATE kb3_corps SET crp_name = '".$this->name_."' where crp_id = ".$old_id);
+				$qry->execute("DELETE FROM kb3_corps WHERE crp_id = ".$this->id_);
+				$qry->execute("DELETE FROM kb3_sum_corp WHERE csm_crp_id = ".$this->id_);
+				$qry->execute("DELETE FROM kb3_sum_corp WHERE csm_crp_id = ".$old_id);
+				$qry->autocommit(true);
+				$this->id_ = $old_id;
+				return true;
+			}
 			if($qry->execute("UPDATE kb3_corps SET crp_external_id = ".$externalid." where crp_id = ".$this->id_))
 			{
 				$this->externalid_ = $externalid;
@@ -245,34 +267,34 @@ class Corporation
 		}
 		return false;
 	}
-	
+
 	//! Returns an array of pilots we know to be in this corp.
 	function getMemberList()
 	{
-        $qry = new DBQuery();
-        $qry->execute("SELECT plt_id FROM kb3_pilots
+		$qry = new DBQuery();
+		$qry->execute("SELECT plt_id FROM kb3_pilots
                        WHERE plt_crp_id = " . $this->id_);
 
-        if ($qry->recordCount() < 1)
-            return null;
-        else
-        {
-            $list = array();
-            while ($row = $qry->getRow())
-            {
-                $pilot = new Pilot($row['plt_id']);
-                $list[] = $pilot;
-            }
-        }
-        return $list;
+		if ($qry->recordCount() < 1)
+			return null;
+		else
+		{
+			$list = array();
+			while ($row = $qry->getRow())
+			{
+				$pilot = new Pilot($row['plt_id']);
+				$list[] = $pilot;
+			}
+		}
+		return $list;
 	}
 }
 
 class Corporations
 {
-	//! Add an array of pilots to be checked.
+//! Add an array of pilots to be checked.
 
-	//! \param $names array of corp names indexed by pilot name.
+//! \param $names array of corp names indexed by pilot name.
 	function addNames($names)
 	{
 		$qry = new DBQuery(true);
@@ -305,9 +327,9 @@ class Corporations
 			if(isset($names[slashfix($name['name'])]) && $name['characterID'])
 			{
 				$newcorp->add(slashfix($name['name']), $names[slashfix($name['name'])], '0000-00-00', $name['characterID']);
-				// Adding all at once is faster but skips checks for name/id clashes.
-				//if($sql == '') $sql = "INSERT INTO kb3_corps (crp_name, crp_all_id, crp_updated, crp_external_id) VALUES ('".slashfix($name['name'])."', ".$names[slashfix($name['name'])]->getID().", '0000-00-00', ".$name['characterID'].")";
-				//else $sql .= ",\n ('".slashfix($name['name'])."', ".$names[slashfix($name['name'])]->getID().", '0000-00-00', ".$name['characterID'].")";
+			// Adding all at once is faster but skips checks for name/id clashes.
+			//if($sql == '') $sql = "INSERT INTO kb3_corps (crp_name, crp_all_id, crp_updated, crp_external_id) VALUES ('".slashfix($name['name'])."', ".$names[slashfix($name['name'])]->getID().", '0000-00-00', ".$name['characterID'].")";
+			//else $sql .= ",\n ('".slashfix($name['name'])."', ".$names[slashfix($name['name'])]->getID().", '0000-00-00', ".$name['characterID'].")";
 			}
 		}
 		if($sql) $qry->execute($sql);
--- common/includes/class.pilot.php
+++ common/includes/class.pilot.php
@@ -6,31 +6,31 @@ require_once('class.thumb.php');
 //! Creates a new Pilot or fetches an existing one from the database.
 class Pilot
 {
-    //! Create a new Pilot object from the given $id.
+//! Create a new Pilot object from the given $id.
 
     /*!
      * \param $id The pilot ID.
 	 * \param $externalIDFlag whether the id is external or internal
      */
-    function Pilot($id = 0, $externalIDFlag = false)
-    {
-        if($externalIDFlag) $this->externalid_=intval($id);
-        else $this->id_ = intval($id);
-    }
+	function Pilot($id = 0, $externalIDFlag = false)
+	{
+		if($externalIDFlag) $this->externalid_=intval($id);
+		else $this->id_ = intval($id);
+	}
 	//! Return the alliance ID.
-    function getID()
-    {
-            if($this->id_) return $this->id_;
-            elseif($this->externalid_)
-            {
-                    $this->execQuery();
-                    return $this->id_;
-            }
-            else return 0;
-    }
+	function getID()
+	{
+		if($this->id_) return $this->id_;
+		elseif($this->externalid_)
+		{
+			$this->execQuery();
+			return $this->id_;
+		}
+		else return 0;
+	}
 	//! Return the pilot's CCP ID.
-    function getExternalID()
-    {
+	function getExternalID()
+	{
 		if($this->externalid_) return $this->externalid_;
 		elseif($this->id_)
 		{
@@ -43,96 +43,96 @@ class Pilot
 			return $this->externalid_;
 		}
 		else return 0;
-    }
-    //! Return the pilot name.
-    function getName()
-    {
-        if(!$this->name_) $this->execQuery();
-        $pos = strpos($this->name_, "#");
-        if ($pos === false)
-        {
-            return $this->name_;
-        }
-        else
-        {
-            $name = explode("#", $this->name_);
-            $item = new Item($name[2]);
-            return $item->getName();
-        }
-    }
-    //! Return the URL for the pilot's portrait.
+	}
+	//! Return the pilot name.
+	function getName()
+	{
+		if(!$this->name_) $this->execQuery();
+		$pos = strpos($this->name_, "#");
+		if ($pos === false)
+		{
+			return $this->name_;
+		}
+		else
+		{
+			$name = explode("#", $this->name_);
+			$item = new Item($name[2]);
+			return $item->getName();
+		}
+	}
+	//! Return the URL for the pilot's portrait.
 
     /*!
      * \param $size The desired portrait size.
 	 * \return URL for a portrait.
      */
-    function getPortraitURL($size = 64)
-    {
+	function getPortraitURL($size = 64)
+	{
 		if(!$this->externalid_) $this->execQuery();
-        if (!$this->externalid_)
-        {
-        	return '?a=thumb&amp;id='.$this->id_.'&amp;size='.$size.'&amp;int=1';
-        }
-        else
-        {
+		if (!$this->externalid_)
+		{
+			return '?a=thumb&amp;id='.$this->id_.'&amp;size='.$size.'&amp;int=1';
+		}
+		else
+		{
 			if( file_exists('cache/portraits/'.$this->externalid_.'_'.$size.'.jpg'))
 				return 'cache/portraits/'.$this->externalid_.'_'.$size.'.jpg';
 			else return '?a=thumb&amp;id='.$this->externalid_.'&amp;size='.$size;
-        }
-    }
-    //! Fetch the pilot details from the database using the id given on construction.
-    function execQuery()
-    {
-        if (!$this->qry_)
-        {
+		}
+	}
+	//! Fetch the pilot details from the database using the id given on construction.
+	function execQuery()
+	{
+		if (!$this->qry_)
+		{
 			if(!$this->externalid_ && !$this->id_)
 			{
-					$this->valid_ = false;
-					return;
+				$this->valid_ = false;
+				return;
 			}
-            $this->qry_ = new DBQuery();
-            $this->sql_ = 'select * from kb3_pilots plt, kb3_corps crp, kb3_alliances ali
+			$this->qry_ = new DBQuery();
+			$this->sql_ = 'select * from kb3_pilots plt, kb3_corps crp, kb3_alliances ali
             	  	       where crp.crp_id = plt.plt_crp_id
             		       and ali.all_id = crp.crp_all_id ';
-            if($this->externalid_) $this->sql_ .= 'and plt.plt_externalid = '.$this->externalid_;
-            else $this->sql_ .= 'and plt.plt_id = '.$this->id_;
-            $this->qry_->execute($this->sql_) or die($this->qry_->getErrorMsg());
-            //$this->row_ = $this->qry_->getRow();
-            $row = $this->qry_->getRow();
-            if (!$row)
-                $this->valid_ = false;
-            else
-            {
-                $this->valid_ = true;
-                $this->id_ = $row['plt_id'];
-                $this->name_ = $row['plt_name'];
-                $this->corp_ = $row['plt_crp_id'];
-                $this->externalid_ = intval($row['plt_externalid']);
+			if($this->externalid_) $this->sql_ .= 'and plt.plt_externalid = '.$this->externalid_;
+			else $this->sql_ .= 'and plt.plt_id = '.$this->id_;
+			$this->qry_->execute($this->sql_) or die($this->qry_->getErrorMsg());
+			//$this->row_ = $this->qry_->getRow();
+			$row = $this->qry_->getRow();
+			if (!$row)
+				$this->valid_ = false;
+			else
+			{
+				$this->valid_ = true;
+				$this->id_ = $row['plt_id'];
+				$this->name_ = $row['plt_name'];
+				$this->corp_ = $row['plt_crp_id'];
+				$this->externalid_ = intval($row['plt_externalid']);
 
-            }
-        }
-    }
-    //! Return the corporation this pilot is a member of.
+			}
+		}
+	}
+	//! Return the corporation this pilot is a member of.
 
     /*!
 	 * \return Corporation object
      */
-    function getCorp()
-    {
-        if(!$this->corp_) $this->execQuery();
-        return new Corporation($this->corp_);
-    }
+	function getCorp()
+	{
+		if(!$this->corp_) $this->execQuery();
+		return new Corporation($this->corp_);
+	}
 	//! Check if the id given on construction is valid.
 
     /*!
 	 * \return boolean - true for exists.
      */
-    function exists()
-    {
-        $this->execQuery();
-        return $this->valid_;
-    }
-    //! Add a new pilot to the database or update the details of an existing one.
+	function exists()
+	{
+		$this->execQuery();
+		return $this->valid_;
+	}
+	//! Add a new pilot to the database or update the details of an existing one.
 
     /*!
      * \param $name Pilot name
@@ -140,18 +140,18 @@ class Pilot
 	 * \param $timestamp time this pilot's corp was updated
 	 * \param $externalID CCP external id
      */
-    function add($name, $corp, $timestamp, $externalID = 0)
-    {
-		// Check if pilot exists with a non-cached query.
-        $qry = new DBQuery(true);
+	function add($name, $corp, $timestamp, $externalID = 0)
+	{
+	// Check if pilot exists with a non-cached query.
+		$qry = new DBQuery(true);
 		// Insert or update a pilot with a cached query to update cache.
-		$qryI = new DBQuery();
-        $qry->execute("select *
+		$qryI = new DBQuery(true);
+		$qry->execute("select *
                         from kb3_pilots
                        where plt_name = '".slashfix($name)."'");
 
-        if ($qry->recordCount() == 0)
-        {
+		if ($qry->recordCount() == 0)
+		{
 			$externalID = intval($externalID);
 			// If no external id is given then look it up.
 			if(!$externalID)
@@ -190,73 +190,92 @@ class Pilot
 					return $this->id_;
 				}
 			}
-            $qryI->execute("insert into kb3_pilots (plt_id, plt_name, plt_crp_id, plt_externalid, plt_updated) values ( null,
+			$qryI->execute("insert into kb3_pilots (plt_name, plt_crp_id, plt_externalid, plt_updated) values (
                                                         '".slashfix($name)."',
                                                         ".$corp->getID().",
                                                         ".$externalID.",
                                                         date_format( '".$timestamp."', '%Y.%m.%d %H:%i:%s'))");
-            $this->id_ = $qry->getInsertID();
-        }
-        else
-        {
-            $row = $qry->getRow();
-            $this->id_ = $row['plt_id'];
+			$this->id_ = $qry->getInsertID();
+		}
+		else
+		{
+			$row = $qry->getRow();
+			$this->id_ = $row['plt_id'];
 			$this->updated_ = strtotime($row['plt_updated']." UTC");
 			if(!$this->updated_) $this->updated_ = 0;
 			if ($this->isUpdatable($timestamp) && $row['plt_crp_id'] != $corp->getID())
-            {
-                $qryI->execute("update kb3_pilots
+			{
+				$qryI->execute("update kb3_pilots
                              set plt_crp_id = ".$corp->getID().",
                                  plt_updated = date_format( '".$timestamp."', '%Y.%m.%d %H:%i:%s') where plt_id = ".$this->id_);
-            }
-            if (!$row['plt_externalid'] && $externalID) $this->setCharacterID($externalID);
-        }
+			}
+			if (!$row['plt_externalid'] && $externalID) $this->setCharacterID($externalID);
+		}
 
-        return $this->id_;
-    }
-    //! Return whether this pilot was updated before the given timestamp.
+		return $this->id_;
+	}
+	//! Return whether this pilot was updated before the given timestamp.
 
     /*!
      * \param $timestamp A timestamp to compare this pilot's details with.
 	 * \return boolean - true if update time was before the given timestamp.
      */
-    function isUpdatable($timestamp)
-    {
+	function isUpdatable($timestamp)
+	{
 		if(isset($this->updated_))
 			if(is_null($this->updated_) || strtotime($timestamp." UTC") > $this->updated_) return true;
 			else return false;
-        $qry = new DBQuery();
-        $qry->execute("select plt_id
+		$qry = new DBQuery();
+		$qry->execute("select plt_id
                         from kb3_pilots
                        where plt_id = ".$this->id_."
                          and ( plt_updated < date_format( '".$timestamp."', '%Y.%m.%d %H:%i')
                                or plt_updated is null )");
 
-        return $qry->recordCount() == 1;
-    }
+		return $qry->recordCount() == 1;
+	}
 	//! Set the CCP external ID for this pilot.
 
     /*!
      * \param $externalID CCP external ID for this pilot.
+	 * If a character already exists with this id then a name change is assumed
+	 * and the old pilot is updated.
      */
-    function setCharacterID($externalID)
-    {
-        if (!intval($externalID))
-        {
-            return false;
-        }
-        $this->externalid_ = intval($externalID);
-        $qry = new DBQuery();
-        $qry->execute("update kb3_pilots set plt_externalid = ".$this->externalid_."
+	function setCharacterID($externalID)
+	{
+		if (!intval($externalID))
+		{
+			return false;
+		}
+		$this->externalid_ = intval($externalID);
+		$qry = new DBQuery(true);
+		$qry->execute("SELECT plt_id FROM kb3_pilots WHERE plt_external_id = ".$externalid." AND plt_id <> ".$this->id_);
+		if($qry->recordCount())
+		{
+			$result = $qry->getRow();
+			$qry->autocommit(false);
+			$old_id = $result['crp_id'];
+			$qry->execute("UPDATE kb3_pilots SET plt_name = '.$this->name_.' where plt_id = ".$old_id);
+			$qry->execute("UPDATE kb3_kills SET kll_victim_id = ".$old_id." WHERE kll_victim_id = ".$this->id_);
+			$qry->execute("UPDATE kb3_kills SET kll_fb_plt_id = ".$old_id." WHERE kll_fb_plt_id = ".$this->id_);
+			$qry->execute("UPDATE kb3_inv_detail SET ind_plt_id = ".$old_id." WHERE ind_plt_id = ".$this->id_);
+			$qry->execute("DELETE FROM kb3_pilots WHERE kll_victim_id = ".$this->id_);
+			$qry->execute("DELETE FROM kb3_sum_pilot WHERE psm_plt_id = ".$this->id_);
+			$qry->execute("DELETE FROM kb3_sum_pilot WHERE psm_plt_id = ".$old_id);
+			$this->id_ = $old_id;
+			$qry->autocommit(true);
+			return true;
+		}
+		$qry->execute("update kb3_pilots set plt_externalid = ".$this->externalid_."
                        where plt_id = ".$this->id_);
-    }
+	}
 }
 
 class Pilots
 {
-	//! Add an array of pilots to be checked.
+//! Add an array of pilots to be checked.
 
-	//! \param $names array of corp names indexed by pilot name.
+//! \param $names array of corp names indexed by pilot name.
 	function addNames($names)
 	{
 		$qry = new DBQuery(true);
@@ -285,15 +304,15 @@ class Pilots
 		}
 		$newpilot = new Pilot();
 		//$sql = '';
-		if(!is_array($myNames)) {echo $myNames;die("name fetch error");}
+		if(!is_array($myNames)) die("Name fetch error : ".$myNames);
 		foreach($myNames as $name)
 		{
 			if(isset($names[slashfix($name['name'])]))
 			{
 				$newpilot->add(slashfix($name['name']), $names[slashfix($name['name'])], '0000-00-00', $name['characterID']);
-				// Adding all at once is faster but skips checks for name/id clashes.
-				//if($sql == '') $sql = "INSERT INTO kb3_pilots (plt_name, plt_crp_id, plt_externalid, plt_updated) values ('".slashfix($name['name'])."', ".$names[slashfix($name['name'])]->getID().', '.$name['characterID'].", '0000-00-00')";
-				//else $sql .= ", ('".slashfix($name['name'])."', ".$names[slashfix($name['name'])]->getID().', '.$name['characterID'].", '0000-00-00')";
+			// Adding all at once is faster but skips checks for name/id clashes.
+			//if($sql == '') $sql = "INSERT INTO kb3_pilots (plt_name, plt_crp_id, plt_externalid, plt_updated) values ('".slashfix($name['name'])."', ".$names[slashfix($name['name'])]->getID().', '.$name['characterID'].", '0000-00-00')";
+			//else $sql .= ", ('".slashfix($name['name'])."', ".$names[slashfix($name['name'])]->getID().', '.$name['characterID'].", '0000-00-00')";
 			}
 		}
 		if($sql) $qry->execute($sql);
