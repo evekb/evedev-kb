74e4797f0a90293994b753ecc378049dc5ff8ce5
mastergamer0168 <mastergamer0168@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2009-09-10 19:56:14 +0000

    System Detail now uses pageAssembly. Code has been moved from templates to PHP thus the tpl is no longer needed.

--- common/system_detail.php
+++ common/system_detail.php
@@ -3,78 +3,102 @@ require_once('common/includes/class.system.php');
 require_once('common/includes/class.killlist.php');
 require_once('common/includes/class.killlisttable.php');
 require_once('common/includes/class.killsummarytable.php');
+require_once('common/includes/class.pageAssembly.php');
 
-$sys_id = intval($_GET['sys_id']);
-
-if (!$sys_id)
+class pSystemDetail extends pageAssembly
 {
-    echo 'no valid id supplied<br/>';
-    exit;
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->sys_id = intval($_GET['sys_id']);
+		global $smarty;
+		$this->smarty = $smarty;
+		
+		$this->queue("start");
+		$this->queue("map");
+		$this->queue("killList");
+	}
+	
+	function start()
+	{
+		if (!$this->sys_id)
+		{
+			echo 'no valid id supplied<br/>';
+			exit;
+		}
+		$this->system = new SolarSystem($this->sys_id);
+		$this->page = new Page('System details - '.$this->system->getName());
+		$this->smarty->assign('sys_id',$this->sys_id);
+	}
+	
+	function map()
+	{
+		return $this->smarty->fetch(get_tpl("system_detail_map"));
+	}
+	
+	function killList()
+	{
+		$klist = new KillList();
+		$klist->setOrdered(true);
+		if ($_GET['view'] == 'losses')
+			involved::load($klist,'loss');
+		else
+			involved::load($klist,'kill');
+		$klist->addSystem($this->system);
+		if ($_GET['scl_id'])
+			$klist->addVictimShipClass(intval($_GET['scl_id']));
+		else
+			$klist->setPodsNoobShips(false);
+		$klist->setLimit(20);
+
+		if ($_GET['view'] == 'recent' || !isset($_GET['view']))
+			$html .= "<div class='kb-kills-header'>20 most recent kills</div>";
+		elseif ($_GET['view'] == 'losses')
+			$html .= "<div class='kb-kills-header'>All losses</div>";
+		else
+			$html .= "<div class='kb-kills-header'>All kills</div>";
+
+		$this->pagesplitter = new PageSplitter($klist->getCount(), config::get('killcount'));
+
+		$table = new KillListTable($klist);
+		$html .= $table->generate();
+		if (is_object($this->pagesplitter))
+		{
+			$html .= $this->pagesplitter->generate();
+		}
+		
+		return $html;
+	}
+	
+	function context()
+	{
+		parent::__construct();
+		$this->queue("box");
+	}
+	
+	function box()
+	{
+		$menubox = new box("Menu");
+		$menubox->setIcon("menu-item.gif");
+		$menubox->addOption("caption","Navigation");
+		$menubox->addOption("link","All kills", "?a=system_detail&amp;sys_id=".$this->sys_id."&amp;view=kills");
+		$menubox->addOption("link","All losses", "?a=system_detail&amp;sys_id=".$this->sys_id."&amp;view=losses");
+		$menubox->addOption("link","Recent Activity", "?a=system_detail&amp;sys_id=".$this->sys_id."&amp;view=recent");
+		return $menubox->generate();
+	}
+	
 }
-$system = new SolarSystem($sys_id);
 
-$page = new Page('System details - '.$system->getName());
+$systemDetail = new pSystemDetail();
+event::call("systemdetail_assembling", $systemDetail);
+$html = $systemDetail->assemble();
+$systemDetail->page->setContent($html);
 
-$smarty->assign('sys_id',$sys_id);
+$systemDetail->context();
+event::call("systemdetail_context_assembling", $systemDetail);
+$context = $systemDetail->assemble();
+$systemDetail->page->addContext($context);
 
-$kslist = new KillList();
-involved::load($kslist,'kill');
-$kslist->addSystem($system);
-if(config::get('kill_classified')) $kslist->setEndDate(gmdate('Y-m-d H:i',strtotime('now - '.(config::get('kill_classified')*3600).' hours')));
-
-$lslist = new KillList();
-involved::load($lslist,'loss');
-$lslist->addSystem($system);
-if(config::get('kill_classified')) $lslist->setEndDate(gmdate('Y-m-d H:i',strtotime('now - '.(config::get('kill_classified')*3600).' hours')));
-
-$summarytable = new KillSummaryTable($kslist, $lslist);
-$html .= $summarytable->generate();
-
-$klist = new KillList();
-$klist->setOrdered(true);
-if ($_GET['view'] == 'losses') involved::load($klist,'loss');
-else involved::load($klist,'kill');
-$klist->addSystem($system);
-if ($_GET['scl_id']) $klist->addVictimShipClass(intval($_GET['scl_id']));
-else $klist->setPodsNoobShips(false);
-
-if ($_GET['view'] == 'recent' || !isset($_GET['view']))
-{
-	$smarty->assign('sys_view', 'recentkills');
-//    $html .= "<div class=kb-kills-header>20 most recent kills</div>";
-    $klist->setLimit(20);
-}
-else
-{
-    if ($_GET['view'] == 'losses')
-    {
-		$smarty->assign('sys_view', 'losses');
-        //$html .= "<div class=kb-kills-header>All losses</div>";
-    }
-    else
-    {
-		$smarty->assign('sys_view', 'kills');
-        //$html .= "<div class=kb-kills-header>All kills</div>";
-    }
-	$klist->setPageSplitter(config::get('killcount'));
-	$pagesplitter = new PageSplitter($klist->getCount(), config::get('killcount'));
-}
-
-$table = new KillListTable($klist);
-$smarty->assign('sys_killlist', $table->generate());
-if (is_object($pagesplitter))
-{
-    $smarty->assign('sys_splitter', $pagesplitter->generate());
-}
-
-$page->setContent($smarty->fetch(get_tpl(system_detail)));
-$menubox = new box("Menu");
-$menubox->setIcon("menu-item.gif");
-$menubox->addOption("caption","Navigation");
-$menubox->addOption("link","All kills", "?a=system_detail&amp;sys_id=".$sys_id."&amp;view=kills");
-$menubox->addOption("link","All losses", "?a=system_detail&amp;sys_id=".$sys_id."&amp;view=losses");
-$menubox->addOption("link","Recent Activity", "?a=system_detail&amp;sys_id=".$sys_id."&amp;view=recent");
-$page->addContext($menubox->generate());
-
-$page->generate();
+$systemDetail->page->generate();
 ?>
--- themes/default/templates/system_detail_map.tpl
+++ themes/default/templates/system_detail_map.tpl
@@ -3,9 +3,3 @@
 <td><img src="?a=mapview&amp;sys_id={$sys_id}&amp;mode=region&amp;size=250" border="0" width="250" height="250"></td>
 <td><img src="?a=mapview&amp;sys_id={$sys_id}&amp;mode=cons&amp;size=250" border="0" width="250" height="250"></td>
 </tr></table><br/>
-{if $sys_view == 'recentkills'}<div class=kb-kills-header>20 most recent kills</div>
-{elseif $sys_view == 'losses'}<div class=kb-kills-header>All losses</div>
-{else}<div class=kb-kills-header>All kills</div>
-{/if}
-{$sys_killlist}
-{$sys_splitter}
