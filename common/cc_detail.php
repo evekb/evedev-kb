3c35b77ba498d2f911964937c99948d47d94215e
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2009-10-07 11:11:59 +0000

    Check if given contract ID is valid and return an error if not.

--- common/cc_detail.php
+++ common/cc_detail.php
@@ -9,21 +9,27 @@ $ctr_id = intval($_GET['ctr_id']);
 $_GET['scl_id'] = intval($_GET['scl_id']);
 $contract = new Contract($ctr_id);
 
+if(!$contract->validate())
+{
+	echo 'No valid contract ID specified<br />';
+	return;
+}
+
 if ($contract->getCampaign())
-    $title = 'Campaign details';
+	$title = 'Campaign details';
 else
-    $title = 'Contract details';
+	$title = 'Contract details';
 
 $page = new Page($title.' - '.$contract->getName());
 
 if ($contract->getCampaign())
-    $smarty->assign('campaign', true);
+	$smarty->assign('campaign', true);
 else
-    $smarty->assign('campaign', false);
+	$smarty->assign('campaign', false);
 if ($contract->getEndDate() == "")
-    $smarty->assign('contract_enddate', "Active");
+	$smarty->assign('contract_enddate', "Active");
 else
-    $smarty->assign('contract_enddate', substr($contract->getEndDate(), 0, 10));
+	$smarty->assign('contract_enddate', substr($contract->getEndDate(), 0, 10));
 $smarty->assign('contract_startdate', substr($contract->getStartDate(), 0, 10));
 $smarty->assign('kill_count', $contract->getKills());
 $smarty->assign('loss_count', $contract->getLosses());
@@ -41,7 +47,7 @@ $smarty->assign('contract_summary', $killsummary->generate());
 $smarty->assign('view',$_GET['view']);
 switch ($_GET['view'])
 {
-    case "":
+	case "":
 		$qrylength=new DBQuery();
 		// set break at half of the number of valid classes - excludes noob ships, drones and unknown
 		$qrylength->execute("SELECT count(*) - 3 AS cnt FROM kb3_ship_classes");
@@ -54,120 +60,120 @@ switch ($_GET['view'])
 		unset($qrylength);
 		$targets = array();
 		$curtarget = array();
-        while ($target = &$contract->getContractTarget())
-        {
-            $kl = &$target->getKillList();
-            $ll = &$target->getLossList();
-            $summary = new KillSummaryTable($kl, $ll);
-            $summary->setVerbose(true);
+		while ($target = &$contract->getContractTarget())
+		{
+			$kl = &$target->getKillList();
+			$ll = &$target->getLossList();
+			$summary = new KillSummaryTable($kl, $ll);
+			$summary->setVerbose(true);
 			$summary->setBreak($breaklen);
-            $summary->setView('combined');
+			$summary->setView('combined');
 
 			$curtargets['type'] = $target->getType();
 			$curtargets['id'] = $target->getID();
 			$curtargets['name'] = $target->getName();
 			$curtargets['summary'] = $summary->generate();
 
-            if ($summary->getTotalKillISK())
-                $curtargets['efficiency'] = round($summary->getTotalKillISK() / ($summary->getTotalKillISK() + $summary->getTotalLossISK()) * 100, 2);
-            else
-                $curtargets['efficiency'] = 0;
+			if ($summary->getTotalKillISK())
+				$curtargets['efficiency'] = round($summary->getTotalKillISK() / ($summary->getTotalKillISK() + $summary->getTotalLossISK()) * 100, 2);
+			else
+				$curtargets['efficiency'] = 0;
 			$curtargets['total_kills'] = $summary->getTotalKills();
 			$curtargets['total_losses'] = $summary->getTotalLosses();
 			$curtargets['total_kill_isk'] = round($summary->getTotalKillISK()/1000000000, 2);
 			$curtargets['total_loss_isk'] = round($summary->getTotalLossISK()/1000000000, 2);
-            $bar = new BarGraph($curtargets['efficiency'], 100, 120);
+			$bar = new BarGraph($curtargets['efficiency'], 100, 120);
 			$curtargets['bar'] = $bar->generate();
 			$targets[] = $curtargets;
-        }
+		}
 		$smarty->assign_by_ref('targets', $targets);
 		$html .= $smarty->fetch(get_tpl('cc_detail'));
-        break;
-    case "recent_activity":
-        $contract = new Contract($ctr_id);
-        $klist = $contract->getKillList();
-        $klist->setOrdered(true);
-        if ($_GET['scl_id'])
-            $klist->addVictimShipClass($_GET['scl_id']);
-        else
-            $klist->setPodsNoobShips(false);
+		break;
+	case "recent_activity":
+		$contract = new Contract($ctr_id);
+		$klist = $contract->getKillList();
+		$klist->setOrdered(true);
+		if ($_GET['scl_id'])
+			$klist->addVictimShipClass($_GET['scl_id']);
+		else
+			$klist->setPodsNoobShips(false);
 
 		$table = new KillListTable($klist);
-        $table->setLimit(10);
-        $table->setDayBreak(false);
-        $smarty->assign('killtable', $table->generate());
+		$table->setLimit(10);
+		$table->setDayBreak(false);
+		$smarty->assign('killtable', $table->generate());
 
-        $llist = $contract->getLossList();
-        $llist->setOrdered(true);
-        if ($_GET['scl_id'])
-            $llist->addVictimShipClass($_GET['scl_id']);
-        else
-            $llist->setPodsNoobShips(false);
+		$llist = $contract->getLossList();
+		$llist->setOrdered(true);
+		if ($_GET['scl_id'])
+			$llist->addVictimShipClass($_GET['scl_id']);
+		else
+			$llist->setPodsNoobShips(false);
 
-        $table = new KillListTable($llist);
-        $table->setLimit(10);
-        $table->setDayBreak(false);
-        $smarty->assign('losstable', $table->generate());
+		$table = new KillListTable($llist);
+		$table->setLimit(10);
+		$table->setDayBreak(false);
+		$smarty->assign('losstable', $table->generate());
 		$html .= $smarty->fetch(get_tpl('cc_detail'));
-        break;
-    case "kills":
-        $contract = new Contract($ctr_id);
-        $list = $contract->getKillList();
-        $list->setOrdered(true);
-        if ($_GET['scl_id'])
-            $list->addVictimShipClass($_GET['scl_id']);
+		break;
+	case "kills":
+		$contract = new Contract($ctr_id);
+		$list = $contract->getKillList();
+		$list->setOrdered(true);
+		if ($_GET['scl_id'])
+			$list->addVictimShipClass($_GET['scl_id']);
 
 		$list->setPageSplit(config::get('killcount'));
 		$pagesplitter = new PageSplitter($list->getCount(), config::get('killcount'));
-        $table = new KillListTable($list);
-        $table->setDayBreak(false);
-        $smarty->assign('killtable', $table->generate());
-        $smarty->assign('splitter', $pagesplitter->generate());
+		$table = new KillListTable($list);
+		$table->setDayBreak(false);
+		$smarty->assign('killtable', $table->generate());
+		$smarty->assign('splitter', $pagesplitter->generate());
 		$html .= $smarty->fetch(get_tpl('cc_detail'));
-        break;
-    case "losses":
-        $contract = new Contract($ctr_id);
-        $llist = $contract->getLossList();
-        $llist->setOrdered(true);
-        if ($_GET['scl_id'])
-            $llist->addVictimShipClass($_GET['scl_id']);
+		break;
+	case "losses":
+		$contract = new Contract($ctr_id);
+		$llist = $contract->getLossList();
+		$llist->setOrdered(true);
+		if ($_GET['scl_id'])
+			$llist->addVictimShipClass($_GET['scl_id']);
 
 		$llist->setPageSplit(config::get('killcount'));
 		$pagesplitter = new PageSplitter($llist->getCount(), config::get('killcount'));
-        $table = new KillListTable($llist);
-        $table->setDayBreak(false);
-        $smarty->assign('losstable', $table->generate());
-        $smarty->assign('splitter', $pagesplitter->generate());
+		$table = new KillListTable($llist);
+		$table->setDayBreak(false);
+		$smarty->assign('losstable', $table->generate());
+		$smarty->assign('splitter', $pagesplitter->generate());
 		$html .= $smarty->fetch(get_tpl('cc_detail'));
-        break;
-    case "combined":
-        $contract = new Contract($ctr_id);
-        $list = $contract->getKillList();
-        $list->setOrdered(true);
-        if ($_GET['scl_id'])
-            $list->addVictimShipClass($_GET['scl_id']);
+		break;
+	case "combined":
+		$contract = new Contract($ctr_id);
+		$list = $contract->getKillList();
+		$list->setOrdered(true);
+		if ($_GET['scl_id'])
+			$list->addVictimShipClass($_GET['scl_id']);
 
 		$list->setPageSplit(config::get('killcount'));
 		$pagesplitter = new PageSplitter($list->getCount(), config::get('killcount'));
-        $table = new KillListTable($list);
-        $table->setDayBreak(false);
-        $smarty->assign('killtable', $table->generate());
-        $smarty->assign('killsplitter', $pagesplitter->generate());
+		$table = new KillListTable($list);
+		$table->setDayBreak(false);
+		$smarty->assign('killtable', $table->generate());
+		$smarty->assign('killsplitter', $pagesplitter->generate());
 
-        $contract = new Contract($ctr_id);
-        $llist = $contract->getLossList();
-        $llist->setOrdered(true);
-        if ($_GET['scl_id'])
-            $llist->addVictimShipClass($_GET['scl_id']);
+		$contract = new Contract($ctr_id);
+		$llist = $contract->getLossList();
+		$llist->setOrdered(true);
+		if ($_GET['scl_id'])
+			$llist->addVictimShipClass($_GET['scl_id']);
 
 		$llist->setPageSplit(config::get('killcount'));
 		$pagesplitter = new PageSplitter($llist->getCount(), config::get('killcount'));
-        $table = new KillListTable($llist);
-        $table->setDayBreak(false);
-        $smarty->assign('losstable', $table->generate());
-        $smarty->assign('losssplitter', $pagesplitter->generate());
+		$table = new KillListTable($llist);
+		$table->setDayBreak(false);
+		$smarty->assign('losstable', $table->generate());
+		$smarty->assign('losssplitter', $pagesplitter->generate());
 		$html .= $smarty->fetch(get_tpl('cc_detail'));
-        break;
+		break;
 }
 
 $menubox = new box("Menu");
@@ -187,22 +193,22 @@ involved::load($tklist,'kill');
 
 $tklist->generate();
 if ($contract->getCampaign())
-    $campaign = "campaign";
+	$campaign = "campaign";
 else
-    $campaign = "contract";
+	$campaign = "contract";
 $tkbox = new AwardBox($tklist, "Top killers", "kills in this ".$campaign, "kills", "eagle");
 
 $page->addContext($tkbox->generate());
 
 if (config::get('kill_points'))
 {
-    $tklist = new TopContractScoreList();
-    $tklist->setContract(new Contract($ctr_id));
-    involved::load($tklist,'kill');
+	$tklist = new TopContractScoreList();
+	$tklist->setContract(new Contract($ctr_id));
+	involved::load($tklist,'kill');
 
-    $tklist->generate();
-    $tkbox = new AwardBox($tklist, "Top scorers", "points in this ".$campaign, "points", "redcross");
-    $page->addContext($tkbox->generate());
+	$tklist->generate();
+	$tkbox = new AwardBox($tklist, "Top scorers", "points in this ".$campaign, "points", "redcross");
+	$page->addContext($tkbox->generate());
 }
 
 $page->setContent($html);
