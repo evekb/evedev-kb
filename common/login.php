80cc8e5f109bc5cd1a5ca8c02032eaf25fa67a7f
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2009-10-31 03:58:48 +0000

    Legacy unencrypted admin passwords are automatically encrypted on first use.
    Fixes issue 29

--- common/login.php
+++ common/login.php
@@ -3,7 +3,35 @@ $page = new Page('Login');
 
 if (trim($_POST['usrpass']))
 {
-	if ($_POST['usrlogin'] == '' && crypt($_POST['usrpass'],ADMIN_PASSWORD) == ADMIN_PASSWORD )
+	if($_POST['usrlogin'] == '' && $_POST['usrpass'] == ADMIN_PASSWORD 
+		&& substr(ADMIN_PASSWORD,0,3) != '$1$'
+		&& substr(ADMIN_PASSWORD,0,3) != '$2$'
+		&& substr(ADMIN_PASSWORD,0,3) != '$2a$')
+	{
+		@chmod("kbconfig.php", 0660);
+		if(!is_writeable("kbconfig.php"))
+		{
+			$smarty->assign('error', 'Admin password is unencrypted and '.
+				'kbconfig.php is not writeable. Either encrypt the admin '.
+				'password or set kbconfig.php writeable.');
+		}
+		else
+		{
+			$kbconfig = file_get_contents('kbconfig.php');
+			$newpwd = preg_replace('/(\$|\\\\)/', '\\\\$1',crypt(ADMIN_PASSWORD));
+			$kbconfig = preg_replace('/define\s*\(\s*[\'"]ADMIN_PASSWORD[\'"][^)]*\)/',
+					"define('ADMIN_PASSWORD', '".$newpwd."')", $kbconfig);
+			file_put_contents("kbconfig.php", trim($kbconfig));
+			chmod("kbconfig.php", 0440);
+
+			session::create(true);
+
+			session_write_close();
+			header('Location: ?a=admin');
+			die;
+		}
+	}
+	elseif ($_POST['usrlogin'] == '' && crypt($_POST['usrpass'],ADMIN_PASSWORD) == ADMIN_PASSWORD )
 	{
 		session::create(true);
 
@@ -28,4 +56,3 @@ if (trim($_POST['usrpass']))
 
 $page->setContent($smarty->fetch(get_tpl('user_login')));
 $page->generate();
-?>
