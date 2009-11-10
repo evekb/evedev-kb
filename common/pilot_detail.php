a1b1e1a3993a392d180535922ef0af813d4a1bc7
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2009-11-10 11:04:10 +0000

    pilot details updated. HTML corrected (not in template yet) and need for one killlist removed.

--- common/pilot_detail.php
+++ common/pilot_detail.php
@@ -8,7 +8,7 @@ require_once('common/includes/class.killlisttable.php');
 require_once('common/includes/class.killsummarytable.php');
 require_once('common/includes/class.toplist.php');
 
-$_GET['scl_id'] = intval($_GET['scl_id']);
+$scl_id = intval($_GET['scl_id']);
 $plt_id = intval($_GET['plt_id']);
 $plt_external_id = intval($_GET['plt_external_id']);
 if(!$plt_id)
@@ -44,42 +44,40 @@ if (!$pilot->exists())
 }
 
 $klist = new KillList();
-$tklist = new KillList();
 $llist = new KillList();
 $klist->addInvolvedPilot($pilot);
-$tklist->addInvolvedPilot($pilot);
 $llist->addVictimPilot($pilot);
 $klist->getAllKills();
 $llist->getAllKills();
-$tklist->setPodsNoobShips(false);
 
-$html .= "<table class=kb-table cellspacing=1 width=\"100%\">";
+$html .= "<table class='kb-table' cellspacing='1' width=\"100%\">";
 
-$html .= "<tr class=kb-table-row-even>";
-$html .= "<td rowspan=8 width=128><img src=\"".$pilot->getPortraitURL(128)."\" border=\"0\" width=\"128\" height=\"128\" alt=\"portrait\"></td>";
+$html .= "<tr class='kb-table-row-even'>";
+$html .= "<td rowspan='8' width='128'><img src=\"".$pilot->getPortraitURL(128)."\" border=\"0\" width=\"128\" height=\"128\" alt=\"portrait\" /></td>";
 
-$html .= "<td class=kb-table-cell width=160><b>Corporation:</b></td><td class=kb-table-cell><a href=\"?a=corp_detail&amp;crp_id=".$corp->getID()."\">".$corp->getName()."</a></td></tr>";
-$html .= "<tr class=kb-table-row-even><td class=kb-table-cell><b>Alliance:</b></td><td class=kb-table-cell>";
+$html .= "<td class='kb-table-cell' width='160'><b>Corporation:</b></td><td class='kb-table-cell'><a href=\"?a=corp_detail&amp;crp_id=".$corp->getID()."\">".$corp->getName()."</a></td></tr>";
+$html .= "<tr class='kb-table-row-even'><td class='kb-table-cell'><b>Alliance:</b></td><td class='kb-table-cell'>";
 if ($alliance->getName() == "Unknown" || $alliance->getName() == "None")
 	$html .= "<b>".$alliance->getName()."</b>";
 else
 	$html .= "<a href=\"?a=alliance_detail&amp;all_id=".$alliance->getID()."\">".$alliance->getName()."</a>";
-$html .= "<tr class=kb-table-row-even><td class=kb-table-cell><b>Kills:</b></td><td class=kl-kill>".$klist->getCount()."</td></tr>";
-$html .= "<tr class=kb-table-row-even><td class=kb-table-cell><b>Real kills:</b></td><td class=kl-kill>".$tklist->getCount()."</td></tr>";
-$html .= "<tr class=kb-table-row-even><td class=kb-table-cell><b>Losses:</b></td><td class=kl-loss>".$llist->getCount()."</td></tr>";
-$html .= "<tr class=kb-table-row-even><td class=kb-table-cell><b>Damage done (ISK):</b></td><td class=kl-kill>".round($klist->getISK()/1000000000,2)."B</td></tr>";
-$html .= "<tr class=kb-table-row-even><td class=kb-table-cell><b>Damage received (ISK):</b></td><td class=kl-loss>".round($llist->getISK()/1000000000,2)."B</td></tr>";
+$html .= "</td></tr>";
+$html .= "<tr class='kb-table-row-even'><td class='kb-table-cell'><b>Kills:</b></td><td class='kl-kill'>".$klist->getCount()."</td></tr>";
+$html .= "<tr class='kb-table-row-even'><td class='kb-table-cell'><b>Real kills:</b></td><td class='kl-kill'>".$klist->getRealCount()."</td></tr>";
+$html .= "<tr class='kb-table-row-even'><td class='kb-table-cell'><b>Losses:</b></td><td class='kl-loss'>".$llist->getCount()."</td></tr>";
+$html .= "<tr class='kb-table-row-even'><td class='kb-table-cell'><b>Damage done (ISK):</b></td><td class='kl-kill'>".round($klist->getISK()/1000000000,2)."B</td></tr>";
+$html .= "<tr class='kb-table-row-even'><td class='kb-table-cell'><b>Damage received (ISK):</b></td><td class='kl-loss'>".round($llist->getISK()/1000000000,2)."B</td></tr>";
 
 //Pilot Efficiency Mod Begin (K Austin)
 
-if ($tklist->getCount() == 0)
+if ($klist->getRealCount() == 0)
 {
 	$pilot_survival = 100;
 	$pilot_efficiency = 0;
 }
 else
 {
-	if($tklist->getCount() + $llist->getCount()) $pilot_survival = round($llist->getCount() / ($tklist->getCount() + $llist->getCount()) * 100,2);
+	if($klist->getRealCount() + $llist->getCount()) $pilot_survival = round($llist->getCount() / ($klist->getRealCount() + $llist->getCount()) * 100,2);
 	else $pilot_survival = 0;
 	if($klist->getISK() + $llist->getISK()) $pilot_efficiency = round(($klist->getISK() / ($klist->getISK() + $llist->getISK())) * 100,2);
 	else $pilot_efficiency = 0;
@@ -93,8 +91,8 @@ else $ps_color = "#AA0000";
 if ($pilot_efficiency < $half) $pe_color = "#AA0000";
 else $pe_color = "#00AA00";
 
-$html .= "<tr class=kb-table-row-even><td class=kb-table-cell><b>Chance of enemy survival:</b></td><td class=kb-table-cell><b><span style=\"color:" .$ps_color .";\">".$pilot_survival ."%</span></b></td></tr>";
-$html .= "<tr class=kb-table-row-even><td class=kb-table-cell></td><td class=kb-table-cell><b>Pilot Efficiency (ISK):</b></td><td class=kb-table-cell><b><span style=\"color:" .$pe_color .";\">".$pilot_efficiency ."%</span></b></td></tr>";
+$html .= "<tr class='kb-table-row-even'><td class='kb-table-cell'><b>Chance of enemy survival:</b></td><td class='kb-table-cell'><b><span style=\"color:" .$ps_color .";\">".$pilot_survival ."%</span></b></td></tr>";
+$html .= "<tr class='kb-table-row-even'><td class='kb-table-cell'></td><td class='kb-table-cell'><b>Pilot Efficiency (ISK):</b></td><td class='kb-table-cell'><b><span style=\"color:" .$pe_color .";\">".$pilot_efficiency ."%</span></b></td></tr>";
 
 //Pilot Efficiency Mod End
 
@@ -118,13 +116,12 @@ $html .= $summary->generate();
 switch ($_GET['view'])
 {
 	case "kills":
-		$html .= "<div class=kb-kills-header>All kills</div>";
+		$html .= "<div class='kb-kills-header'>All kills</div>";
 
 		$list = new KillList();
 		$list->setOrdered(true);
 		$list->addInvolvedPilot($pilot);
-		if ($_GET['scl_id'])
-			$list->addVictimShipClass($_GET['scl_id']);
+		if ($scl_id) $list->addVictimShipClass($scl_id);
 		$list->setPageSplit(config::get('killcount'));
 		$pagesplitter = new PageSplitter($list->getCount(), config::get('killcount'));
 		$table = new KillListTable($list);
@@ -134,14 +131,13 @@ switch ($_GET['view'])
 
 		break;
 	case "losses":
-		$html .= "<div class=kb-losses-header>All losses</div>";
+		$html .= "<div class='kb-losses-header'>All losses</div>";
 
 		$list = new KillList();
 		$list->setOrdered(true);
-		$list->setPodsNoobships(true);
+		$list->setPodsNoobships(config::get('podnoobs'));
 		$list->addVictimPilot($pilot);
-		if ($_GET['scl_id'])
-			$list->addVictimShipClass($_GET['scl_id']);
+		if ($scl_id) $list->addVictimShipClass($scl_id);
 		$list->setPageSplit(config::get('killcount'));
 		$pagesplitter = new PageSplitter($list->getCount(), config::get('killcount'));
 
@@ -151,9 +147,9 @@ switch ($_GET['view'])
 		$html .= $pagesplitter->generate();
 		break;
 	case "ships_weapons":
-		$html .= "<div class=block-header2>Ships & weapons used</div>";
+		$html .= "<div class='block-header2'>Ships & weapons used</div>";
 
-		$html .= "<table class=kb-subtable><tr><td valign=top width=400>";
+		$html .= "<table class='kb-subtable'><tr><td valign=top width=400>";
 		$shiplist = new TopShipList();
 		$shiplist->addInvolvedPilot($pilot);
 		$shiplisttable = new TopShipListTable($shiplist);
@@ -168,31 +164,29 @@ switch ($_GET['view'])
 
 		break;
 	default:
-		$html .= "<div class=kb-kills-header>10 Most recent kills</div>";
+		$html .= "<div class='kb-kills-header'>10 Most recent kills</div>";
 		$list = new KillList();
 		$list->setOrdered(true);
 		if (config::get('comments_count')) $list->setCountComments(true);
 		if (config::get('killlist_involved')) $list->setCountInvolved(true);
 		$list->setLimit(10);
-		$list->setPodsNoobships(true);
+		$list->setPodsNoobships(config::get('podnoobs'));
 		$list->addInvolvedPilot($pilot);
-		if ($_GET['scl_id'])
-			$list->addVictimShipClass($_GET['scl_id']);
+		if ($scl_id) $list->addVictimShipClass($scl_id);
 
 		$table = new KillListTable($list);
 		$table->setDayBreak(false);
 		$html .= $table->generate();
 
-		$html .= "<div class=kb-losses-header>10 Most recent losses</div>";
+		$html .= "<div class='kb-losses-header'>10 Most recent losses</div>";
 		$list = new KillList();
 		$list->setOrdered(true);
 		if (config::get('comments_count')) $list->setCountComments(true);
 		if (config::get('killlist_involved')) $list->setCountInvolved(true);
 		$list->setLimit(10);
-		$list->setPodsNoobships(true);
+		$list->setPodsNoobships(config::get('podnoobs'));
 		$list->addVictimPilot($pilot);
-		if ($_GET['scl_id'])
-			$list->addVictimShipClass($_GET['scl_id']);
+		if ($scl_id) $list->addVictimShipClass($scl_id);
 
 		$table = new KillListTable($list);
 		$table->setDayBreak(false);
