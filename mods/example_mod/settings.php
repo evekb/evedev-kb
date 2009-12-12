e310c6389549868b69d9a2b32fafee75bf9fa8bd
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2009-12-12 05:37:02 +0000

    Removing example mod. Existing default mods are sufficient for demonstration.

--- mods/example_mod/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once( "common/admin/admin_menu.php" );
-
-$page = new Page( "Settings - Example Mod" );
-                                                                         
-$page->setContent( $html );
-$page->addContext( $menubox->generate() );
-$page->generate();
-?>
