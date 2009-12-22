0246c14a250da479de262e54474ea211a6e8015d
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2009-12-22 11:02:23 +0000

    Prepared statement execute command now works identically to execute_prepared.

--- common/includes/class.db.mysqli.prepared.php
+++ common/includes/class.db.mysqli.prepared.php
@@ -3,93 +3,92 @@ require_once('common/includes/class.db.mysqli.php');
 //! mysqli uncached query class. Manages SQL queries to a MySQL DB using mysqli.
 class DBPreparedQuery
 {
-    //! Prepare a connection for a new mysqli query.
-    function DBPreparedQuery()
-    {
-        $this->executed_ = false;
-        $this->dbconn_ = new DBConnection_mysqli;
-        static $totalexectime = 0;
+//! Prepare a connection for a new mysqli query.
+	function DBPreparedQuery()
+	{
+		$this->executed_ = false;
+		$this->dbconn_ = new DBConnection_mysqli;
+		static $totalexectime = 0;
 		$this->totalexectime_ = &$totalexectime;
-    }
-    //! Return the count of queries performed.
+	}
+	//! Return the count of queries performed.
 
     /*!
      * \param $increase if true then increment the count.
      * \return the count of queries so far.
      */
-    function queryCount($increase = false)
-    {
-        static $count;
+	function queryCount($increase = false)
+	{
+		static $count;
 
-        if ($increase)
-        {
-            $count++;
-        }
+		if ($increase)
+		{
+			$count++;
+		}
 
-        return $count;
-    }
-    //! Return the count of cached queries performed - 0 for uncache queries.
-    function queryCachedCount($increase = false)
-    {
-        return 0;
-    }
-    //! Execute an SQL string.
+		return $count;
+	}
+	//! Return the count of cached queries performed - 0 for uncache queries.
+	function queryCachedCount($increase = false)
+	{
+		return 0;
+	}
+	//! Execute the prepared command.
 
-    /* 
+    /*
      * If DB_HALTONERROR is set then this will exit on an error.
      * \return false on error or true if successful.
      */
-    function execute($sql)
-    {
-		if(isset($this->stmt)) $this->execute_prepared();
-		else return false;
-    }
-    //! Return the number of rows returned by the last query.
-    function recordCount()
-    {
+	function execute()
+	{
+		return $this->execute_prepared();
+	}
+	//! Return the number of rows returned by the last query.
+	function recordCount()
+	{
 		if($this->stmt) return $this->stmt->num_rows;
-        if ($this->resid_)
-        {
-            return $this->resid_->num_rows;
-        }
-        return false;
-    }
-    //! Return the auto-increment ID from the last insert operation.
-    function getInsertID()
-    {
-        return $this->stmt->insert_id;
-    }
-    //! Return the most recent error message for the DB connection.
-    function getErrorMsg()
-    {
+		if ($this->resid_)
+		{
+			return $this->resid_->num_rows;
+		}
+		return false;
+	}
+	//! Return the auto-increment ID from the last insert operation.
+	function getInsertID()
+	{
+		return $this->stmt->insert_id;
+	}
+	//! Return the most recent error message for the DB connection.
+	function getErrorMsg()
+	{
 		if($this->stmt)
 		{
 			return "Prepared statement failed: ".$this->stmt->errno;
 		}
-        $msg = $this->sql_ . "<br>";
-        $msg .= "Query failed. " . mysqli_error($this->dbconn_->id());
+		$msg = $this->sql_ . "<br>";
+		$msg .= "Query failed. " . mysqli_error($this->dbconn_->id());
 
-        return $msg;
-    }
-    //! Set the autocommit status.
+		return $msg;
+	}
+	//! Set the autocommit status.
 
     /*! The default of true commits after every query.
-     * If set to false the queries will not be commited until autocommit is set 
+     * If set to false the queries will not be commited until autocommit is set
      * to true.
      *  \param $commit The new autocommit status.
      *  \return true on success and false on failure.
      */
-    function autocommit($commit = true)
-    {
-        return $this->dbconn_->id()->autocommit($commit);
-    }
-    //! Rollback all queries in the current transaction.
-    function rollback()
-    {
-        return mysqli_rollback($this->dbconn_->id());
-    }
+	function autocommit($commit = true)
+	{
+		return $this->dbconn_->id()->autocommit($commit);
+	}
+	//! Rollback all queries in the current transaction.
+	function rollback()
+	{
+		return mysqli_rollback($this->dbconn_->id());
+	}
 	//! Prepare a statement.
-	
+
 	/* \param $sql String containing a prepared statement.
 	 * \return true on success and false on failure.
 	 */
@@ -98,21 +97,21 @@ class DBPreparedQuery
 		$this->stmt = $this->dbconn_->id()->prepare($sql);
 		if(!$this->stmt)
 		{
-            if(defined('KB_PROFILE'))
+			if(defined('KB_PROFILE'))
 			{
 				DBDebug::recordError("Database error: ".$this->stmt->error);
 				DBDebug::recordError("SQL: ".$sql);
 			}
-            if (defined('DB_HALTONERROR') && DB_HALTONERROR)
-            {
-                echo "Database error: " . $this->stmt->error . "<br>";
-                echo "SQL: " . $sql . "<br>";
-                exit;
-            }
-            else
-            {
-                return false;
-            }
+			if (defined('DB_HALTONERROR') && DB_HALTONERROR)
+			{
+				echo "Database error: " . $this->stmt->error . "<br>";
+				echo "SQL: " . $sql . "<br>";
+				exit;
+			}
+			else
+			{
+				return false;
+			}
 		}
 		return true;
 	}
@@ -126,10 +125,8 @@ class DBPreparedQuery
 		$arr[0]=$this->stmt;
 		$args = func_get_args();
 		$Args = array();
-        foreach($args as $k => &$arg){
-            $Args[$k] = &$arg;
-        }
- 		array_unshift($Args,$this->stmt);
+		foreach($args as $k => &$arg) $Args[$k] = &$arg;
+		array_unshift($Args,$this->stmt);
 		return call_user_func_array('mysqli_stmt_bind_param',$Args);
 	}
 	//! Bind the prepared query results to the given variables.
@@ -165,29 +162,36 @@ class DBPreparedQuery
 		}
 		return call_user_func_array(array($this->stmt,'bind_result'),$args);
 	}
+	//! Execute the prepared command.
+	
+    /*
+     * If DB_HALTONERROR is set then this will exit on an error.
+     * \return false on error or true if successful.
+     */
 	function execute_prepared()
 	{
 		if(!$this->stmt->execute())
 		{
-            if(defined('KB_PROFILE'))
+			if(defined('KB_PROFILE'))
 			{
 				DBDebug::recordError("Database error: ".$this->dbconn_->id()->error);
 				DBDebug::recordError("SQL: ".$sql);
 			}
-            if (defined('DB_HALTONERROR') && DB_HALTONERROR)
-            {
-                echo "Database error: " . $this->dbconn_->id()->error . "<br>";
-                echo "SQL: " . $sql . "<br>";
-                exit;
-            }
-            else
-            {
-                return false;
-            }
+			if (defined('DB_HALTONERROR') && DB_HALTONERROR)
+			{
+				echo "Database error: " . $this->dbconn_->id()->error . "<br>";
+				echo "SQL: " . $sql . "<br>";
+				exit;
+			}
+			else
+			{
+				return false;
+			}
 		}
 		$this->stmt->store_result();
 		return true;
 	}
+	//! Fetch the next results of the prepared statement into bound variables.
 	function fetch_prepared()
 	{
 		return $this->stmt->fetch();
