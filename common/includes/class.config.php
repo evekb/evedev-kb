a253a720a6aa44a40ac9892f45d5a29811db692d
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-01-27 11:26:24 +0000

    Config uses static storage for site.

--- common/includes/class.config.php
+++ common/includes/class.config.php
@@ -4,164 +4,181 @@ require_once('common/includes/class.registry.php');
 
 class Config
 {
-    function Config($site = KB_SITE)
-    {
-        $this->site = $site;
-        config::init();
-    }
+	//! Set up the config for the given site.
 
-    function checkCheckbox($name)
-    {
-        if ($_POST[$name] == 'on')
-        {
-            config::set($name, '1');
-            return true;
-        }
-        config::set($name, '0');
-        return false;
-    }
+	/*!
+	 *  \param $site The site to configure for. Default is KB_SITE define.
+	 */
+	function Config($site = KB_SITE)
+	{
+		$configSite = &Config::_getSite();
+		$configSite = $site;
+		Config::init();
+	}
+	//! Return the site used for this configuration.
 
-    function init()
-    {
-        global $config_init;
+	/*!
+	 * \returns String containing the site used for this configuration.
+	 */
+	function &_getSite()
+	{
+		static $site;
+		if(!isset($site)) $site = KB_SITE;
+		return $site;
+	}
 
-        if ($config_init)
-        {
-            return;
-        }
+	function checkCheckbox($name)
+	{
+		if ($_POST[$name] == 'on')
+		{
+			Config::set($name, '1');
+			return true;
+		}
+		Config::set($name, '0');
+		return false;
+	}
 
-        $db = new DBQuery();
-        $db->execute("SELECT * FROM kb3_config WHERE cfg_site='".$this->site."'");
-        $config = &config::_getCache();
-        while ($row = $db->getRow())
-        {
-            if (substr($row['cfg_value'], 0, 2) == 'a:')
-            {
-                $config[$row['cfg_key']] = unserialize($row['cfg_value']);
-            }
-            else
-            {
-                $config[$row['cfg_key']] = stripslashes($row['cfg_value']);
-            }
-        }
-        $config_init = true;
+	function init()
+	{
+		global $config_init;
 
-        $defaults = array();
-        $defaults['killcount'] = 50;
-        $defaults['kill_points'] = 1;
-        $defaults['least_active'] = 0;
-        registry::set('config_defaults', $defaults);
+		if ($config_init)
+		{
+			return;
+		}
 
-        if (config::get('post_password') === null)
-        {
-        // no config supplied, generate standard one
-            config::set('theme_name', 'default');
-            config::set('style_name', 'default');
-            config::set('style_banner', 'default.jpg');
-            config::set('post_password', 'CHANGEME');
-            config::set('comment_password', 'CHANGEME');
-            config::set('cfg_memcache', 0);
-            config::set('cfg_memcache_server', 'memcached server address');
-            config::set('cfg_memcache_port', 'memcached server port');
-//            config::set('cache_dir', KB_QUERYCACHEDIR);
-//            config::set('km_cache_dir', KB_CACHEDIR.'/mails');
-            config::set('DBUpdate',LATEST_DB_UPDATE);
-        }
-    }
+		$db = new DBQuery();
+		$db->execute("SELECT * FROM kb3_config WHERE cfg_site='".Config::_getSite()."'");
+		$config = &Config::_getCache();
+		while ($row = $db->getRow())
+		{
+			if (substr($row['cfg_value'], 0, 2) == 'a:')
+			{
+				$config[$row['cfg_key']] = unserialize($row['cfg_value']);
+			}
+			else
+			{
+				$config[$row['cfg_key']] = stripslashes($row['cfg_value']);
+			}
+		}
+		$config_init = true;
 
-    function &_getCache()
-    {
-        static $cache;
+		$defaults = array();
+		$defaults['killcount'] = 50;
+		$defaults['kill_points'] = 1;
+		$defaults['least_active'] = 0;
+		registry::set('config_defaults', $defaults);
 
-        if (!isset($cache))
-        {
-            $cache = array();
-        }
-        return $cache;
-    }
+		if (Config::get('post_password') === null)
+		{
+			// no config supplied, generate standard one
+			Config::set('theme_name', 'default');
+			Config::set('style_name', 'default');
+			Config::set('style_banner', 'default.jpg');
+			Config::set('post_password', 'CHANGEME');
+			Config::set('comment_password', 'CHANGEME');
+			Config::set('cfg_memcache', 0);
+			Config::set('cfg_memcache_server', 'memcached server address');
+			Config::set('cfg_memcache_port', 'memcached server port');
+//            Config::set('cache_dir', KB_QUERYCACHEDIR);
+//            Config::set('km_cache_dir', KB_CACHEDIR.'/mails');
+			Config::set('DBUpdate',LATEST_DB_UPDATE);
+		}
+	}
 
-    function put($key, $data)
-    {
-        $cache = &config::_getCache();
-        $cache[$key] = $data;
-    }
+	function &_getCache()
+	{
+		static $cache;
 
-    function del($key)
-    {
-        $cache = &config::_getCache();
-        if (isset($cache[$key]))
-        {
-            unset($cache[$key]);
-        }
+		if (!isset($cache))
+		{
+			$cache = array();
+		}
+		return $cache;
+	}
 
-        $qry = new DBQuery(true);
-        $qry->execute("DELETE FROM kb3_config WHERE cfg_key = '".$key."'
-                       AND cfg_site = '".$this->site."'");
-    }
+	function put($key, $data)
+	{
+		$cache = &Config::_getCache();
+		$cache[$key] = $data;
+	}
 
-    function set($key, $value)
-    {
-        $cache = &config::_getCache();
+	function del($key)
+	{
+		$cache = &Config::_getCache();
+		if (isset($cache[$key]))
+		{
+			unset($cache[$key]);
+		}
 
-        // only update the database when the old value differs
-        if (isset($cache[$key]))
-        {
-            if ($cache[$key] === $value)
-            {
-                return;
-            }
-        }
+		$qry = new DBQuery(true);
+		$qry->execute("DELETE FROM kb3_config WHERE cfg_key = '".$key."'
+                       AND cfg_site = '".Config::_getSite()."'");
+	}
 
-        if (is_array($value))
-        {
-            $cache[$key] = $value;
-            $value = serialize($value);
-        }
-        else
-        {
-            $cache[$key] = stripslashes($value);
-        }
-        $value = addslashes($value);
+	function set($key, $value)
+	{
+		$cache = &Config::_getCache();
 
-        $qry = new DBQuery(true);
-        $sql = "INSERT INTO kb3_config (cfg_site, cfg_key, cfg_value) VALUES ('".
-            $this->site."','".$key."','".$value.
-            "') ON DUPLICATE KEY UPDATE cfg_value = '".$value."'";
-        $qry->execute($sql);
-    }
+		// only update the database when the old value differs
+		if (isset($cache[$key]))
+		{
+			if ($cache[$key] === $value)
+			{
+				return;
+			}
+		}
 
-    function &get($key)
-    {
-        $cache = &config::_getCache();
+		if (is_array($value))
+		{
+			$cache[$key] = $value;
+			$value = serialize($value);
+		}
+		else
+		{
+			$cache[$key] = stripslashes($value);
+		}
+		$value = addslashes($value);
 
-        if (!isset($cache[$key]))
-        {
-            return config::defaultval($key);
-        }
-        return $cache[$key];
-    }
+		$qry = new DBQuery(true);
+		$sql = "INSERT INTO kb3_config (cfg_site, cfg_key, cfg_value) VALUES ('".
+				Config::_getSite()."','".$key."','".$value.
+				"') ON DUPLICATE KEY UPDATE cfg_value = '".$value."'";
+		$qry->execute($sql);
+	}
 
-    function &getnumerical($key)
-    {
-        $cache = &config::_getCache();
+	function &get($key)
+	{
+		$cache = &Config::_getCache();
 
-        if (!isset($cache[$key]))
-        {
-            return config::defaultval($key);
-        }
-        return $cache[$key];
-    }
+		if (!isset($cache[$key]))
+		{
+			return Config::defaultval($key);
+		}
+		return $cache[$key];
+	}
 
-    function defaultval($key)
-    {
-    // add important upgrade configs here, they will return the default if not set
-    // they will be shown as set but take no space in the database
-        $defaults = registry::get('config_defaults');
+	function &getnumerical($key)
+	{
+		$cache = &Config::_getCache();
 
-        if (!isset($defaults[$key]))
-        {
-            return null;
-        }
-        return $defaults[$key];
-    }
+		if (!isset($cache[$key]))
+		{
+			return Config::defaultval($key);
+		}
+		return $cache[$key];
+	}
+
+	function defaultval($key)
+	{
+		// add important upgrade configs here, they will return the default if not set
+		// they will be shown as set but take no space in the database
+		$defaults = registry::get('config_defaults');
+
+		if (!isset($defaults[$key]))
+		{
+			return null;
+		}
+		return $defaults[$key];
+	}
 }
