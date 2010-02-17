817a2d5e69cc999410d6ccd0c08af522bf730a9a
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-02-17 10:12:04 +0000

    Killboard static methods set as static.

--- common/includes/class.killboard.php
+++ common/includes/class.killboard.php
@@ -2,28 +2,27 @@
 
 class Killboard
 {
-    function hasCampaigns($active = false)
-    {
-        $qry = DBFactory::getDBQuery();;
-        $sql = "select ctr_id
+	public static function hasCampaigns($active = false)
+	{
+		$qry = DBFactory::getDBQuery();
+		$sql = "select ctr_id
                  from kb3_contracts
 	         where ctr_campaign = 1
 	           and ctr_site = '".KB_SITE."'";
-        if ($active) $sql .= " and ( ctr_ended is null or now() <= ctr_ended )";
-        $qry->execute($sql);
-        return ($qry->recordCount() > 0);
-    }
+		if ($active) $sql .= " and ( ctr_ended is null or now() <= ctr_ended ) limit 1";
+		$qry->execute($sql);
+		return ($qry->recordCount() > 0);
+	}
 
-    function hasContracts($active = false)
-    {
-        $qry = DBFactory::getDBQuery();;
-        $sql = "select ctr_id
+	public static function hasContracts($active = false)
+	{
+		$qry = DBFactory::getDBQuery();
+		$sql = "select ctr_id
                  from kb3_contracts
                  where ctr_campaign = 0
                    and ctr_site = '".KB_SITE."'";
-        if ($active) $sql .= " and ( ctr_ended is null or now() <= ctr_ended )";
-        $qry->execute($sql);
-        return ($qry->recordCount() > 0);
-    }
+		if ($active) $sql .= " and ( ctr_ended is null or now() <= ctr_ended ) limit 1";
+		$qry->execute($sql);
+		return ($qry->recordCount() > 0);
+	}
 }
-?>
