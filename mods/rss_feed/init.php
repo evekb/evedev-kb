89138c7f8874f2730ec613cf5875e99e454ecd23
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-04-02 02:48:36 +0000

    rss feed mod uses static functions.

--- mods/rss_feed/init.php
+++ mods/rss_feed/init.php
@@ -3,15 +3,14 @@ event::register('home_assembling', 'rss_feed::handler');
 
 class rss_feed
 {
-    function handler(&$home)
-    {
-		$home->addBehind("start", "rss_feed::addRSS");
-    }
-	function addRSS($home)
+	public static function handler(&$home)
 	{
-        $home->page->addHeader('<link rel="alternate" type="application/rss+xml" title="Watched Kills" href="?a=rss&amp;kills" />');
-        $home->page->addHeader('<link rel="alternate" type="application/rss+xml" title="Watched Losses" href="?a=rss&amp;losses" />');
+		$home->addBehind("start", "rss_feed::addRSS");
+	}
+	public static function addRSS($home)
+	{
+		$home->page->addHeader('<link rel="alternate" type="application/rss+xml" title="Watched Kills" href="?a=rss&amp;kills" />');
+		$home->page->addHeader('<link rel="alternate" type="application/rss+xml" title="Watched Losses" href="?a=rss&amp;losses" />');
 	}
 
 }
-?>
