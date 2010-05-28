cf8a8ed93a7b12a23faa4585c3769931be4cc135
Fried.RoadKill <Fried.RoadKill@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-05-28 23:01:22 +0000

    Small update to the auto-updater allowing it to actually run SQL queries again.

--- update/CCPDB/db.updater.php
+++ update/CCPDB/db.updater.php
@@ -22,14 +22,18 @@ class DBUpdater
     function runQueries()
     {
 	$fp = gzopen($this->fileName, 'r');
-    $qry = new DBQuery(true);
+	$qry = new DBQuery(true);
 	while(!feof($fp))
 	{
 	    $line = fgets($fp);
-		if(empty($line)) continue;
-		if(stripos($line, '--') == 0) continue;
-	    $qry->execute($line);
-	    $qry->queryCount(true);   
+	    //empty() won't work because the variable is set, even if the content is NULL
+	    if(strlen(trim($line)) == 0)  continue;
+	    //explicetly check for false instead of 0 because 0 can be a valid location
+	    if(stripos($line, '--') === false)
+	    {
+		$qry->execute($line);
+		$qry->queryCount(true);
+	    }
 	}
 	fclose($fp);
 	return $qry->queryCount();
