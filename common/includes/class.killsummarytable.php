513ad6f405f5ad5f20ff7b41786f53f1df8b9d44
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-05-30 03:03:23 +0000

    Updating killsummarytable classes with more OO and less NOTICEs

--- common/includes/class.killsummarytable.php
+++ common/includes/class.killsummarytable.php
@@ -3,21 +3,36 @@
  * $Date$
  * $Revision$
  * $HeadURL$
- */
+*/
 
 
 class KillSummaryTable
 {
+	protected $klist = null;
+	protected $llist = null;
+	protected $verbose = false;
+	protected $filter = true;
+	protected $inv_plt = array();
+	protected $inv_crp = array();
+	protected $inv_all = array();
+	protected $html = '';
+	protected $monthno = 0;
+	protected $weekno = 0;
+	protected $yearno = 0;
+	protected $startweekno = 0;
+	protected $startDate = 0;
+	protected $endDate = 0;
+	protected $tkcount = 0;
+	protected $tlcount = 0;
+	protected $tkisk = 0;
+	protected $tlisk = 0;
+	protected $system = 0;
+	protected $view = "";
+
 	function KillSummaryTable($klist = null, $llist = null)
 	{
-		$this->klist_ = $klist;
-		$this->llist_ = $llist;
-		$this->verbose_ = false;
-		$this->filter_ = true;
-		$this->inv_plt_ = array();
-		$this->inv_crp_ = array();
-		$this->inv_all_ = array();
-		$this->html_ = '';
+		$this->klist = $klist;
+		$this->llist = $llist;
 	}
 	//! Stub to handle deprecated code.
 	function setBreak($break)
@@ -26,65 +41,66 @@ class KillSummaryTable
 
 	function setVerbose($verbose)
 	{
-		$this->verbose_ = $verbose;
+		$this->verbose = $verbose;
 	}
 
 	function setFilter($filter)
 	{
-		$this->filter_ = $filter;
+		$this->filter = $filter;
 	}
 
 	function setWeek($weekno)
 	{
 		$weekno=intval($weekno);
-		if($weekno <1)  $this->weekno_ = 1;
-		if($weekno >53) $this->weekno_ = 53;
-		else $this->weekno_ = $weekno;
+		if($weekno <1)  $this->weekno = 1;
+		if($weekno >53) $this->weekno = 53;
+		else $this->weekno = $weekno;
 	}
 
 	function setMonth($monthno)
 	{
 		$monthno = intval($monthno);
-		if($monthno < 1) $this->monthno_ = 1;
-		if($monthno > 12) $this->monthno_ = 12;
-		else $this->monthno_ = $monthno;
+		if($monthno < 1) $this->monthno = 1;
+		if($monthno > 12) $this->monthno = 12;
+		else $this->monthno = $monthno;
 	}
 
 	function setYear($yearno)
 	{
-	// 1970-2038 is the allowable range for the timestamp code used
-	// Needs to be revisited in the next 30 years
+		// 1970-2038 is the allowable range for the timestamp code used
+		// Needs to be revisited in the next 30 years
 		$yearno = intval($yearno);
-		if($yearno < 1970) $this->yearno_ = 1970;
-		if($yearno > 2038) $this->yearno_ = 2038;
-		else $this->yearno_ = $yearno;
+		if($yearno < 1970) $this->yearno = 1970;
+		if($yearno > 2038) $this->yearno = 2038;
+		else $this->yearno = $yearno;
 	}
 
 	function setStartWeek($weekno)
 	{
 		$weekno=intval($weekno);
-		if($weekno <1)  $this->startweekno_ = 1;
-		if($weekno >53) $this->startweekno_ = 53;
-		else $this->startweekno_ = $weekno;
+		if($weekno <1)  $this->startweekno = 1;
+		if($weekno >53) $this->startweekno = 53;
+		else $this->startweekno = $weekno;
 	}
 
 	function setStartDate($timestamp)
 	{
-	// Check timestamp is valid before adding
-		if(strtotime($timestamp)) $this->startDate_ = $timestamp;
+		// Check timestamp is valid before adding
+		if(strtotime($timestamp)) $this->startDate = $timestamp;
 	}
 
 	function setEndDate($timestamp)
 	{
-	// Check timestamp is valid before adding
-		if(strtotime($timestamp)) $this->endDate_ = $timestamp;
+		// Check timestamp is valid before adding
+		if(strtotime($timestamp)) $this->endDate = $timestamp;
 	}
 
 	// Return SQL for date filter using currently set date limits
 	function setDateFilter()
 	{
-		$qstartdate = makeStartDate($this->weekno_, $this->yearno_, $this->monthno_, $this->startweekno_, $this->startDate_);
-		$qenddate = makeEndDate($this->weekno_, $this->yearno_, $this->monthno_, $this->endDate_);
+		$sql = '';
+		$qstartdate = makeStartDate($this->weekno, $this->yearno, $this->monthno, $this->startweekno, $this->startDate);
+		$qenddate = makeEndDate($this->weekno, $this->yearno, $this->monthno, $this->endDate);
 		if($qstartdate) $sql .= " kll.kll_timestamp >= '".gmdate('Y-m-d H:i',$qstartdate)."' ";
 		if($qstartdate && $qenddate) $sql .= " AND ";
 		if($qenddate) $sql .= " kll.kll_timestamp <= '".gmdate('Y-m-d H:i',$qenddate)."' ";
@@ -93,139 +109,135 @@ class KillSummaryTable
 
 	function getTotalKills()
 	{
-		return $this->tkcount_;
+		return $this->tkcount;
 	}
 
 	function getTotalLosses()
 	{
-		return $this->tlcount_;
+		return $this->tlcount;
 	}
 
 	function getTotalKillPoints()
 	{
-		return $this->tkpoints_;
+		return $this->tkpoints;
 	}
 
 	function getTotalLossPoints()
 	{
-		return $this->tlpoints_;
+		return $this->tlpoints;
 	}
 
 	function getTotalKillISK()
 	{
-		return $this->tkisk_;
+		return $this->tkisk;
 	}
 
 	function getTotalLossISK()
 	{
-		return $this->tlisk_;
+		return $this->tlisk;
 	}
 
 	function setView($string)
 	{
-		$this->view_ = $string;
+		$this->view = $string;
 	}
 
 	function addInvolvedPilot($pilot)
 	{
-		if(is_numeric($pilot)) $this->inv_plt_[] = $pilot;
-		else $this->inv_plt_[] = $pilot->getID();
+		if(is_numeric($pilot)) $this->inv_plt[] = $pilot;
+		else $this->inv_plt[] = $pilot->getID();
 	}
 
 	function addInvolvedCorp($corp)
 	{
-		if(is_numeric($corp)) $this->inv_crp_[] = $corp;
-		else $this->inv_crp_[] = $corp->getID();
+		if(is_numeric($corp)) $this->inv_crp[] = $corp;
+		else $this->inv_crp[] = $corp->getID();
 	}
 
 	function addInvolvedAlliance($alliance)
 	{
-		if(is_numeric($alliance)) $this->inv_all_[] = $alliance;
-		else $this->inv_all_[] = $alliance->getID();
+		if(is_numeric($alliance)) $this->inv_all[] = $alliance;
+		else $this->inv_all[] = $alliance->getID();
 	}
 
 	function setSystem($system)
 	{
-		if(is_numeric($system)) $this->system_[] = $system;
-		else $this->system_[] = $system->getID();
+		if(is_numeric($system)) $this->system[] = $system;
+		else $this->system[] = $system->getID();
 	}
 
 	// do it faster, baby!
 	function getkills()
 	{
-		if ($this->mixedinvolved_)
+		if($this->setDateFilter() == "" && empty($this->system))
 		{
-			echo 'mode not supported<br>';
-			exit;
-		}
-		if($this->setDateFilter() == "" && empty($this->system_))
-		{
-			if( count($this->inv_all_) == 1 && !$this->inv_crp_ && !$this->inv_plt_)
+			if( count($this->inv_all) == 1 && !$this->inv_crp && !$this->inv_plt)
 			{
-				$allsum = new allianceSummary($this->inv_all_[0]);
+				$allsum = new allianceSummary($this->inv_all[0]);
 				$summary = $allsum->getSummary();
 				foreach($summary as $key => $row)
 				{
-					$this->entry_[$row['class_name']] = array('id' => $key,
+					$this->entry[$row['class_name']] = array('id' => $key,
 						'kills' => $row['killcount'], 'kills_isk' => $row['killisk'],
 						'losses' => $row['losscount'], 'losses_isk' => $row['lossisk']);
 
-					$this->tkcount_ += $row['killcount'];
-					$this->tkisk_ += $row['killisk'];
-					$this->tlcount_ += $row['losscount'];
-					$this->tlisk_ += $row['lossisk'];
+					$this->tkcount += $row['killcount'];
+					$this->tkisk += $row['killisk'];
+					$this->tlcount += $row['losscount'];
+					$this->tlisk += $row['lossisk'];
 				}
 				return;
 			}
-			elseif( count($this->inv_crp_) == 1 && !$this->inv_all_ && !$this->inv_plt_)
+			elseif( count($this->inv_crp) == 1 && !$this->inv_all && !$this->inv_plt)
 			{
-				$crpsum = new corpSummary($this->inv_crp_[0]);
+				$crpsum = new corpSummary($this->inv_crp[0]);
 				$summary = $crpsum->getSummary();
 				foreach($summary as $key => $row)
 				{
-					$this->entry_[$row['class_name']] = array('id' => $key,
+					$this->entry[$row['class_name']] = array('id' => $key,
 						'kills' => $row['killcount'], 'kills_isk' => $row['killisk'],
 						'losses' => $row['losscount'], 'losses_isk' => $row['lossisk']);
 
-					$this->tkcount_ += $row['killcount'];
-					$this->tkisk_ += $row['killisk'];
-					$this->tlcount_ += $row['losscount'];
-					$this->tlisk_ += $row['lossisk'];
+					$this->tkcount += $row['killcount'];
+					$this->tkisk += $row['killisk'];
+					$this->tlcount += $row['losscount'];
+					$this->tlisk += $row['lossisk'];
 				}
 				return;
 			}
-			elseif( count($this->inv_plt_) == 1 && !$this->inv_all_ && !$this->inv_crp_)
+			elseif( count($this->inv_plt) == 1 && !$this->inv_all && !$this->inv_crp)
 			{
-				$pltsum = new pilotSummary($this->inv_plt_[0]);
+				$pltsum = new pilotSummary($this->inv_plt[0]);
 				$summary = $pltsum->getSummary();
 				foreach($summary as $key => $row)
 				{
-					$this->entry_[$row['class_name']] = array('id' => $key,
+					$this->entry[$row['class_name']] = array('id' => $key,
 						'kills' => $row['killcount'], 'kills_isk' => $row['killisk'],
 						'losses' => $row['losscount'], 'losses_isk' => $row['lossisk']);
 
-					$this->tkcount_ += $row['killcount'];
-					$this->tkisk_ += $row['killisk'];
-					$this->tlcount_ += $row['losscount'];
-					$this->tlisk_ += $row['lossisk'];
+					$this->tkcount += $row['killcount'];
+					$this->tkisk += $row['killisk'];
+					$this->tlcount += $row['losscount'];
+					$this->tlisk += $row['lossisk'];
 				}
 				return;
 			}
 		}
 
-		$this->entry_ = array();
+		$this->entry = array();
 		// as there is no way to do this elegantly in sql
 		// i'll keep it in php
 		$sql = "select scl_id, scl_class from kb3_ship_classes
                where scl_class not in ('Drone','Unknown') order by scl_class";
-		$startdate = makeStartDate($this->weekno_, $this->yearno_, $this->monthno_, $this->startweekno_, $this->startDate_);
-		$enddate = makeEndDate($this->weekno_, $this->yearno_, $this->monthno_, $this->endDate_);
+		$startdate = makeStartDate($this->weekno, $this->yearno, $this->monthno, $this->startweekno, $this->startDate);
+		$enddate = makeEndDate($this->weekno, $this->yearno, $this->monthno, $this->endDate);
 
-		$qry = DBFactory::getDBQuery();;
+		$qry = DBFactory::getDBQuery();
+		;
 		$qry->execute($sql);
 		while ($row = $qry->getRow())
 		{
-			$this->entry_[$row['scl_class']] = array('id' => $row['scl_id'],
+			$this->entry[$row['scl_class']] = array('id' => $row['scl_id'],
 				'kills' => 0, 'kills_isk' => 0,
 				'losses' => 0, 'losses_isk' => 0);
 		}
@@ -237,26 +249,26 @@ class KillSummaryTable
 
 
 		$sqlop = " WHERE ";
-		if ($this->inv_all_)
+		if ($this->inv_all)
 		{
 			$sql .= " INNER JOIN kb3_inv_all inv ON (inv.ina_kll_id = kll.kll_id)
-				".$sqlop."inv.ina_all_id in (".implode(',', $this->inv_all_)." ) ";
+				".$sqlop."inv.ina_all_id in (".implode(',', $this->inv_all)." ) ";
 			if($startdate) $sql .=" AND inv.ina_timestamp >= '".gmdate('Y-m-d H:i',$startdate)."' ";
 			if($enddate) $sql .=" AND inv.ina_timestamp <= '".gmdate('Y-m-d H:i',$enddate)."' ";
 			$sqlop = " AND ";
 		}
-		elseif ($this->inv_crp_)
+		elseif ($this->inv_crp)
 		{
 			$sql .= " INNER JOIN kb3_inv_crp inv ON (inv.inc_kll_id = kll.kll_id)
-				".$sqlop."inv.inc_crp_id in (".implode(',', $this->inv_crp_)." ) ";
+				".$sqlop."inv.inc_crp_id in (".implode(',', $this->inv_crp)." ) ";
 			if($startdate) $sql .=" AND inv.inc_timestamp >= '".gmdate('Y-m-d H:i',$startdate)."' ";
 			if($enddate) $sql .=" AND inv.inc_timestamp <= '".gmdate('Y-m-d H:i',$enddate)."' ";
 			$sqlop = " AND ";
 		}
-		elseif ($this->inv_plt_)
+		elseif ($this->inv_plt)
 		{
 			$sql .= " INNER JOIN kb3_inv_detail inv ON (inv.ind_kll_id = kll.kll_id)
-					".$sqlop."inv.ind_plt_id in (".implode(',', $this->inv_plt_)." ) ";
+					".$sqlop."inv.ind_plt_id in (".implode(',', $this->inv_plt)." ) ";
 			if($startdate) $sql .=" AND inv.ind_timestamp >= '".gmdate('Y-m-d H:i',$startdate)."' ";
 			if($enddate) $sql .=" AND inv.ind_timestamp <= '".gmdate('Y-m-d H:i',$enddate)."' ";
 			$sqlop = " AND ";
@@ -274,22 +286,23 @@ class KillSummaryTable
 				$sqlop = " AND ";
 			}
 		}
-			if($this->system_)
-			{
-				$sql .= $sqlop." kll.kll_system_id = ".join(',', $this->system_)." ";
-				$sqlop = " AND ";
-			}
+		if($this->system)
+		{
+			$sql .= $sqlop." kll.kll_system_id = ".join(',', $this->system)." ";
+			$sqlop = " AND ";
+		}
 
 		$sql .= 'GROUP BY scl_class order by scl_class';
 
-		$qry = DBFactory::getDBQuery();;
+		$qry = DBFactory::getDBQuery();
+		;
 		$qry->execute($sql);
 		while ($row = $qry->getRow())
 		{
-			$this->entry_[$row['scl_class']]['kills'] = $row['knb'];
-			$this->entry_[$row['scl_class']]['kills_isk'] = $row['kisk'];
-			$this->tkcount_ += $row['knb'];
-			$this->tkisk_ += $row['kisk'];
+			$this->entry[$row['scl_class']]['kills'] = $row['knb'];
+			$this->entry[$row['scl_class']]['kills_isk'] = $row['kisk'];
+			$this->tkcount += $row['knb'];
+			$this->tkisk += $row['kisk'];
 		}
 
 
@@ -305,46 +318,47 @@ class KillSummaryTable
 			$sqlop = " AND ";
 		}
 
-		if ($this->inv_all_)
+		if ($this->inv_all)
 		{
-			$sql .= $sqlop.' kll.kll_all_id IN ( '.implode(',', $this->inv_all_).' ) ';
-			$sql .= 'AND EXISTS (SELECT 1 FROM kb3_inv_all ina WHERE kll.kll_id = ina_kll_id AND ina.ina_all_id NOT IN ( '.implode(',', $this->inv_all_).' ) limit 0,1) ';
+			$sql .= $sqlop.' kll.kll_all_id IN ( '.implode(',', $this->inv_all).' ) ';
+			$sql .= 'AND EXISTS (SELECT 1 FROM kb3_inv_all ina WHERE kll.kll_id = ina_kll_id AND ina.ina_all_id NOT IN ( '.implode(',', $this->inv_all).' ) limit 0,1) ';
 			$sqlop = " AND ";
 		}
-		elseif ($this->inv_crp_)
+		elseif ($this->inv_crp)
 		{
-			$sql .= $sqlop.' kll.kll_crp_id IN ( '.implode(',', $this->inv_crp_).' ) ';
-			$sql .= 'AND EXISTS (SELECT 1 FROM kb3_inv_crp inc WHERE kll.kll_id = inc_kll_id AND inc.inc_crp_id NOT IN ( '.implode(',', $this->inv_crp_).' ) limit 0,1) ';
+			$sql .= $sqlop.' kll.kll_crp_id IN ( '.implode(',', $this->inv_crp).' ) ';
+			$sql .= 'AND EXISTS (SELECT 1 FROM kb3_inv_crp inc WHERE kll.kll_id = inc_kll_id AND inc.inc_crp_id NOT IN ( '.implode(',', $this->inv_crp).' ) limit 0,1) ';
 			$sqlop = " AND ";
 		}
-		elseif ($this->inv_plt_)
+		elseif ($this->inv_plt)
 		{
-			$sql .= $sqlop.' kll.kll_victim_id IN ( '.implode(',', $this->inv_plt_).' ) ';
-			$sql .= ' AND EXISTS (SELECT 1 FROM kb3_inv_detail ind WHERE kll.kll_id = ind_kll_id AND ind.ind_plt_id NOT IN ( '.implode(',', $this->inv_plt_).' ) limit 0,1) ';
+			$sql .= $sqlop.' kll.kll_victim_id IN ( '.implode(',', $this->inv_plt).' ) ';
+			$sql .= ' AND EXISTS (SELECT 1 FROM kb3_inv_detail ind WHERE kll.kll_id = ind_kll_id AND ind.ind_plt_id NOT IN ( '.implode(',', $this->inv_plt).' ) limit 0,1) ';
 			$sqlop = " AND ";
 		}
-		if($this->system_)
+		if($this->system)
 		{
-			$sql .= $sqlop." kll.kll_system_id = ".join(',', $this->system_)." ";
+			$sql .= $sqlop." kll.kll_system_id = ".join(',', $this->system)." ";
 		}
 		$sql .= 'GROUP BY scl_class order by scl_class';
 
-		$qry = DBFactory::getDBQuery();;
+		$qry = DBFactory::getDBQuery();
+		;
 		$qry->execute($sql);
 		while ($row = $qry->getRow())
 		{
-			$this->entry_[$row['scl_class']]['losses'] = $row['lnb'];
-			$this->entry_[$row['scl_class']]['losses_isk'] =  $row['lisk'];
+			$this->entry[$row['scl_class']]['losses'] = $row['lnb'];
+			$this->entry[$row['scl_class']]['losses_isk'] =  $row['lisk'];
 
-			$this->tlcount_ += $row['lnb'];
-			$this->tlisk_ += $row['lisk'];
+			$this->tlcount += $row['lnb'];
+			$this->tlisk += $row['lisk'];
 		}
 	}
 
 	function generate()
 	{
-		if($this->html_ != '') return $this->html_;
-		if ($this->klist_)
+		if($this->html != '') return $this->html;
+		if ($this->klist)
 		{
 			$entry = array();
 			// build array
@@ -353,7 +367,7 @@ class KillSummaryTable
                    where scl_class not in ( 'Drone', 'Unknown' )
                   order by scl_class";
 
-			$qry = DBFactory::getDBQuery();;
+			$qry = DBFactory::getDBQuery();
 			$qry->execute($sql) or die($qry->getErrorMsg());
 			while ($row = $qry->getRow())
 			{
@@ -370,28 +384,28 @@ class KillSummaryTable
 				$entry[$shipclass->getName()]['losses_isk'] = 0;
 			}
 			// kills
-			while ($kill = $this->klist_->getKill())
+			while ($kill = $this->klist->getKill())
 			{
 				$classname = $kill->getVictimShipClassName();
 				$entry[$classname]['kills']++;
 				$entry[$classname]['kills_isk'] += $kill->getISKLoss();
-				$this->tkcount_++;
-				$this->tkisk_ += $kill->getISKLoss();
+				$this->tkcount++;
+				$this->tkisk += $kill->getISKLoss();
 			}
 			// losses
-			while ($kill = $this->llist_->getKill())
+			while ($kill = $this->llist->getKill())
 			{
 				$classname = $kill->getVictimShipClassName();
 				$entry[$classname]['losses']++;
 				$entry[$classname]['losses_isk'] += $kill->getISKLoss();
-				$this->tlcount_++;
-				$this->tlisk_ += $kill->getISKLoss();
+				$this->tlcount++;
+				$this->tlisk += $kill->getISKLoss();
 			}
 		}
 		else
 		{
 			$this->getkills();
-			$entry = &$this->entry_;
+			$entry = &$this->entry;
 		}
 
 		$odd = false;
@@ -403,16 +417,15 @@ class KillSummaryTable
 		foreach ($entry as $k => $v)
 		{
 			if($v['id'] == 3) continue;
-			if($count && $this->break_ && $count%$this->break_ == 0) $v['break'] = 1;
-			else $v['break'] = 0;
-			if($_GET['scl_id'] && $_GET['scl_id'] == $v['id']) $v['hl'] = 1;
+			$v['break'] = 0;
+			if(isset($_GET['scl_id']) && $_GET['scl_id'] == $v['id']) $v['hl'] = 1;
 			else $v['hl'] = 0;
 			$qrystring = preg_replace("/&scl_id=([0-9]?[0-9])/", "", $_SERVER['QUERY_STRING']);
 			$qrystring = preg_replace("/&page=([0-9]?[0-9])/", "", $qrystring);
 			$qrystring = preg_replace("/&/", "&amp;", $qrystring);
-			if ($this->view_)
+			if ($this->view)
 			{
-				$qrystring .= '&amp;view='.$this->view_;
+				$qrystring .= '&amp;view='.$this->view;
 			}
 			$v['qry'] = $qrystring;
 			$v['kisk'] = round($v['kills_isk']/1000000, 2);
@@ -421,46 +434,39 @@ class KillSummaryTable
 
 			$summary[] = $v;
 
-			$this->tkcount_ += $kcount;
-			$this->tkisk_ += $kisk;
-			$this->tkpoints_ += $kpoints;
-			$this->tlcount_ += $lcount;
-			$this->tlisk_ += $lisk;
-			$this->tlpoints_ += $lpoints;
 			$count++;
 
 		}
 		global $smarty;
 		$smarty->assign('summary', $summary);
 		$smarty->assign('count', $num);
-		$smarty->assign('verbose', $this->verbose_);
-		$smarty->assign('filter', $this->filter_);
+		$smarty->assign('verbose', $this->verbose);
+		$smarty->assign('filter', $this->filter);
 		$smarty->assign('losses', 1);
 
 		if (config::get('summarytable_summary'))
 		{
 			$smarty->assign('summarysummary', 1);
 			if (config::get('summarytable_efficiency'))
-				$smarty->assign('efficiency', round($this->tkisk_ / (($this->tkisk_ + $this->tlisk_) == 0 ? 1 : ($this->tkisk_ + $this->tlisk_)) * 100, 2));
+				$smarty->assign('efficiency', round($this->tkisk / (($this->tkisk + $this->tlisk) == 0 ? 1 : ($this->tkisk + $this->tlisk)) * 100, 2));
 			else $smarty->assign('efficiency', 0);
-			$smarty->assign('kiskB', round($this->tkisk_/1000000000, 2));
-			$smarty->assign('liskB', round($this->tlisk_/1000000000, 2));
-			$smarty->assign('kiskM', round($this->tkisk_/1000000, 2));
-			$smarty->assign('liskM', round($this->tlisk_/1000000, 2));
-			$smarty->assign('kcount', $this->tkcount_);
-			$smarty->assign('lcount', $this->tlcount_);
+			$smarty->assign('kiskB', round($this->tkisk/1000000000, 2));
+			$smarty->assign('liskB', round($this->tlisk/1000000000, 2));
+			$smarty->assign('kiskM', round($this->tkisk/1000000, 2));
+			$smarty->assign('liskM', round($this->tlisk/1000000, 2));
+			$smarty->assign('kcount', $this->tkcount);
+			$smarty->assign('lcount', $this->tlcount);
 		}
 
-		if ($_GET['scl_id'] != "")
+		if (!empty($_GET['scl_id']))
 		{
 			$qrystring = preg_replace("/&scl_id=([0-9]?[0-9])/", "", '?'.$_SERVER['QUERY_STRING']);
 			$qrystring = preg_replace("/&/", "&amp;", $qrystring);
 			$smarty->assign('clearfilter',$qrystring);
 		}
 
-		$this->html_ = $smarty->fetch(get_tpl('summarytable'));
+		$this->html = $smarty->fetch(get_tpl('summarytable'));
 
-		return $this->html_;
+		return $this->html;
 	}
 }
-?>
--- common/includes/class.killsummarytable.public.php
+++ common/includes/class.killsummarytable.public.php
@@ -10,37 +10,28 @@ class KillSummaryTablePublic extends KillSummaryTable
 {
     function KillSummaryTablePublic($klist = null)
     {
-        $this->klist_ = $klist;
-        $this->verbose_ = true;
-        $this->filter_ = true;
-        $this->inv_crp_ = array();
-        $this->inv_all_ = array();
+        $this->klist = $klist;
+		$this->verbose = true;
     }
 
     // do it faster, baby!
     function getkills()
     {
-        if ($this->mixedinvolved_)
-        {
-            echo 'mode not supported<br>';
-            exit;
-        }
-
-        $this->entry_ = array();
+        $this->entry = array();
         // as there is no way to do this elegant in sql
         // i'll keep it in php
         $sql = "select scl_id, scl_class from kb3_ship_classes
                where scl_class not in ('Drone','Unknown') order by scl_class";
 
-        $qry = DBFactory::getDBQuery();;
+        $qry = DBFactory::getDBQuery();
         $qry->execute($sql);
         while ($row = $qry->getRow())
         {
-            $this->entry_[$row['scl_class']] = array('id' => $row['scl_id'],
+            $this->entry[$row['scl_class']] = array('id' => $row['scl_id'],
                                                      'kills' => 0, 'kills_isk' => 0);
         }
-		$startdate = makeStartDate($this->weekno_, $this->yearno_, $this->monthno_, $this->startweekno_, $this->startDate_);
-		$enddate = makeEndDate($this->weekno_, $this->yearno_, $this->monthno_, $this->endDate_);
+		$startdate = makeStartDate($this->weekno, $this->yearno, $this->monthno, $this->startweekno, $this->startDate);
+		$enddate = makeEndDate($this->weekno, $this->yearno, $this->monthno, $this->endDate);
 
         $sql = 'SELECT count(kll.kll_id) AS knb, scl_id, scl_class,';
 
@@ -49,18 +40,18 @@ class KillSummaryTablePublic extends KillSummaryTable
 		
         $sql .= ' INNER JOIN kb3_ship_classes scl ON ( scl.scl_id = shp.shp_class )';
 
-        if ($this->inv_crp_)
+        if ($this->inv_crp)
         {
-            $sql .= ' inner join kb3_inv_detail ind on ( ind.ind_crp_id in ( '.join(',', $this->inv_crp_).' ) and kll.kll_id = ind.ind_kll_id ) ';
+            $sql .= ' inner join kb3_inv_detail ind on ( ind.ind_crp_id in ( '.join(',', $this->inv_crp).' ) and kll.kll_id = ind.ind_kll_id ) ';
         }
-        elseif ($this->inv_all_)
+        elseif ($this->inv_all)
         {
-            $sql .= ' inner join kb3_inv_detail ind on ( ind.ind_all_id in ( '.join(',', $this->inv_all_).' ) and kll.kll_id = ind.ind_kll_id ) ';
+            $sql .= ' inner join kb3_inv_detail ind on ( ind.ind_all_id in ( '.join(',', $this->inv_all).' ) and kll.kll_id = ind.ind_kll_id ) ';
         }
 		$sqlop = " WHERE ";
-		if($this->system_)
+		if($this->system)
 		{
-			$sql .= $sqlop." kll.kll_system_id in ".join(',', $this->system_)." ";
+			$sql .= $sqlop." kll.kll_system_id in ".join(',', $this->system)." ";
 		}
 		if($startdate)
 		{
@@ -69,23 +60,23 @@ class KillSummaryTablePublic extends KillSummaryTable
 		}
 		if($enddate) $sql .= $sqlop." kll.kll_timestamp <= '".gmdate('Y-m-d H:i',$enddate)."' ";
         $sql .= 'GROUP BY shp.shp_class';
-		if($this->inv_crp_ || $this->inv_all_) $sql .= ', kll.kll_id';
+		if($this->inv_crp || $this->inv_all) $sql .= ', kll.kll_id';
 		$sql .= ' order by scl.scl_class';
 
-        $qry = DBFactory::getDBQuery();;
+        $qry = DBFactory::getDBQuery();
         $qry->execute($sql);
         while ($row = $qry->getRow())
         {
-            $this->entry_[$row['scl_class']]['kills'] = $row['knb'];
-            $this->entry_[$row['scl_class']]['kills_isk'] = $row['kisk'];
-            $this->tkcount_ += $row['knb'];
-            $this->tkisk_ += $row['kisk'];
+            $this->entry[$row['scl_class']]['kills'] = $row['knb'];
+            $this->entry[$row['scl_class']]['kills_isk'] = $row['kisk'];
+            $this->tkcount += $row['knb'];
+            $this->tkisk += $row['kisk'];
         }
     }
 
     function generate()
     {
-        if ($this->klist_)
+        if ($this->klist)
         {
             $entry = array();
             // build array
@@ -94,7 +85,7 @@ class KillSummaryTablePublic extends KillSummaryTable
                    where scl_class not in ( 'Drone', 'Unknown' )
                   order by scl_class";
 
-            $qry = DBFactory::getDBQuery();;
+            $qry = DBFactory::getDBQuery();
             $qry->execute($sql) or die($qry->getErrorMsg());
             while ($row = $qry->getRow())
             {
@@ -109,19 +100,19 @@ class KillSummaryTablePublic extends KillSummaryTable
                 $entry[$shipclass->getName()]['kills_isk'] = 0;
             }
 
-            while ($kill = $this->klist_->getKill())
+            while ($kill = $this->klist->getKill())
             {
                 $classname = $kill->getVictimShipClassName();
                 $entry[$classname]['kills']++;
                 $entry[$classname]['kills_isk'] += $kill->getISKLoss();
-                $this->tkcount_++;
-                $this->tkisk_ += $kill->getISKLoss();
+                $this->tkcount++;
+                $this->tkisk += $kill->getISKLoss();
             }
         }
         else
         {
             $this->getkills();
-            $entry = &$this->entry_;
+            $entry = &$this->entry;
         }
 
         $odd = false;
@@ -133,16 +124,15 @@ class KillSummaryTablePublic extends KillSummaryTable
 		foreach ($entry as $k => $v)
 		{
 			if($v['id'] == 3) continue;
-			if($count && $this->break_ && $count%$this->break_ == 0) $v['break'] = 1;
-			else $v['break'] = 0;
+			$v['break'] = 0;
 			if($_GET['scl_id'] && $_GET['scl_id'] == $v['id']) $v['hl'] = 1;
 			else $v['hl'] = 0;
 			$qrystring = preg_replace("/&scl_id=([0-9]?[0-9])/", "", $_SERVER['QUERY_STRING']);
 			$qrystring = preg_replace("/&page=([0-9]?[0-9])/", "", $qrystring);
 			$qrystring = preg_replace("/&/", "&amp;", $qrystring);
-			if ($this->view_)
+			if ($this->view)
 			{
-				$qrystring .= '&view='.$this->view_;
+				$qrystring .= '&view='.$this->view;
 			}
 			$v['url'] = $querystring;
 			$v['kisk'] = round($v['kills_isk']/1000000, 2);
@@ -150,27 +140,27 @@ class KillSummaryTablePublic extends KillSummaryTable
 
 			$summary[] = $v;
 
-			$this->tkcount_ += $kcount;
-			$this->tkisk_ += $kisk;
-			$this->tkpoints_ += $kpoints;
+			$this->tkcount += $kcount;
+			$this->tkisk += $kisk;
+			$this->tkpoints += $kpoints;
 			$count++;
 		}
 		global $smarty;
 		$smarty->assign('summary', $summary);
 		$smarty->assign('count', $num);
-		$smarty->assign('verbose', $this->verbose_);
-		$smarty->assign('filter', $this->filter_);
+		$smarty->assign('verbose', $this->verbose);
+		$smarty->assign('filter', $this->filter);
 
 		if (config::get('summarytable_summary'))
 		{
 			$smarty->assign('summarysummary', 1);
 			$smarty->assign('efficiency', 0);
-			$smarty->assign('kiskB', round($this->tkisk_/1000000000, 2));
-			$smarty->assign('kiskM', round($this->tkisk_/1000000, 2));
-			$smarty->assign('kcount', $this->tkcount_);
+			$smarty->assign('kiskB', round($this->tkisk/1000000000, 2));
+			$smarty->assign('kiskM', round($this->tkisk/1000000, 2));
+			$smarty->assign('kcount', $this->tkcount);
 		}
 
-		if ($_GET['scl_id'] != "")
+		if (!empty($_GET['scl_id']))
 		{
 			$qrystring = preg_replace("/&scl_id=([0-9]?[0-9])/", "", '?'.$_SERVER['QUERY_STRING']);
 			$qrystring = preg_replace("/&/", "&amp;", $qrystring);
@@ -182,4 +172,3 @@ class KillSummaryTablePublic extends KillSummaryTable
         return $html;
     }
 }
-?>
