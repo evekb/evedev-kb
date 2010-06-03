2343483cd762e52d2acee4c6e06f9dcd8781e4be
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-06-03 13:11:16 +0000

    Killlog relies on the corp and alliance classes to update the external id. (At some point should perhaps move the api code out of the alliance and corp classes and into the api classes?)

--- common/includes/api/class.killlog.php
+++ common/includes/api/class.killlog.php
@@ -392,10 +392,10 @@ class API_KillLog
                 }
 
 				// update crp_external_id
-				API_Helpers::Update_CorpID($this->corporation_, $this->corporationID_);
+				//API_Helpers::Update_CorpID($this->corporation_, $this->corporationID_);
 				// update all_external_id
-				if ($this->allianceID_ != 0)
-					API_Helpers::Update_AllianceID($this->alliance_, $this->allianceID_);
+				//if ($this->allianceID_ != 0)
+				//	API_Helpers::Update_AllianceID($this->alliance_, $this->allianceID_);
 
                 // set victim corp and alliance for FF check
                 $this->valliance_ = $this->alliance_;
