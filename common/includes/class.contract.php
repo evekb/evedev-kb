318c106c417ff2555885503423ac1bfc97aac3e1
mastergamer0168 <mastergamer0168@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-06-04 20:19:36 +0000

    Fixing quote issue with new campaigns.

--- common/includes/class.contract.php
+++ common/includes/class.contract.php
@@ -241,7 +241,7 @@ class Contract
 		$qry = DBFactory::getDBQuery();;
 		if ($type == "campaign") $campaign = 1;
 		else $campaign = 0;
-		if ($enddate != "") $enddate = "'".$enddate." 23:59:59'";
+		if ($enddate != "") $enddate = "".$enddate." 23:59:59";
 		else $enddate = "null";
 
 		if (!$this->ctr_id_)
@@ -249,7 +249,7 @@ class Contract
 			$sql = "insert into kb3_contracts values ( null, '".$qry->escape($name)."',
                                                    '".KB_SITE."', ".$campaign.",
 						   '".$qry->escape($startdate)." 00:00:00',
-						   ".$qry->escape($enddate).",
+						   '".$qry->escape($enddate)."',
 						   '".$qry->escape($comment)."' )";
 			$qry->execute($sql) or die($qry->getErrorMsg());
 			$this->ctr_id_ = $qry->getInsertID();
@@ -258,7 +258,7 @@ class Contract
 		{
 			$sql = "update kb3_contracts set ctr_name = '".$qry->escape($name)."',
 			                 ctr_started = '".$qry->escape($startdate)." 00:00:00',
-					 ctr_ended = ".$qry->escape($enddate).",
+					 ctr_ended = '".$qry->escape($enddate)."',
 					 ctr_comment = '" . $qry->escape($comment) . "'
 				     where ctr_id = ".$this->ctr_id_;
 			$qry->execute($sql) or die($qry->getErrorMsg());
