68c013a385e82061bc39f9dcefe7b7670d087f1b
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-06-20 12:23:15 +0000

    Check if update has been performed before attempting to update contract table.

--- update/015/update.php
+++ update/015/update.php
@@ -7,7 +7,8 @@ function update015()
 	{
 		$qry = new DBQuery(true);
 		$sql = "ALTER TABLE `kb3_contracts` ADD `ctr_comment` VARCHAR( 255 ) NULL DEFAULT NULL ";
-		$qry->execute($sql);
+		$qry->execute("SHOW COLUMNS FROM kb3_contracts LIKE 'ctr_comment'");
+		if(!$qry->recordCount()) $qry->execute($sql);
 
 		config::set("DBUpdate", "015");
 		$qry->execute("INSERT INTO kb3_config (cfg_site, cfg_key, cfg_value) SELECT cfg_site, 'DBUpdate', '015' FROM kb3_config GROUP BY cfg_site ON DUPLICATE KEY UPDATE cfg_value = '015'");
