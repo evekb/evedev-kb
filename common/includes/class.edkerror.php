835d2f5aec8bbc30fd8645e035faa7723b5be8f2
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-08-28 07:38:17 +0000

    Error handler does not try to get config settings before config class is loaded.

--- common/includes/class.edkerror.php
+++ common/includes/class.edkerror.php
@@ -8,6 +8,7 @@
 
 class EDKError
 {
+	public static $errors = array();
 	public static function handler  ( $errno  , $errstr  , $errfile, $errline, $errcontext)
 	{
 		$output = '';
@@ -51,7 +52,7 @@ class EDKError
 		
 		if (ini_get('log_errors') && (error_reporting() & $errno))
 			error_log(sprintf("PHP %s:  %s in %s on line %d", $errors, $errstr, $errfile, $errline));
-		if(config::get('cfg_log'))
+		if(class_exists('config') && config::get('cfg_log'))
 		{
 			error_log(sprintf("PHP %s %s:  %s in %s on line %d\n", gmdate("Y-m-d H:i:s"), $errors, $errstr, $errfile, $errline), 3, $logfile);
 			if(filesize($logfile) > 1024*1024*10)
@@ -61,6 +62,7 @@ class EDKError
 			}
 		}
 		echo $output;
+		self::$errors[] = $output;
 		return true;
 	}
 }
