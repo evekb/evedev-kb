4a139091fe246d5219bd50113a5b465ccef11a42
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-09-01 13:51:43 +0000

    Use distinct for mixed involved pilot/corp/alliance

--- common/includes/class.killlist.php
+++ common/includes/class.killlist.php
@@ -252,7 +252,7 @@ class KillList
 			if (!count($this->groupby_))
 			{
 				$this->sqltop_ = 'SELECT ';
-				if(count($this->inv_all_) > 1 || count($this->inv_crp_) > 1 || count($this->inv_plt_) > 1)
+				if(count($this->inv_all_) + count($this->inv_crp_) + count($this->inv_plt_) > 1)
 					$this->sqltop_ .= ' DISTINCT ';
 				$this->sqltop_ .= ' kll.kll_id, kll.kll_timestamp, kll.kll_external_id,
 							plt.plt_name, crp.crp_name, crp.crp_id,
