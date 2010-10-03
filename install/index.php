e606525a99ec158df367f9375d71050a91deedc1
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-10-03 01:07:30 +0000

    Move installation smarty cache to killboard cache. Add a check for existing and writeable cache directory before Smarty runs.

--- install/index.php
+++ install/index.php
@@ -8,9 +8,47 @@ include_once('../common/smarty/Smarty.class.php');
 $smarty = new Smarty();
 $smarty->template_dir = './templates';
 //as much as i don't want it, the compiled templates directory is needed
-$smarty->compile_dir = './templates_c';
-if(!is_writeable('./templates_c')) chmod('./templates_c', 755);
-
+$smarty->compile_dir = '../cache/templates_c';
+$cacheWriteable = true;
+if(!file_exists('../cache'))
+{
+	// If we can create it then it's writeable.
+	if(!mkdir('../cache')) $cacheWriteable = false;
+	else if(!mkdir('../cache/templates_c')) $cacheWriteable = false;
+}
+else if(!is_writeable('../cache'))
+{
+	if(!chmod('../cache', 755)) $cacheWriteable = false;
+	else if(!mkdir('../cache/templates_c')) $cacheWriteable = false;
+}
+else if(!file_exists('../cache/templates_c'))
+{
+	// If we can create it then it's writeable.
+	if(!mkdir('../cache/templates_c')) $cacheWriteable = false;
+}
+else if(!is_writeable('../cache/templates_c'))
+{
+	if(chmod('../cache/templates_c', 755)) $cacheWriteable = false;
+}
+if(!cacheWriteable)
+{
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF8">
+	<title>EVE Development Network Killboard Install Script</title>
+	<link rel="stylesheet" type="text/css" href="common.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body style="background-color:#222222; height: 100%">
+		<p>The cache directory is not writeable. [killboard]/cache/ must exist and be writeable to install the killboard.</p>
+		<p>Make sure the cache directory exists and permissions are recursively set to 755 or 777.
+    </body>
+</html>
+<?php
+die;
+}
 $installrunning = true;
 session_start();
 
