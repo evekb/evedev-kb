a6d9256282b8dff014f13f361d7700b4f79fa2ab
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-11-14 01:19:27 +0000

    Main index.php checks php version. Error handler shows strict and user notices. Include paths commented out as this is handled by the class loader (which so far is less prone to errors on strange system setups).

--- index.php
+++ index.php
@@ -4,18 +4,23 @@
 * Request forwarder, look at common/index.php for the action and license
 */
 
+// Enable custom error handling.
 require_once ('common/includes/class.edkerror.php');
 
-set_error_handler(array('EDKError', 'handler'), E_ALL & ~(E_STRICT | E_NOTICE | E_USER_NOTICE) );
-@error_reporting(E_ERROR);
+set_error_handler(array('EDKError', 'handler'), E_ALL & ~(E_NOTICE) );
+@error_reporting(E_ALL & ~E_NOTICE);
+// Check we're using PHP5
+if(phpversion() < '5') die("PHP5 is required to run. You have PHP ".phpversion());
 
-if (!substr_compare(PHP_OS, 'win', 0, 3, true))
-{
-	@ini_set('include_path', ini_get('include_path').';.\\common\\includes');
-}
-else
-{
-	@ini_set('include_path', ini_get('include_path').':./common/includes');
-}
+//// Set up include paths.
+//if (!substr_compare(PHP_OS, 'win', 0, 3, true))
+//{
+//	@set_include_path(get_include_path() . PATH_SEPARATOR . '.\\common\\includes');
+//}
+//else
+//{
+//	@set_include_path(get_include_path() . PATH_SEPARATOR . './common/includes');
+//}
 
+// Party time!
 include('common/index.php');
