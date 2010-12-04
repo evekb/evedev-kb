475b3494732f24b101ace37acb612acdecaa968b
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2010-12-04 06:07:56 +0000

    Uncommenting root include path commands.

--- index.php
+++ index.php
@@ -12,15 +12,15 @@ set_error_handler(array('EDKError', 'handler'), E_ALL & ~(E_NOTICE) );
 // Check we're using PHP5
 if(phpversion() < '5') die("PHP5 is required to run. You have PHP ".phpversion());
 
-//// Set up include paths.
-//if (!substr_compare(PHP_OS, 'win', 0, 3, true))
-//{
-//	@set_include_path(get_include_path() . PATH_SEPARATOR . '.\\common\\includes');
-//}
-//else
-//{
-//	@set_include_path(get_include_path() . PATH_SEPARATOR . './common/includes');
-//}
+// Set up include paths.
+if (!substr_compare(PHP_OS, 'win', 0, 3, true))
+{
+	@set_include_path(get_include_path() . PATH_SEPARATOR . '.\\common\\includes');
+}
+else
+{
+	@set_include_path(get_include_path() . PATH_SEPARATOR . './common/includes');
+}
 
 // Party time!
 include('common/index.php');
