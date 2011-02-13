3b0bb77712b33fa66e113a3f904345f6398b2716
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-02-13 07:45:22 +0000

    Add svn keywords

--- themes/default/init.php
+++ themes/default/init.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * $Date: 2011-02-13 18:18:57 +1100 (Sun, 13 Feb 2011) $
- * $Revision: 1185 $
+ * $Date$
+ * $Revision$
  * $HeadURL: https://evedev-kb.googlecode.com/svn/trunk/common/about.php $
  */
 
