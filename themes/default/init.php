1d096e43b1e00aa2ae8b0ed128dbbc3475ef840e
kovell@kitexp.info <kovell@kitexp.info@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-02-13 07:30:45 +0000

    Add a section for the theme to the about page.

---
+++ themes/default/init.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * $Date: 2011-02-13 18:18:57 +1100 (Sun, 13 Feb 2011) $
+ * $Revision: 1185 $
+ * $HeadURL: https://evedev-kb.googlecode.com/svn/trunk/common/about.php $
+ */
+
+$themeInfo = array("name" => "EDK default", "info"=>"The default theme for EDK.");
---
+++ themes/default/templates/about_theme.tpl
@@ -0,0 +1,7 @@
+<!-- about_theme -->
+<div id="about_theme">
+	<div class="block-header2">Theme:</div>
+		<div class="mod_name">{$themeInfo.name}</div>
+		<div class="mod_abstract">{$themeInfo.info}</div>
+</div>
+<!-- /about_theme -->
