792031f67a1b80c562feb3bedef2014e3e3ef76e
kovellia@gmail.com <kovellia@gmail.com@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-05-12 13:16:08 +0000

    Remove thumbnail checks for an existing locally known external pilot ID. ID existence is instead handled by the image server. (Thus allowing for a single EDK image server for a group of boards.)

--- common/includes/class.thumb.php
+++ common/includes/class.thumb.php
@@ -150,21 +150,6 @@ class thumb
 		}
 		else
 		{
-			if ($this->id)
-			{
-				// check for a valid, known external id
-				$qry = DBFactory::getDBQuery();;
-				$qry->execute('SELECT plt_externalid FROM kb3_pilots WHERE plt_externalid = '.$this->id.' LIMIT 1');
-				if (!$qry->recordCount())
-				{
-					// there is no such id so set it to 0
-					$this->id = 0;
-					$this->thumbName = 'portrait_0_'.$this->size.'.jpg';
-					$this->thumbDir = 'img';
-					$this->thumb = CacheHandler::getInternal('portrait_0_'.$this->size.'.jpg', $this->thumbDir);
-					return true;
-				}
-			}
 			// Assume external id < 100,000 is NPC structure/ship
 			if($this->id < 100000 && file_exists("img/ships/64_64/".$this->id.".png"))
 				$img = imagecreatefrompng("img/ships/64_64/".$this->id.".png");
