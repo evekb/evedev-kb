7d0309eaf976cc1ef9d6f9207e1e7b1829b2745a
kovellia@gmail.com <kovellia@gmail.com@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-06-15 12:28:11 +0000

    Move bargraph class to bargraph class file.

---
+++ common/includes/class.bargraph.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * $Date$
+ * $Revision$
+ * $HeadURL$
+ */
+
+
+class BarGraph
+{
+	private $value;
+	private $width;
+	private $text;
+	private $max;
+	private $class = "bar";
+
+	function BarGraph($value = 0, $max = 100, $width = 75, $text = "")
+	{
+		$this->value = $value;
+		$this->width = $width;
+		$this->text = $text;
+		$this->max = $max;
+
+		$this->class = "bar";
+	}
+
+	function generate()
+	{
+		if ($this->text == "") $this->text = "&nbsp;";
+
+		if ($this->value) $width = $this->width / ($this->max / $this->value);
+		else $width = 0;
+
+		global $smarty;
+		$smarty->assign('class', $this->class);
+		$smarty->assign('width', $width);
+		$smarty->assign('maxwidth', $this->width);
+		$smarty->assign('text', $this->text);
+
+		return $smarty->fetch(get_tpl("bargraph"));
+	}
+
+	function setLow($low, $class)
+	{
+		if ($this->value <= $low) $this->class = "bar-".$class;
+	}
+}
