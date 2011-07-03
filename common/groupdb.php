1b52db972536fe1ee353ee9bb30a5a20ff89af59
kovellia@gmail.com <kovellia@gmail.com@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-07-03 06:12:41 +0000

    Groupdb page uses smarty templates.

--- common/groupdb.php
+++ common/groupdb.php
@@ -5,36 +5,60 @@
  * $HeadURL$
  * @package EDK
  */
-
-if (!$id = intval($_GET['id']))
+/*
+ * @package EDK
+ */
+class pInvtype extends pageAssembly
 {
-    $page = new Page('Error');
-    $page->setContent('No valid ID specified.');
-    $page->generate();
-    exit;
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->queue("start");
+		$this->queue("details");
+	}
+
+	function start()
+	{
+		$this->groupID = intval($_GET['id']);
+		$this->page = new Page('Item Database');
+
+	}
+
+	function details()
+	{
+		global $smarty;
+		if (!$this->groupID)
+		{
+			$this->page->setTitle('Error');
+			return 'This ID is not a valid group ID.';
+		}
+		$sql = 'SELECT * FROM kb3_item_types d'.
+				' WHERE d.itt_id = '.$this->groupID;
+		$qry = DBFactory::getDBQuery();;
+		$qry->execute($sql);
+		$row = $qry->getRow();
+
+		$this->page->setTitle('Item Database - '.$row['itt_name'].' Index');
+
+		$sql = 'SELECT * FROM kb3_invtypes d'.
+				' WHERE d.groupID = '.$this->groupID.
+				' ORDER BY d.typeName ASC';
+		$qry = DBFactory::getDBQuery();;
+		$qry->execute($sql);
+		$rows= array();
+		while($row = $qry->getRow()) {
+			$rows[] = array('typeID'=>$row['typeID'], 'typeName'=>$row['typeName']);
+		}
+		$smarty->assign('rows', $rows);
+		return $smarty->fetch(get_tpl('groupdb'));
+	}
 }
 
-$sql = 'SELECT * FROM kb3_item_types d
-        WHERE d.itt_id = '.$id;
-$qry = DBFactory::getDBQuery();;
-$qry->execute($sql);
-$row = $qry->getRow();
 
-$page = new Page('Item Database - '.$row['itt_name'].' Index');
+$invtype = new pInvtype();
+event::call("invtype_assembling", $invtype);
+$html = $invtype->assemble();
+$invtype->page->setContent($html);
 
-$sql = 'SELECT * FROM kb3_invtypes d
-        WHERE d.groupID = '.$id.'
-	ORDER BY d.typeName ASC';
-    $qry = DBFactory::getDBQuery();;
-    $qry->execute($sql);
-    $html .= "<table class=kb-table cellspacing=1>";
-    $html .= "<tr class=kb-table-header><td width=400>Item Name</td></tr>";
-    while ($row = $qry->getRow())
-    {
-        $html .= '<tr class=kb-table-row-odd><td><a href="?a=invtype&id='.$row['typeID'].'">'.$row['typeName'].'</a></td></tr>';
-    }
-    $html .= "</table>";
-
-$page->setContent($html);
-$page->generate();
-?>
+$invtype->page->generate();
