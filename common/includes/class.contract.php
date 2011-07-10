534dfcb36b0ce78e3a1854eb7df1bb71e82ca609
kovellia@gmail.com <kovellia@gmail.com@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-07-10 11:34:39 +0000

    Remove obsolete code. Update some comments.

--- common/includes/class.contract.php
+++ common/includes/class.contract.php
@@ -24,7 +24,6 @@ class Contract
 	private $ctr_name_ = '';
 	private $ctr_started_ = '';
 	private $ctr_ended_ = '';
-	private $campaign_ = 0;
 	private $ctr_comment_ = '';
 
 	function Contract($ctr_id = 0)
@@ -42,7 +41,7 @@ class Contract
 	{
 		if ($this->executed)
 			return;
-		$qry = DBFactory::getDBQuery();;
+		$qry = DBFactory::getDBQuery();
 		// general
 		$sql = "select * from kb3_contracts ctr
                 where ctr.ctr_id = ".$this->ctr_id_;
@@ -55,7 +54,6 @@ class Contract
 		$this->ctr_name_ = $row['ctr_name'];
 		$this->ctr_started_ = $row['ctr_started'];
 		$this->ctr_ended_ = $row['ctr_ended'];
-		$this->campaign_ = ($row['ctr_campaign'] == "1");
 		$this->ctr_comment_ = $row['ctr_comment'];
 
 		// get corps & alliances for contract
@@ -64,7 +62,7 @@ class Contract
                 where ctd.ctd_ctr_id = ".$row['ctr_id']."
 	            order by 3, 2, 1 -- get corps & alliances for contract";
 
-		$caqry = DBFactory::getDBQuery();;
+		$caqry = DBFactory::getDBQuery();
 		if (!$caqry->execute($sql))
 		{
 			check_contracts();
@@ -110,29 +108,46 @@ class Contract
 		}
 	}
 
+	/**
+	 * @return integer
+	 */
 	function getID()
 	{
 		return $this->ctr_id_;
 	}
 
+	/**
+	 * @return string
+	 */
 	function getName()
 	{
 		$this->execQuery();
 		return $this->ctr_name_;
 	}
 
+	/**
+	 * @return string Formatted string for start date
+	 */
 	function getStartDate()
 	{
 		$this->execQuery();
 		return $this->ctr_started_;
 	}
 
+	/**
+	 * @return string Formatted string for end date
+	 */
 	function getEndDate()
 	{
 		$this->execQuery();
 		return $this->ctr_ended_;
 	}
 
+	/**
+	 * How long this campaign has run for.
+	 * For current campaigns this returns the length so far.
+	 * @return integer The length of this campaign.
+	 */
 	function getRunTime()
 	{
 		if (!$datet = $this->getEndDate())
@@ -144,54 +159,89 @@ class Contract
 		return floor($diff/86400);
 	}
 
+	/**
+	 * @deprecated
+	 * @return boolean
+	 */
 	function getCampaign()
 	{
-		$this->execQuery();
-		return $this->campaign_;
+		return true;
 	}
-	
+
+	/**
+	 * Returns a comment string for this campaign.
+	 * @return string
+	 */
 	function getComment()
 	{
 		$this->execQuery();
 		return $this->ctr_comment_;
 	}
 
+	/**
+	 * Return an array of Corporations in this campaign.
+	 * @return array
+	 */
 	function getCorps()
 	{
 		$this->execQuery();
 		return $this->corps;
 	}
 
+	/**
+	 * Return an array of Alliances in this campaign.
+	 * @return array
+	 */
 	function getAlliances()
 	{
 		$this->execQuery();
 		return $this->alliances;
 	}
 
+	/**
+	 * Return an array of Systems in this campaign.
+	 * @return array
+	 */
 	function getSystems()
 	{
 		$this->execQuery();
 		return $this->systems;
 	}
 
+	/**
+	 * Return an array of Regions in this campaign.
+	 * @return array
+	 */
 	function getRegions()
 	{
 		$this->execQuery();
 		return $this->regions;
 	}
 
+	/**
+	 * Return the count of kills for this campaign
+	 * @return integer
+	 */
 	function getKills()
 	{
 		$this->execQuery();
 		return $this->klist_->getCount();
 	}
 
+	/**
+	 * Return the count of losses for this campaign
+	 * @return integer
+	 */
 	function getLosses()
 	{
 		$this->execQuery();
 		return $this->llist_->getCount();
 	}
 
+	/**
+	 * Return the isk destroyed during this campaign
+	 * @return float
+	 */
 	function getKillISK()
 	{
 		$this->execQuery();
@@ -199,6 +249,10 @@ class Contract
 		return $this->klist_->getISK();
 	}
 
+	/**
+	 * Return the isk lost during this campaign
+	 * @return float
+	 */
 	function getLossISK()
 	{
 		$this->execQuery();
@@ -206,6 +260,10 @@ class Contract
 		return $this->llist_->getISK();
 	}
 
+	/**
+	 * Return the efficiency of this campaign as a percent.
+	 * @return float
+	 */
 	function getEfficiency()
 	{
 		$this->execQuery();
@@ -217,18 +275,30 @@ class Contract
 		return $efficiency;
 	}
 
+	/**
+	 * Return the KillList showing kills in this campaign.
+	 * @return KillList
+	 */
 	function getKillList()
 	{
 		$this->execQuery();
 		return $this->klist_;
 	}
 
+	/**
+	 * Return the KillList showing losses in this campaign.
+	 * @return KillList
+	 */
 	function getLossList()
 	{
 		$this->execQuery();
 		return $this->llist_;
 	}
 
+	/**
+	 * Get next ContractTarget
+	 * @return ContractTarget
+	 */
 	function getContractTarget()
 	{
 		if ($this->contractpointer_ > 30)
@@ -240,9 +310,17 @@ class Contract
 		return $target;
 	}
 
+	/**
+	 *
+	 * @param string $name
+	 * @param string $type
+	 * @param string $startdate
+	 * @param string $enddate
+	 * @param string $comment
+	 */
 	function add($name, $type, $startdate, $enddate = "", $comment = "")
 	{
-		$qry = DBFactory::getDBQuery();;
+		$qry = DBFactory::getDBQuery();
 		if ($type == "campaign") $campaign = 1;
 		else $campaign = 0;
 		// null doesn't work inside the quotes.
@@ -271,9 +349,12 @@ class Contract
 		}
 	}
 
+	/**
+	 * Delete this campaign.
+	 */
 	function remove()
 	{
-		$qry = DBFactory::getDBQuery();;
+		$qry = DBFactory::getDBQuery();
 
 		$qry->execute("delete from kb3_contracts
                        where ctr_id = ".$this->ctr_id_);
@@ -282,16 +363,24 @@ class Contract
                        where ctd_ctr_id = ".$this->ctr_id_);
 	}
 
+	/**
+	 * Check that this campaign exists.
+	 * @return boolean
+	 */
 	function validate()
 	{
-		$qry = DBFactory::getDBQuery();;
+		$qry = DBFactory::getDBQuery();
 
 		$qry->execute("select * from kb3_contracts
                        where ctr_id = ".$this->ctr_id_."
 		         and ctr_site = '".KB_SITE."'");
 		return ($qry->recordCount() > 0);
 	}
-	
+
+	/**
+	 * Set a string describing this campaign.
+	 * @param string $comment
+	 */
 	function setComment($comment)
 	{
 		$qry = DBFactory::getDBQuery();
