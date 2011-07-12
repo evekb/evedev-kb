ec85fa92d2e05e506885191f92f5a2e6909e7419
kovellia@gmail.com <kovellia@gmail.com@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-07-12 07:01:30 +0000

    Change clearup script to use a maximum cache size for store instead of maximum age.

--- cron/cron_clearup.php
+++ cron/cron_clearup.php
@@ -7,25 +7,29 @@
  * @package EDK
  */
 
-if (!substr_compare(PHP_OS, 'win', 0, 3, true))
-{
+/** @var integer Maximum size for the store in megabytes. */
+$maxStoreSize = 512;
+/** @var integer Maximum image age in days. */
+$maxImageAge = 30;
+/** @var integer Maximum SQL query age in days. */
+$maxSQLAge = 2;
+/** @var integer Maximum cache age for everything else in days. */
+$maxOtherAge = 7;
+
+if (!substr_compare(PHP_OS, 'win', 0, 3, true)) {
 	@ini_set('include_path', ini_get('include_path').';.\\common\\includes');
-}
-else
-{
+} else {
 	@ini_set('include_path', ini_get('include_path').':./common/includes');
 }
 
-if(file_exists(getcwd().'/cron_clearup.php'))
-{
+if(file_exists(getcwd().'/cron_clearup.php')) {
 	// current working directory minus last 5 letters of string ("/cron")
 	$KB_HOME = preg_replace('/[\/\\\\]cron$/', '', getcwd());
-}
-elseif(file_exists(__FILE__))
-{
+} else if(file_exists(__FILE__)) {
 	$KB_HOME = preg_replace('/[\/\\\\]cron[\/\\\\]cron_clearup\.php$/', '', __FILE__);
+} else {
+	die("Set \$KB_HOME to the killboard root in cron/cron_clearup.php.");
 }
-else die("Set \$KB_HOME to the killboard root in cron/cron_clearup.php.");
 
 // If the above doesn't work - place your working directory path to killboard root below - comment out the above two lines and uncomment the two below
 
@@ -41,6 +45,7 @@ require_once('common/includes/db.php');
 require_once ('common/includes/class.edkerror.php');
 
 set_error_handler(array('EDKError', 'handler'), E_ERROR );
+@error_reporting(E_ERROR);
 
 // disable query caching while the script is running.
 $qcache = config::get('cfg_qcache');
@@ -54,14 +59,17 @@ if($pcache) {
 	config::set('cache_enabled', 0);
 }
 
-echo "<br />Removed ".CacheHandler::removeByAge('SQL/', 7 * 24)." files from SQL/<br />\n";
-echo "Removed ".CacheHandler::removeByAge('page/'.KB_SITE.'/', 7 * 24)." files from page/<br />\n";
-echo "Removed ".CacheHandler::removeByAge("templates_c/", 1 * 24)." files from templates_c/<br />\n";
-echo "Removed ".CacheHandler::removeByAge("mail/", 7 * 24)." files from mail/<br />\n";
+echo "<br />Removed ".CacheHandler::removeByAge('SQL/', $maxSQLAge * 24)." files from SQL/<br />\n";
+echo "Removed ".CacheHandler::removeByAge('page/'.KB_SITE.'/', $maxOtherAge * 24)." files from page/<br />\n";
+echo "Removed ".CacheHandler::removeByAge("templates_c/", $maxOtherAge * 24)." files from templates_c/<br />\n";
+echo "Removed ".CacheHandler::removeByAge("mail/", $maxOtherAge * 24)." files from mail/<br />\n";
 // Let's let people see their latest beautiful creation in the character creator.
-echo "Removed ".CacheHandler::removeByAge('img/', 30 * 24)." files from img/<br />\n";
-echo "Removed ".CacheHandler::removeByAge('store/', 7 * 24)." files from store/<br />\n";
-echo "Removed ".CacheHandler::removeByAge('/', 30 * 24, false)." files from entire cache<br />\n";
+echo "Removed ".CacheHandler::removeByAge('img/', $maxImageAge * 24)." files from img/<br />\n";
+//echo "Removed ".CacheHandler::removeBySize('img/', 512 * 24)." files from img/<br />\n";
+//echo "Removed ".CacheHandler::removeByAge('store/', 7 * 24)." files from store/<br />\n";
+echo "Removed ".CacheHandler::removeBySize('store/', $maxStoreSize)." files from store/<br />\n";
+
+//echo "Removed ".CacheHandler::removeByAge('/', 30 * 24, false)." files from entire cache<br />\n";
 
 if($qcache) {
 	echo "<br />\n File query cache re-enabled <br />\n";
