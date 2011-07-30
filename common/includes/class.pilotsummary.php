e582076a9ac1f886dfb470ced580ef4d71644cee
kovellia@gmail.com <kovellia@gmail.com@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-07-30 03:53:31 +0000

    Fix for pilot kill points on adding kills.

--- common/includes/class.pilotsummary.php
+++ common/includes/class.pilotsummary.php
@@ -189,7 +189,7 @@ select $plt_id as psm_plt_id, losses.psm_shp_id, ifnull(kills.knb,0), ifnull(kil
 				"psm_kill_isk = psm_kill_isk + ".$kill->getISKLoss();
 			$qry->execute($sql);
 			$sql = "UPDATE kb3_pilots SET plt_kpoints = plt_kpoints + ".$kill->calculateKillPoints().
-				" WHERE plt_id = ".$kill->getVictimID();
+				" WHERE plt_id = ".$inv->getPilotID();
 			$qry->execute($sql);
 		}
 	}
@@ -231,7 +231,7 @@ select $plt_id as psm_plt_id, losses.psm_shp_id, ifnull(kills.knb,0), ifnull(kil
 			$qry->execute($sql);
 
 			$sql = "UPDATE kb3_pilots SET plt_kpoints = plt_kpoints - ".$kill->getKillPoints().
-				" WHERE plt_id = ".$kill->getVictimID();
+				" WHERE plt_id = ".$inv->getPilotID();
 			$qry->execute($sql);
 		}
 	}
