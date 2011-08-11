b9e680a77d100ec4b9ee7c714c2be8de47e85e03
kovellia@gmail.com <kovellia@gmail.com@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-08-11 13:13:17 +0000

    Add some default javascript libraries for mods to use. Add 'addOnLoad' to allow chaining of onload functions.

--- common/includes/class.page.php
+++ common/includes/class.page.php
@@ -18,9 +18,10 @@ class Page
 	private $igb = IS_IGB;
 	private $timestart = 0;
 	private $cachable = true;
-	private $onload = null;
+	private $onload = array();
 	private $contenthtml = "";
 	private $contexthtml = array();
+	private $jsLibs = array();
 
 	/**
 	 * Construct a Page class with the given title.
@@ -32,14 +33,14 @@ class Page
 		global $timeStarted;
 		$this->timestart = &$timeStarted;
 		event::call('page_initialisation', $this);
-		if (!config::get('public_stats'))
-		{
-			config::set('public_stats','do nothing');
+		if (!config::get('public_stats')) {
+			config::set('public_stats', 'do nothing');
 		}
 
 		$this->title = htmlspecialchars($title);
 		$this->cachable = $cachable;
 	}
+
 	/**
 	 * Set the content html that is displayed in the main body panel.
 	 *
@@ -49,6 +50,7 @@ class Page
 	{
 		$this->contenthtml = $html;
 	}
+
 	/**
 	 * Set the context html that is displayed in the sidebar.
 	 *
@@ -58,6 +60,7 @@ class Page
 	{
 		$this->contexthtml[] = $html;
 	}
+
 	/**
 	 * Create and display an error message.
 	 *
@@ -71,6 +74,7 @@ class Page
 		$this->setContent($smarty->fetch(get_tpl('error')));
 		$this->generate();
 	}
+
 	/**
 	 * Add a line to the header html.
 	 *
@@ -80,6 +84,28 @@ class Page
 	{
 		$this->headlines[] = $line;
 	}
+
+	/**
+	 * Load javascript libraries from CDN if required
+	 * and add to header.
+	 */
+	public function addJsLibs($jsLib)
+	{
+		$jsLib = strtolower($jsLib);
+		if (!isset($this->jsLibs[$jsLib])) {
+			switch($jsLib)
+			{
+				case 'jquery' :
+					$this->addHeader("<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js\" type=\"text/javascript\"></script>");
+					break;
+				case 'prototype' :
+					$this->addHeader("<script src=\"//ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js\" type=\"text/javascript\"></script>");
+					break;
+			}
+			$this->jsLibs[$jsLib] = true;
+		}
+	}
+
 	/**
 	 * Add a line to the start of the body html.
 	 *
@@ -89,6 +115,7 @@ class Page
 	{
 		$this->bodylines[] = $line;
 	}
+
 	/**
 	 * Generate the output html.
 	 *
@@ -101,9 +128,9 @@ class Page
 
 		$smarty->assign('kb_title', config::get('cfg_kbtitle').' '.$this->title);
 
-		if ($this->onload)
-		{
-			$smarty->assign('on_load', ' onload="'.$this->onload.'"');
+		if ($this->onload) {
+			$smarty->assign('on_load', ' onload="'
+					.implode('; ', $this->onload).'"');
 		}
 
 		// header
@@ -123,38 +150,37 @@ class Page
 
 		$nav = new Navigation();
 		$menu = $nav->generateMenu();
-		if(!count($menu->get())) $w = 100;
+		if (!count($menu->get())) $w = 100;
 		else $w = floor(100 / count($menu->get()));
 
-		$smarty->assign('menu_w',$w.'%');
+		$smarty->assign('menu_w', $w.'%');
 		$smarty->assign('menu_count', count($menu->get()));
 		$smarty->assign('menu', $menu->get());
 
 		//check if banner is a swf
-		$bannerExn = substr(config::get('style_banner'),-3);
-		if (strtoupper($bannerExn) == 'SWF')
-		{
+		$bannerExn = substr(config::get('style_banner'), -3);
+		if (strtoupper($bannerExn) == 'SWF') {
 			$smarty->assign('bannerswf', 'true');
-		}
-		else
-		{
+		} else {
 			$smarty->assign('bannerswf', 'false');
 		}
 
 		$smarty->assign('page_title', $this->title);
 
-		$processingtime = number_format((microtime(true) - $this->timestart),4);
+		$processingtime = number_format((microtime(true) - $this->timestart), 4);
 
 		$qry = DBFactory::getDBQuery();
 		$smarty->assign('profile_sql_cached', $qry->queryCachedCount());
 		$smarty->assign('profile_sql', $qry->queryCount());
 		$smarty->assign('profile_time', $processingtime);
-		$smarty->assign('sql_time', number_format($qry->getTotalTime(),4));
-		if($this->isAdmin() || config::get('cfg_profile') || intval(KB_PROFILE))
+		$smarty->assign('sql_time', number_format($qry->getTotalTime(), 4));
+		if ($this->isAdmin()
+				|| config::get('cfg_profile')
+				|| intval(KB_PROFILE)) {
 			$smarty->assign('profile', 1);
+		}
 		$smarty->assign('content_html', $this->contenthtml);
-		if (config::get('user_showmenu'))
-		{
+		if (config::get('user_showmenu')) {
 			$this->contexthtml = array_merge(array(user::menu()), $this->contexthtml);
 		}
 		$smarty->assign('context_html', implode($this->contexthtml));
@@ -162,13 +188,17 @@ class Page
 		event::call('smarty_displayindex', $smarty);
 
 		$html = $smarty->fetch(get_tpl('index'));
-		if (!$this->cachable) config::put('cache_enabled', false);
+		if (!$this->cachable) {
+			config::put('cache_enabled', false);
+		}
 		event::call('final_content', $html);
 		// Reduce page size by about 10%
+		//TODO: enable for prod
 		//$html = preg_replace('/\s+\<\//', ' </', $html);
 		//$html = preg_replace('/>\s+/', '> ', $html);
 		echo $html;
 	}
+
 	/**
 	 * Return whether this will display as an igb page.
 	 *
@@ -178,13 +208,28 @@ class Page
 	{
 		return $this->igb;
 	}
+
+	/**
+	 * Add an onload instruction for Smarty.
+	 *
+	 * @param string $onload
+	 */
+	public function addOnLoad($onload)
+	{
+		$this->onload[] = $onload;
+	}
+
 	/**
 	 * Set the onload variable for Smarty.
+	 *
+	 * @param string $onload
 	 */
 	public function setOnLoad($onload)
 	{
-		$this->onload = $onload;
+		$this->onload = array();
+		$this->onload[] = $onload;
 	}
+
 	/**
 	 * Set the page title.
 	 * @param string $title  The page title.
@@ -193,26 +238,28 @@ class Page
 	{
 		$this->title = htmlspecialchars($title);
 	}
+
 	/**
 	 * Get the page title.
-	 * @return <type>  The page title.
+	 * @return string  The page title.
 	 */
 	public function getTitle()
 	{
 		return $this->title;
 	}
+
 	/**
 	 * If this is not an admin session redirect to the login page.
 	 */
 	public function setAdmin()
 	{
-		if (!Session::isAdmin())
-		{
+		if (!Session::isAdmin()) {
 			header("Location: ".KB_HOST."/?a=login");
 			echo '<a href="'.KB_HOST.'/?a=login">Login</a>';
 			exit;
 		}
 	}
+
 	/**
 	 * Return whether this is an admin session.
 	 *
@@ -222,6 +269,7 @@ class Page
 	{
 		return Session::isAdmin();
 	}
+
 	/**
 	 * Return whether this is a superadmin session.
 	 *
@@ -231,14 +279,17 @@ class Page
 	{
 		return Session::isSuperAdmin();
 	}
+
 	/**
 	 * If this is not a superadmin session redirect to the login page.
 	 */
 	public function setSuperAdmin()
 	{
-		if (!Session::isSuperAdmin())
-			Header("Location: ".KB_HOST."/?a=login");
+		if (!Session::isSuperAdmin()) {
+			header("Location: ".KB_HOST."/?a=login");
+		}
 	}
+
 	/**
 	 * Set whether this page is cacheable.
 	 *
