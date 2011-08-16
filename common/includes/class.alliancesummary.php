1ce4c593ba352f979e2e23de3bec21d4439cf693
kovellia@gmail.com <kovellia@gmail.com@03c0853e-7e65-11de-a390-7ba58b65c1e3>
2011-08-16 11:01:24 +0000

    Minute speed boost to building alliance stat summaries.

--- common/includes/class.alliancesummary.php
+++ common/includes/class.alliancesummary.php
@@ -6,7 +6,6 @@
  * @package EDK
  */
 
-
 /**
  * Store summary statistics for Alliances.
  * @package EDK
@@ -17,24 +16,27 @@ class allianceSummary extends statSummary
 
 	function allianceSummary($all_id)
 	{
-		$this->all_id_ = intval($all_id);
+		$this->all_id_ = (int) $all_id;
 	}
+
 	/**
 	 * Fetch the summary information.
 	 */
 	protected function execute()
 	{
-		if($this->executed) return;
-		if(!$this->all_id_)
-		{
+		if ($this->executed) {
+			return;
+		}
+		if (!$this->all_id_) {
 			$this->executed = true;
 			return false;
 		}
 
 		$qry = DBFactory::getDBQuery();
 		$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = ".$this->all_id_);
-		if(!$qry->recordCount())
+		if (!$qry->recordCount()) {
 			self::buildSummary($this->all_id_);
+		}
 
 		$sql = "SELECT scl_class, scl_id, kb3_sum_alliance.*
 			FROM kb3_ship_classes left join kb3_sum_alliance
@@ -42,23 +44,25 @@ class allianceSummary extends statSummary
 			WHERE scl_class not in ('Drone','Unknown')
 				ORDER BY scl_class";
 		$qry->execute($sql);
-		while($row = $qry->getRow())
-		{
+		while ($row = $qry->getRow()) {
 			$this->summary[$row['scl_id']]['class_name'] = $row['scl_class'];
-			$this->summary[$row['scl_id']]['killcount'] = intval($row['asm_kill_count']);
-			$this->summary[$row['scl_id']]['killisk'] = floatval($row['asm_kill_isk']);
-			$this->summary[$row['scl_id']]['losscount'] = intval($row['asm_loss_count']);
-			$this->summary[$row['scl_id']]['lossisk'] = floatval($row['asm_loss_isk']);
+			$this->summary[$row['scl_id']]['killcount'] = (int) $row['asm_kill_count'];
+			$this->summary[$row['scl_id']]['killisk'] = (float) $row['asm_kill_isk'];
+			$this->summary[$row['scl_id']]['losscount'] = (int) $row['asm_loss_count'];
+			$this->summary[$row['scl_id']]['lossisk'] = (float) $row['asm_loss_isk'];
 		}
 		$this->executed = true;
 	}
+
 	/**
 	 * Build a new summary table for an alliance.
 	 */
 	private static function buildSummary($all_id)
 	{
-		$all_id = intval($all_id);
-		if(!$all_id) return false;
+		$all_id = (int) $all_id;
+		if (!$all_id) {
+			return false;
+		}
 		$qry = DBFactory::getDBQuery();
 		$qry->autocommit(false);
 
@@ -103,46 +107,8 @@ class allianceSummary extends statSummary
 		$qry->execute($sql);
 		$qry->autocommit(true);
 		return;
-
-//		$sql = "CREATE TEMPORARY TABLE `tmp_all_summary` (
-//		  `asm_all_id` int(11) NOT NULL DEFAULT '0',
-//		  `asm_shp_id` int(3) NOT NULL DEFAULT '0',
-//		  `asm_kill_count` int(11) NOT NULL DEFAULT '0',
-//		  `asm_kill_isk` float NOT NULL DEFAULT '0',
-//		  `asm_loss_count` int(11) NOT NULL DEFAULT '0',
-//		  `asm_loss_isk` float NOT NULL DEFAULT '0',
-//		  PRIMARY KEY (`asm_all_id`,`asm_shp_id`)
-//		) ENGINE = MEMORY";
-//		$qry->execute($sql);
-//
-//		$sql = 'INSERT INTO tmp_all_summary (asm_all_id, asm_shp_id, asm_kill_count, asm_kill_isk)
-//			SELECT '.$all_id.', shp_class, COUNT(kll.kll_id) AS knb,
-//				sum(kll_isk_loss) AS kisk
-//			FROM kb3_kills kll
-//				INNER JOIN kb3_ships shp
-//					ON ( shp.shp_id = kll.kll_ship_id )
-//				INNER JOIN kb3_inv_all ina ON (ina.ina_kll_id = kll.kll_id)
-//			WHERE ina.ina_all_id ='.$all_id.'
-//				AND kll.kll_all_id <> '.$all_id.'
-//			GROUP BY shp_class';
-//		$qry->execute($sql);
-//		$sql = 'INSERT INTO tmp_all_summary (asm_all_id, asm_shp_id, asm_loss_count, asm_loss_isk)
-//			SELECT '.$all_id.', shp_class, count(kll_id) AS lnb, sum(kll_isk_loss) AS lisk
-//			FROM kb3_kills kll
-//				INNER JOIN kb3_ships shp ON ( shp.shp_id = kll.kll_ship_id )
-//			WHERE  kll.kll_all_id = '.$all_id.'
-//					AND EXISTS (SELECT 1
-//							FROM kb3_inv_all
-//							WHERE kll.kll_id = ina_kll_id
-//							AND ina_all_id <> '.$all_id.' limit 0,1)
-//			GROUP BY shp_class
-//			ON DUPLICATE KEY UPDATE asm_loss_count = values(asm_loss_count),
-//				asm_loss_isk = values(asm_loss_isk)';
-//		$qry->execute($sql);
-//		$qry->execute("INSERT IGNORE INTO kb3_sum_alliance SELECT * FROM tmp_all_summary");
-//		$qry->execute("DROP TEMPORARY TABLE tmp_all_summary");
-//		$qry->autocommit(true);
 	}
+
 	/**
 	 * Add a Kill and its value to the summary.
 	 */
@@ -151,36 +117,40 @@ class allianceSummary extends statSummary
 		$alls = array();
 		$qry = DBFactory::getDBQuery();
 		$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = ".$kill->getVictimAllianceID());
-		if($qry->recordCount())
-		{
+		if ($qry->recordCount()) {
 			// php4 doesn't handle indirect references so specify each one.
 			$ship = $kill->getVictimShip();
 			$class = $ship->getClass();
 			$sql = "INSERT INTO kb3_sum_alliance (asm_all_id, asm_shp_id, asm_loss_count, asm_loss_isk) ".
-				"VALUES ( ".$kill->getVictimAllianceID().", ".$class->getID().", 1, ".
-				$kill->getISKLoss().") ON DUPLICATE KEY UPDATE ".
-				"asm_loss_count = asm_loss_count + 1, ".
-				"asm_loss_isk = asm_loss_isk + ".$kill->getISKLoss();
+					"VALUES ( ".$kill->getVictimAllianceID().", ".$class->getID().", 1, ".
+					$kill->getISKLoss().") ON DUPLICATE KEY UPDATE ".
+					"asm_loss_count = asm_loss_count + 1, ".
+					"asm_loss_isk = asm_loss_isk + ".$kill->getISKLoss();
 			$qry->execute($sql);
 		}
-		foreach($kill->involvedparties_ as $inv)
-		{
-			if(isset($alls[$inv->getAllianceID()])) continue;
+		foreach ($kill->getInvolved() as $inv) {
+			if (isset($alls[$inv->getAllianceID()])) {
+				continue;
+			}
 			$alls[$inv->getAllianceID()] = 1;
-			$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = ".$inv->getAllianceID());
+			$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = "
+					.$inv->getAllianceID());
 			//if(!$qry->recordCount()) allianceSummary::buildSummary($inv->getAllianceID());
-			if(!$qry->recordCount()) continue;
+			if (!$qry->recordCount()) {
+				continue;
+			}
 			// php4 doesn't handle indirect references so specify each one.
 			$ship = $kill->getVictimShip();
 			$class = $ship->getClass();
 			$sql = "INSERT INTO kb3_sum_alliance (asm_all_id, asm_shp_id, asm_kill_count, asm_kill_isk) ".
-				"VALUES ( ".$inv->getAllianceID().", ".$class->getID().", 1, ".
-				$kill->getISKLoss().") ON DUPLICATE KEY UPDATE ".
-				"asm_kill_count = asm_kill_count + 1, ".
-				"asm_kill_isk = asm_kill_isk + ".$kill->getISKLoss();
+					"VALUES ( ".$inv->getAllianceID().", ".$class->getID().", 1, ".
+					$kill->getISKLoss().") ON DUPLICATE KEY UPDATE ".
+					"asm_kill_count = asm_kill_count + 1, ".
+					"asm_kill_isk = asm_kill_isk + ".$kill->getISKLoss();
 			$qry->execute($sql);
 		}
 	}
+
 	/**
 	 * Add a Kill and its value to the summary.
 	 */
@@ -188,35 +158,38 @@ class allianceSummary extends statSummary
 	{
 		$alls = array();
 		$qry = DBFactory::getDBQuery();
-		$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = ".$kill->getVictimAllianceID());
+		$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = "
+				.$kill->getVictimAllianceID());
 		// No summary table to remove kill from so skip.
-		if($qry->recordCount())
-		{
+		if ($qry->recordCount()) {
 			// php4 doesn't handle indirect references so specify each one.
 			$ship = $kill->getVictimShip();
 			$class = $ship->getClass();
 			$sql = "UPDATE kb3_sum_alliance SET asm_loss_count = asm_loss_count - 1, ".
-				" asm_loss_isk = asm_loss_isk - ".$kill->getISKLoss().
-				" WHERE asm_all_id = ".$kill->getVictimAllianceID().
+					" asm_loss_isk = asm_loss_isk - ".$kill->getISKLoss().
+					" WHERE asm_all_id = ".$kill->getVictimAllianceID().
 					" AND asm_shp_id = ".$class->getID();
 			$qry->execute($sql);
 		}
-		foreach($kill->involvedparties_ as $inv)
-		{
-			if(intval($alls[$inv->getAllianceID()])) continue;
+		foreach ($kill->getInvolved() as $inv) {
+			if (isset($alls[$inv->getAllianceID()])) {
+				continue;
+			}
 			$alls[$inv->getAllianceID()] = 1;
-			$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = ".$inv->getAllianceID());
-			if(!$qry->recordCount()) continue;
-			// php4 doesn't handle indirect references so specify each one.
+			$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = "
+					.$inv->getAllianceID());
+			if (!$qry->recordCount()) {
+				continue;
+			}
 			$ship = $kill->getVictimShip();
-			$class = $ship->getClass();
 			$sql = "UPDATE kb3_sum_alliance SET asm_kill_count = asm_kill_count - 1, ".
-				" asm_kill_isk = asm_kill_isk - ".$kill->getISKLoss().
-				" WHERE asm_all_id = ".$inv->getAllianceID().
-					" AND asm_shp_id = ".$class->getID();
+					" asm_kill_isk = asm_kill_isk - ".$kill->getISKLoss().
+					" WHERE asm_all_id = ".$inv->getAllianceID().
+					" AND asm_shp_id = ".$ship->getClass()->getID();
 			$qry->execute($sql);
 		}
 	}
+
 	/**
 	 * Update the summary table when a kill value changes.
 	 */
@@ -224,29 +197,35 @@ class allianceSummary extends statSummary
 	{
 		$alls = array();
 		$qry = DBFactory::getDBQuery();
-		$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = ".$kill->getVictimAllianceID());
+		$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = "
+				.$kill->getVictimAllianceID());
 		// No summary table to remove kill from so skip.
-		if($qry->recordCount())
-		{
+		if ($qry->recordCount()) {
 			// php4 doesn't handle indirect references so specify each one.
 			$ship = $kill->getVictimShip();
 			$class = $ship->getClass();
-			$sql = "UPDATE kb3_sum_alliance SET asm_loss_isk = asm_loss_isk + ".$difference.
-				" WHERE asm_all_id = ".$kill->getVictimAllianceID().
+			$sql = "UPDATE kb3_sum_alliance SET asm_loss_isk = asm_loss_isk + "
+			.$difference.
+					" WHERE asm_all_id = ".$kill->getVictimAllianceID().
 					" AND asm_shp_id = ".$class->getID();
 			$qry->execute($sql);
 		}
-		foreach($kill->involvedparties_ as $inv)
-		{
-			if(intval($alls[$inv->getAllianceID()])) continue;
+		foreach ($kill->getInvolved() as $inv) {
+			if (isset($alls[$inv->getAllianceID()])) {
+				continue;
+			}
 			$alls[$inv->getAllianceID()] = 1;
-			$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = ".$inv->getAllianceID());
-			if(!$qry->recordCount()) continue;
+			$qry->execute("SELECT 1 FROM kb3_sum_alliance WHERE asm_all_id = "
+					.$inv->getAllianceID());
+			if (!$qry->recordCount()) {
+				continue;
+			}
 			// php4 doesn't handle indirect references so specify each one.
 			$ship = $kill->getVictimShip();
 			$class = $ship->getClass();
-			$sql = "UPDATE kb3_sum_alliance SET asm_kill_isk = asm_kill_isk + ".$difference.
-				" WHERE asm_all_id = ".$inv->getAllianceID().
+			$sql = "UPDATE kb3_sum_alliance SET asm_kill_isk = asm_kill_isk + "
+			.$difference.
+					" WHERE asm_all_id = ".$inv->getAllianceID().
 					" AND asm_shp_id = ".$class->getID();
 			$qry->execute($sql);
 		}
