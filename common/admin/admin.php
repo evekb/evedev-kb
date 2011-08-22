c1780efb92ef3cb969a3c1b0520f70bbf373c3bc
Kovell <kovellia@gmail.com>
2011-08-22 21:37:18 +1000

    Remove long, long, long commented out code.

--- common/admin/admin.php
+++ common/admin/admin.php
@@ -12,16 +12,6 @@ require_once('common/admin/admin_menu.php');
 $page = new Page();
 $page->setAdmin();
 
-/* Causes long delays on some systems. Rely on globals.php
-
-$svn_revNew = @exec('svnversion');
-if (!empty($svn_revNew))
-{
-    @unlink(KB_CACHEDIR.'/svnrev.php');
-    $data = '<'.'?php $svn_rev = "'.$svn_revNew.'";';
-    file_put_contents(KB_CACHEDIR.'/svnrev.php', $data);
-}
-*/
 if (!$_GET['field'] && !$_GET['sub'])
 {
     $_GET['field'] = 'Advanced';
