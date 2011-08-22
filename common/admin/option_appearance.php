83feae6a8bbd15749887930bf87a7f70d2f65296
Kovell <kovellia@gmail.com>
2011-08-22 21:38:24 +1000

    Remove option to show corp/alliance logos in killlists. This is now always on.

--- common/admin/option_appearance.php
+++ common/admin/option_appearance.php
@@ -32,7 +32,6 @@ options::fadd('Fill home page', 'cfg_fillhome', 'checkbox', '', '', 'Include kil
 options::fadd('Display region names', 'killlist_regionnames', 'checkbox');
 options::fadd('Display comment count', 'comments_count', 'checkbox');
 options::fadd('Display involved count', 'killlist_involved', 'checkbox');
-options::fadd('Display alliance logos', 'killlist_alogo', 'checkbox');
 options::fadd('Display clock', 'show_clock', 'checkbox');
 options::fadd('Display Monthly stats', 'show_monthly', 'checkbox', '', '', 'Default is weekly');
 
