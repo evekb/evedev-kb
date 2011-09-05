c6702749d6e1fad7e971fe34bec9ece0cc67e984
Kovell <kovellia@gmail.com>
2011-09-05 13:29:30 +1000

    Redirect images to correct URL. (remove entity escaping in headers)

--- common/includes/class.thumb.php
+++ common/includes/class.thumb.php
@@ -373,7 +373,7 @@ class thumbInt extends thumb
 				if($this->id)
 				{
 					$url = imageURL::getURL('Pilot', $this->id, $size);
-					header("Location: $url");
+					header(html_entity_decode("Location: $url"));
 					die;
 				}
 
