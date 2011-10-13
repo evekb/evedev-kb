c12d08572377f8c93c83bc36fe9e4403f506c5aa
Kovell <kovellia@gmail.com>
2011-10-13 19:31:52 +1100

    Formatting changes, removing unused code.

--- common/includes/globals.php
+++ common/includes/globals.php
@@ -1,18 +1,16 @@
 <?php
+
 /**
  * $Date$
  * $Revision$
  * $HeadURL$
  * @package EDK
  */
+define('SVN_REV', "Dev");
 
-// current subversion revision
-$svnrevision = '$Revision$';
-$svnrevision = trim(substr($svnrevision, 10, strlen($svnrevision)-11));
-
-define('SVN_REV', "Dev ".$svnrevision);
-
-if(!defined('LATEST_DB_UPDATE')) define('LATEST_DB_UPDATE',"022");
+if (!defined('LATEST_DB_UPDATE')) {
+	define('LATEST_DB_UPDATE', "022");
+}
 
 define('KB_CACHEDIR', 'cache');
 define('KB_PAGECACHEDIR', KB_CACHEDIR.'/page');
@@ -22,7 +20,7 @@ define('KB_UPDATE_URL', 'http://evedev-kb.googlecode.com/files');
 define('API_SERVER', "https://api.eveonline.com");
 //define('API_SERVER', "http://apitest.eveonline.com");
 define('IMG_SERVER', "image.eveonline.com");
-define ("APIVERSION", "V3.3");
+define("APIVERSION", "V3.3");
 
 // current version: major.minor.sub
 // even numbers for minor = development version
@@ -48,7 +46,8 @@ edkloader::register('TopDamageDealerList', 'common/includes/class.toplist.php');
 edkloader::register('TopSoloKillerList', 'common/includes/class.toplist.php');
 edkloader::register('TopPodKillerList', 'common/includes/class.toplist.php');
 edkloader::register('TopGrieferList', 'common/includes/class.toplist.php');
-edkloader::register('TopCapitalShipKillerList', 'common/includes/class.toplist.php');
+edkloader::register('TopCapitalShipKillerList',
+		'common/includes/class.toplist.php');
 edkloader::register('TopContractKillsList', 'common/includes/class.toplist.php');
 edkloader::register('TopContractScoreList', 'common/includes/class.toplist.php');
 edkloader::register('TopPilotTable', 'common/includes/class.toplist.php');
@@ -58,21 +57,11 @@ edkloader::register('TopShipListTable', 'common/includes/class.toplist.php');
 edkloader::register('TopWeaponList', 'common/includes/class.toplist.php');
 edkloader::register('TopWeaponListTable', 'common/includes/class.toplist.php');
 edkloader::register('thumbInt', 'common/includes/class.thumb.php');
+
+//include_once('common/pheal/Pheal.php');
+//spl_autoload_register("Pheal::classload");
+
 require_once('common/includes/db.php');
-
-/* ---------------------------------------------------------------- */
-/* Utility functions */
-
-function shorten($shorten, $by = 22)
-{
-	if (strlen($shorten) > $by)
-	{
-		$s = substr($shorten, 0, $by) . "...";
-	}
-	else $s = $shorten;
-
-	return $s;
-}
 
 function slashfix($fix)
 {
@@ -81,13 +70,15 @@ function slashfix($fix)
 
 function roundsec($sec)
 {
-	if ($sec <= 0)
+	if ($sec <= 0) {
 		$s = 0.0;
-	else
+	} else {
 		$s = $sec;
+	}
 
 	return number_format(round($s, 1), 1);
 }
+
 /**
  * Check if a version of this template exists in this theme or for the igb.
  * If client is igb check if theme has an igb version. If not check in default
@@ -101,35 +92,26 @@ function get_tpl($name)
 	global $themename;
 	event::call('get_tpl', $name);
 
-	if($themename == 'default')
-	{
-		if (IS_IGB)
-		{
-			if (file_exists('./themes/default/templates/igb_'.$name.'.tpl'))
-			{
-				return 'igb_'.$name.'.tpl';
-			}
-		}
-		return $name.'.tpl';
-	}
-	if (IS_IGB)
-	{
-		if(file_exists('./themes/'.$themename.'/templates/igb_'.$name.'.tpl'))
-		{
+	if ($themename == 'default') {
+		if (IS_IGB && file_exists('./themes/default/templates/igb_'.$name
+						.'.tpl')) {
 			return 'igb_'.$name.'.tpl';
 		}
-		elseif(file_exists('./themes/default/templates/igb_'.$name.'.tpl'))
-		{
-			return '../../default/templates/igb_'.$name.'.tpl';
-		}
-	}
-	if(file_exists('./themes/'.$themename.'/templates/'.$name.'.tpl'))
-	{
 		return $name.'.tpl';
-	}
-	elseif(file_exists('./themes/default/templates/'.$name.'.tpl'))
-	{
-		return '../../default/templates/'.$name.'.tpl';
+	} else {
+		if (IS_IGB) {
+			if (is_file('./themes/'.$themename.'/templates/igb_'.$name.'.tpl')) {
+				return 'igb_'.$name.'.tpl';
+			} else if (is_file('./themes/default/templates/igb_'.$name.'.tpl')) {
+				return '../../default/templates/igb_'.$name.'.tpl';
+			}
+		} else {
+			if (is_file('./themes/'.$themename.'/templates/'.$name.'.tpl')) {
+				return $name.'.tpl';
+			} else if (is_file('./themes/default/templates/'.$name.'.tpl')) {
+				return '../../default/templates/'.$name.'.tpl';
+			}
+		}
 	}
 	return $name.'.tpl';
 }
@@ -144,12 +126,11 @@ function get_tpl($name)
  */
 function kbdate($format, $timestamp = null)
 {
-	if ($timestamp === null)
-	{
-		$timestamp = time();
+	if ($timestamp === null) {
+		return gmdate($format);
+	} else {
+		return gmdate($format, $timestamp);
 	}
-
-	return gmdate($format, $timestamp);
 }
 
 /**
@@ -158,10 +139,20 @@ function kbdate($format, $timestamp = null)
  */
 function getYear()
 {
+	if (config::get('show_monthly')) {
+		return gmdate('Y');
+	}
+
 	$test = kbdate('o');
-	if ($test == 'o')
-	{
-		$test = kbdate('Y');
+	if ($test == 'o') {
+		$day = gmdate('j');
+		$week = gmdate('W');
+		if ($week == 1 && $day > 14) {
+			return gmdate('Y') - 1;
+		} else if ($week > 50 && $day < 8) {
+			return gmdate('Y') + 1;
+		}
+		return gmdate('Y');
 	}
 	return $test;
 }
@@ -173,10 +164,13 @@ function getYear()
  */
 function getWeeks($year = null)
 {
-	if(is_null($year)) $year = getYear();
+	if (is_null($year)) {
+		$year = getYear();
+	}
 	$weeks = date('W', mktime(1, 0, 0, 12, 31, $year));
 	return $weeks == 1 ? 52 : $weeks;
 }
+
 /**
  * Return start date for the given week, month, year or date.
  *
@@ -192,27 +186,28 @@ function getWeeks($year = null)
  * @param integer $startdate
  * @return integer 
  */
-function makeStartDate($week = 0, $year = 0, $month = 0, $startweek = 0, $startdate = 0)
+function makeStartDate($week = 0, $year = 0, $month = 0, $startweek = 0,
+		$startdate = 0)
 {
-	$qstartdate=0;
-	if(intval($year)>2000)
-	{
-		if($week)
-		{
-			if($week < 10) $week = '0'.$week;
+	$qstartdate = 0;
+	if (intval($year) > 2000) {
+		if ($week) {
+			if ($week < 10) {
+				$week = '0'.$week;
+			}
 			$qstartdate = strtotime($year.'W'.$week.' UTC');
-		}
-		elseif($month)
+		} else if ($month) {
 			$qstartdate = strtotime($year.'-'.$month.'-1 00:00 UTC');
-		elseif($startweek)
-		{
+		} else if ($startweek) {
 			$qstartdate = strtotime($year.'W'.$startweek.' UTC');
-		}
-		else
+		} else {
 			$qstartdate = strtotime($year.'-1-1 00:00 UTC');
+		}
 	}
 	//If set use the latest startdate and earliest enddate set.
-	if($startdate && $qstartdate < strtotime($startdate." UTC")) $qstartdate = strtotime($startdate." UTC");
+	if ($startdate && $qstartdate < strtotime($startdate." UTC")) {
+		$qstartdate = strtotime($startdate." UTC");
+	}
 	return $qstartdate;
 }
 
@@ -233,23 +228,26 @@ function makeStartDate($week = 0, $year = 0, $month = 0, $startweek = 0, $startd
  */
 function makeEndDate($week = 0, $year = 0, $month = 0, $enddate = 0)
 {
-	if($year)
-	{
-		if($week)
-		{
-			if($week < 10) $week = '0'.$week;
-			$qenddate = strtotime($year.'W'.$week.' +7days -1second UTC');
-		}
-		else if($month)
-			{
-				if($month == 12) $qenddate = strtotime(($year).'-12-31 23:59 UTC');
-				else $qenddate = strtotime(($year).'-'.($month + 1).'-1 00:00 - 1 minute UTC');
+	if ($year) {
+		if ($week) {
+			if ($week < 10) {
+				$week = '0'.$week;
 			}
-			else
+			$qenddate = strtotime($year.'W'.$week.' +7days -1second UTC');
+		} else if ($month) {
+			if ($month == 12) {
 				$qenddate = strtotime(($year).'-12-31 23:59 UTC');
+			} else {
+				$qenddate = strtotime(($year).'-'.($month + 1).'-1 00:00 - 1 minute UTC');
+			}
+		} else {
+			$qenddate = strtotime(($year).'-12-31 23:59 UTC');
+		}
 	}
 	//If set use the earliest enddate.
-	if($enddate && (!$qenddate || ($qenddate && $qenddate > strtotime($enddate." UTC")))) $qenddate = strtotime($enddate." UTC");
+	if ($enddate && (!$qenddate || ($qenddate && $qenddate > strtotime($enddate." UTC")))) {
+		$qenddate = strtotime($enddate." UTC");
+	}
 
 	return $qenddate;
 }
