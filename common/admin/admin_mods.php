8abf07e77eba2801fdd2fc619e567d789dccc23b
Kovell <kovellia@gmail.com>
2011-10-25 10:07:00 +1100

    Use simple urls for mod selection.

--- common/admin/admin_mods.php
+++ common/admin/admin_mods.php
@@ -5,20 +5,15 @@
  * $HeadURL$
  * @package EDK
  */
-
 require_once('common/admin/admin_menu.php');
 
 $page = new Page('Administration - Mods');
 $page->setAdmin();
 
-if(isset($_POST['set_mods']) && $_POST['set_mods'])
-{
-	foreach($_POST as $key => $val)
-	{
-		if(substr($key, 0, 4) == "mod_" && $val == "on")
-		{
-			if(substr($key, 4, strlen($key) - 4) != 'item_values')
-			{
+if (isset($_POST['set_mods']) && $_POST['set_mods']) {
+	foreach ($_POST as $key => $val) {
+		if (substr($key, 0, 4) == "mod_" && $val == "on") {
+			if (substr($key, 4, strlen($key) - 4) != 'item_values') {
 				$activemods .= substr($key, 4, strlen($key) - 4) . ",";
 			}
 		}
@@ -29,15 +24,13 @@ if(isset($_POST['set_mods']) && $_POST['set_mods'])
 $activemods = explode(",", config::get("mods_active"));
 
 $rows = array();
-if($handle = opendir('mods'))
-{
+if ($handle = opendir('mods')) {
 	$modlist = array();
-	while($file = readdir($handle))
-	{
-		if(is_dir("mods/$file") && $file != ".." & $file != "." & $file != ".svn")
-		{
+	while ($file = readdir($handle)) {
+		if (is_dir("mods/$file") && $file != ".." & $file != "." & $file != ".svn") {
 			$rows[$file] = array(
 				'name' => $file,
+				'url' => edkURI::page("settings_$file"),
 				'checked' => in_array($file, $activemods),
 				'settings' => file_exists("mods/$file/settings.php"));
 		}
