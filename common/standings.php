770262333f15a7dccf85ab3790265a5a73802b32
Kovell <kovellia@gmail.com>
2011-10-31 02:15:02 +1100

    Update standings page to work with config::get() instead of CORP_ID/ALLIANCE_ID

--- common/standings.php
+++ common/standings.php
@@ -10,64 +10,55 @@ $page = new Page();
 $page->setTitle('Standings');
 
 $qry = DBFactory::getDBQuery();
-if (CORP_ID)
-{
-	$qry->execute('SELECT * FROM kb3_standings WHERE sta_from='.CORP_ID.' AND sta_from_type=\'c\' ORDER BY sta_value DESC');
+$ent = array();
+if (config::get("cfg_corpid")) {
+	$ent[] = 'sta_from IN (' . join(',', config::get("cfg_corpid"))
+			. ') AND sta_from_type=\'c\'';
 }
-else
-{
-	$qry->execute('SELECT * FROM kb3_standings WHERE sta_from='.ALLIANCE_ID.' AND sta_from_type=\'a\' ORDER BY sta_value DESC');
+if (config::get("cfg_allianceid")) {
+	$ent[] = 'sta_from IN (' . join(',', config::get("cfg_allianceid"))
+			. ') AND sta_from_type=\'a\'';
 }
+$qry->execute('SELECT * FROM kb3_standings WHERE ('
+		. join(') OR (', $ent)
+		. ') ORDER BY sta_value DESC');
 
 $permt = array();
-while ($row = $qry->getRow())
-{
+while ($row = $qry->getRow()) {
 	$typ = $row['sta_to_type'];
 	$val = sprintf("%01.1f", $row['sta_value']);
 	$id = $row['sta_to'];
 
-	if ($row['sta_value'] > 5)
-	{
+	if ($row['sta_value'] > 5) {
 		$icon = 'high';
-	}
-	elseif ($row['sta_value'] > 0)
-	{
+	} else if ($row['sta_value'] > 0) {
 		$icon = 'good';
-	}
-	elseif ($row['sta_value'] >= -5)
-	{
+	} else if ($row['sta_value'] >= -5) {
 		$icon = 'bad';
-	}
-	else
-	{
+	} else {
 		$icon = 'horrible';
 	}
 
-	if ($typ == 'a')
-	{
-		$alliance = new Alliance($row['sta_to']);
+	if ($typ == 'a') {
+		$alliance = Alliance::getByID((int) $row['sta_to']);
 		$text = $alliance->getName();
 		$pid = $alliance->getUnique();
-		$link = KB_HOST.'/?a=admin_standings&del='.$typ.$row['sta_to'];
+		$link = edkURI::page('admin_standings', $typ . $row['sta_to'], 'del');
 		$permt[$typ][] = array('text' => $text, 'link' => $link, 'value' => $val, 'comment' => $row['sta_comment'],
 			'id' => $id, 'pid' => $pid, 'typ' => $row['sta_to'], 'icon' => $icon);
-	}
-	if ($typ == 'c')
-	{
-		$corp = new Corporation($row['sta_to']);
+	} else if ($typ == 'c') {
+		$corp = Corporation::getByID((int) $row['sta_to']);
 		$text = $corp->getName();
-		$link = KB_HOST.'/?a=admin_standings&del='.$typ.$row['sta_to'];
+		$link = edkURI::page('admin_standings', $typ . $row['sta_to'], 'del');
 		$permt[$typ][] = array('text' => $text, 'link' => $link, 'value' => $val, 'comment' => $row['sta_comment'],
 			'id' => $id, 'typ' => $typ, 'icon' => $icon);
 	}
 }
 $perm = array();
-if ($permt['a'])
-{
+if ($permt['a']) {
 	$perm[] = array('name' => 'Alliances', 'list' => $permt['a']);
 }
-if ($permt['c'])
-{
+if ($permt['c']) {
 	$perm[] = array('name' => 'Corporations', 'list' => $permt['c']);
 }
 
