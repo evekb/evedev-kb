6071cd61ddfb29793117209058e9d09a872e363c
Kovell <kovellia@gmail.com>
2011-11-18 00:04:15 +1100

    Admin redirect works. Move styles into CSS file.

--- common/login.php
+++ common/login.php
@@ -8,26 +8,23 @@
 
 $page = new Page('Login');
 
-if (trim($_POST['usrpass']))
-{
-	if($_POST['usrlogin'] == '' && $_POST['usrpass'] == ADMIN_PASSWORD 
-		&& substr(ADMIN_PASSWORD,0,3) != '$1$'
-		&& substr(ADMIN_PASSWORD,0,3) != '$2$'
-		&& substr(ADMIN_PASSWORD,0,3) != '$2a$')
-	{
+if (trim($_POST['usrpass'])) {
+	if ($_POST['usrlogin'] == '' && $_POST['usrpass'] == ADMIN_PASSWORD
+			&& substr(ADMIN_PASSWORD, 0, 3) != '$1$'
+			&& substr(ADMIN_PASSWORD, 0, 3) != '$2$'
+			&& substr(ADMIN_PASSWORD, 0, 3) != '$2a$') {
 		@chmod("kbconfig.php", 0660);
-		if(!is_writeable("kbconfig.php"))
-		{
+		if (!is_writeable("kbconfig.php")) {
 			$smarty->assign('error', 'Admin password is unencrypted and '.
-				'kbconfig.php is not writeable. Either encrypt the admin '.
-				'password or set kbconfig.php writeable.');
-		}
-		else
-		{
+					'kbconfig.php is not writeable. Either encrypt the admin '.
+					'password or set kbconfig.php writeable.');
+		} else {
 			$kbconfig = file_get_contents('kbconfig.php');
-			$newpwd = preg_replace('/(\$|\\\\)/', '\\\\$1',crypt(ADMIN_PASSWORD));
-			$kbconfig = preg_replace('/define\s*\(\s*[\'"]ADMIN_PASSWORD[\'"][^)]*\)/',
-					"define('ADMIN_PASSWORD', '".$newpwd."')", $kbconfig);
+			$newpwd = preg_replace('/(\$|\\\\)/', '\\\\$1',
+					crypt(ADMIN_PASSWORD));
+			$kbconfig = preg_replace('/define\s*\(\s*[\'"]ADMIN_PASSWORD[\'"]'
+					.'[^)]*\)/', "define('ADMIN_PASSWORD', '"
+					.$newpwd."')", $kbconfig);
 			file_put_contents("kbconfig.php", trim($kbconfig));
 			chmod("kbconfig.php", 0440);
 
@@ -37,27 +34,23 @@ if (trim($_POST['usrpass']))
 			header('Location: '.edkURI::page('admin'));
 			die;
 		}
-	}
-	elseif ($_POST['usrlogin'] == '' && crypt($_POST['usrpass'],ADMIN_PASSWORD) == ADMIN_PASSWORD )
-	{
+	} elseif ($_POST['usrlogin'] == ''
+			&& crypt($_POST['usrpass'], ADMIN_PASSWORD) == ADMIN_PASSWORD) {
 		session::create(true);
 
 		session_write_close();
-		$link = preg_replace('/[^a-zA-Z0-9-_]/', '', edkURI::getArg("page", 1));
-		header('Location: '.edkURI::page($link));
+		$page = preg_replace('/[^a-zA-Z0-9-_]/', '', edkURI::getArg("page", 1));
+		$page = $page ? $page : "admin";
+		header('Location: '.edkURI::page($page));
 		die;
-	}
-	else
-	{
+	} else {
 		$result = user::login($_POST['usrlogin'], $_POST['usrpass']);
-		if ($result)
-		{
+		if ($result) {
 			header('Location: '.edkURI::page('home'));
 			die;
-		}
-		else
-		{
-			$smarty->assign('error', 'Login error, please check your username and password.');
+		} else {
+			$smarty->assign('error', 'Login error, please check your username'
+					.' and password.');
 		}
 	}
 }
