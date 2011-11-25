a0587ffd14eb4a4f9dc2333623f48949bb18e10a
Kovell <kovellia@gmail.com>
2011-11-26 01:57:12 +1100

    Manually posting a kill redirects using simple urls.

--- common/post.php
+++ common/post.php
@@ -1,27 +1,28 @@
 <?php
+
 /**
  * $Date$
  * $Revision$
  * $HeadURL$
  * @package EDK
  */
-
 $page = new Page('Post killmail');
 
-if(isset($_POST['undelete']) && isset($_POST['kll_id']) && $page->isAdmin())
-{
+if (isset($_POST['undelete']) && isset($_POST['kll_id']) && $page->isAdmin()) {
 	$kll_id = intval($_POST['kll_id']);
 	$qry = DBFactory::getDBQuery();
 	$qry->execute("DELETE FROM kb3_mails WHERE kll_id = ".$kll_id);
-	if (isset($_POST['killmail'])) $html = post();
-	else $html = "Mail lock has been removed.";
-
-}
-else if (isset($_POST['killmail']))
-{
+	if (isset($_POST['killmail'])) {
+		$html = post();
+	} else {
+		$html = "Mail lock has been removed.";
+	}
+} else if (isset($_POST['killmail'])) {
 	$html = post();
 }
-if(isset($html)) $smarty->assign('error', $html);
+if (isset($html)) {
+	$smarty->assign('error', $html);
+}
 $smarty->assign('isadmin', $page->isAdmin());
 $smarty->assign('post_forbid', config::get('post_forbid'));
 $smarty->assign('post_oog_forbid', config::get('post_oog_forbid'));
@@ -32,67 +33,56 @@ $page->generate();
 function post()
 {
 	global $page;
-    if (config::get("post_password") == '' || crypt($_POST['password'],config::get("post_password")) == config::get("post_password") || $page->isAdmin())
-    {
-        $parser = new Parser($_POST['killmail']);
+	if (config::get("post_password") == ''
+			|| crypt($_POST['password'], config::get("post_password"))
+			== config::get("post_password")
+			|| $page->isAdmin()) {
+		$parser = new Parser($_POST['killmail']);
 
-        // Filtering
-        if (config::get('filter_apply'))
-        {
-            $filterdate = config::get('filter_date');
-            $year = substr($_POST['killmail'], 0, 4);
-            $month = substr($_POST['killmail'], 5, 2);
-            $day = substr($_POST['killmail'], 8, 2);
-            $killstamp = mktime(0, 0, 0, $month, $day, $year);
-            if ($killstamp < $filterdate)
-            {
-                $killid = -3;
-            }
-            else
-            {
-                $killid = $parser->parse(true, null, false);
-            }
-        }
-        else
-        {
-            $killid = $parser->parse(true, null, false);
-        }
+		// Filtering
+		if (config::get('filter_apply')) {
+			$filterdate = config::get('filter_date');
+			$year = substr($_POST['killmail'], 0, 4);
+			$month = substr($_POST['killmail'], 5, 2);
+			$day = substr($_POST['killmail'], 8, 2);
+			$killstamp = mktime(0, 0, 0, $month, $day, $year);
+			if ($killstamp < $filterdate) {
+				$killid = -3;
+			} else {
+				$killid = $parser->parse(true, null, false);
+			}
+		} else {
+			$killid = $parser->parse(true, null, false);
+		}
 
-        if ($killid <= 0)
-        {
-            if ($killid == 0)
-            {
-                $html = "Killmail is malformed.<br/>";
-                if ($errors = $parser->getError())
-                {
-                    foreach ($errors as $error)
-                    {
-                        $html .= 'Error: '.$error[0];
-                        if ($error[1])
-                        {
-                            $html .= ' The text leading to this error was: "'.$error[1].'"';
-                        }
-                        $html .= '<br/>';
-                    }
-                }
-            }
-            elseif ($killid == -1)
-            {
-                $html = "That killmail has already been posted <a href=\"?a=kill_detail&kll_id=".$parser->getDupeID()."\">here</a>.";
-            }
-            elseif ($killid == -2)
-            {
-                $html = "You are not authorized to post this killmail.";
-            }
-            elseif ($killid == -3)
-            {
-                $filterdate = kbdate("j F Y", config::get("filter_date"));
-                $html = "You are not allowed to post killmails older than $filterdate.";
-            }
-			elseif ($killid == -4)
-			{
-				$html = "That mail has been deleted. Kill id was ".$parser->getDupeID();
-				if($page->isAdmin()) $html .= '<br />
+		if ($killid <= 0) {
+			if ($killid == 0) {
+				$html = "Killmail is malformed.<br/>";
+				if ($errors = $parser->getError()) {
+					foreach ($errors as $error) {
+						$html .= 'Error: '.$error[0];
+						if ($error[1]) {
+							$html .= ' The text leading to this error was: "'
+									.$error[1].'"';
+						}
+						$html .= '<br/>';
+					}
+				}
+			} elseif ($killid == -1) {
+				$html = "That killmail has already been posted <a href=\""
+						."?a=kill_detail&kll_id=".$parser->getDupeID()
+						."\">here</a>.";
+			} elseif ($killid == -2) {
+				$html = "You are not authorized to post this killmail.";
+			} elseif ($killid == -3) {
+				$filterdate = kbdate("j F Y", config::get("filter_date"));
+				$html = "You are not allowed to post killmails older than"
+						." $filterdate.";
+			} elseif ($killid == -4) {
+				$html = "That mail has been deleted. Kill id was "
+						.$parser->getDupeID();
+				if ($page->isAdmin())
+						$html .= '<br />
 <form id="postform" name="postform" class="f_killmail" method="post" action="'.KB_HOST.'/?a=post">
 	<input type="hidden" name="killmail" id="killmail" value = "'.htmlentities($_POST['killmail']).'"/>
 	<input type="hidden" name="kll_id" id="kill_id" value = "'.$parser->getDupeID().'"/>
@@ -100,44 +90,36 @@ function post()
 <input id="submit" name="submit" type="submit" value="Undelete" />
 </form>';
 			}
-        }
-        else
-        {
-            if (config::get('post_mailto') != "")
-            {
-                $mailer = new PHPMailer();
-                $kill = new Kill($killid);
+		} else {
+			if (config::get('post_mailto') != "") {
+				$mailer = new PHPMailer();
+				$kill = new Kill($killid);
 
-                if (!$server = config::get('post_mailserver'))
-                {
-                    $server = 'localhost';
-                }
-                $mailer->From = "mailer@".config::get('post_mailhost');
-                $mailer->FromName = config::get('post_mailhost');
-                $mailer->Subject = "Killmail #" . $killid;
-                $mailer->Host = $server;
-                $mailer->Port = 25;
-                $mailer->Helo = $server;
-                $mailer->Mailer = "smtp";
-                $mailer->AddReplyTo("no_reply@".config::get('post_mailhost'), "No-Reply");
-                $mailer->Sender = "mailer@".config::get('post_mailhost');
-                $mailer->Body = $_POST['killmail'];
-                $mailer->AddAddress(config::get('post_mailhost'));
-                $mailer->Send();
-            }
+				if (!$server = config::get('post_mailserver')) {
+					$server = 'localhost';
+				}
+				$mailer->From = "mailer@".config::get('post_mailhost');
+				$mailer->FromName = config::get('post_mailhost');
+				$mailer->Subject = "Killmail #".$killid;
+				$mailer->Host = $server;
+				$mailer->Port = 25;
+				$mailer->Helo = $server;
+				$mailer->Mailer = "smtp";
+				$mailer->AddReplyTo("no_reply@".config::get('post_mailhost'),
+						"No-Reply");
+				$mailer->Sender = "mailer@".config::get('post_mailhost');
+				$mailer->Body = $_POST['killmail'];
+				$mailer->AddAddress(config::get('post_mailhost'));
+				$mailer->Send();
+			}
 
-            logger::logKill($killid);
-//			$qry = DBFactory::getDBQuery();;
-//            $qry->execute("insert into kb3_log (log_kll_id, log_site, log_ip_address, log_timestamp) values(".
-//                    $killid.",'".KB_SITE."','".getip()."', now())");
+			logger::logKill($killid);
 
-            header("Location: ".KB_HOST."/?a=kill_detail&kll_id=".$killid);
-            exit;
-        }
-    }
-    else
-    {
-        $html = "Invalid password.";
-    }
+			header("Location: ".edkURI::page('kill_detail', $killid, 'kll_id'));
+			exit;
+		}
+	} else {
+		$html = "Invalid password.";
+	}
 	return $html;
 }
