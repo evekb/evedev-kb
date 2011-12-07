61dba3ef58e47ffb3055ecb0c257d3e42a2f892d
Kovell <kovellia@gmail.com>
2011-12-08 00:24:42 +1100

    Show clearer error messages in killllog. Add error messages to API.

--- common/includes/api/class.killlog.php
+++ common/includes/api/class.killlog.php
@@ -11,7 +11,7 @@
  */
 class API_KillLog extends API
 {
-    function Import($name, $id, $key, $flags)
+	function Import($name, $id, $key, $flags)
 	{
 		$output = "";
 
@@ -19,52 +19,53 @@ class API_KillLog extends API
 		if ( $flags & KB_APIKEY_BADAUTH || $flags & KB_APIKEY_EXPIRED ) {
 			return; // skip bad keys
 		}
-		
+
 		// also skip legacy keys now
 		if( $flags & KB_APIKEY_LEGACY)
 			return;
 
 		// reduces strain on DB
-		if(function_exists("set_time_limit"))
+		if(function_exists("set_time_limit")) {
       		set_time_limit(0);
-
+		}
 		$lastdatakillid = 1;
 		$currentdatakillid = 0;
 
 		$logsource = "New XML";
 		// Load new XML
 		$output = "<i>Downloading latest XML file for $name</i><br><br>";
-		
+
 		$accts = new API_Account();
 		$characters = $accts->fetch($id, $key);
 		$posted = array();
 		$skipped = array();
 
-		foreach($characters as $char) {
-			$output .= "Processing " . $char['charID'] . "<br><br>";
+		foreach ($characters as $char) {
+			$output .= "Processing ".$char['characterName']."<br><br>";
 			$currentkill = 0;
 			$lastkill = -1;
 			while ($lastkill != $currentkill) {
 				$lastkill = $currentkill;
-				$args = array("characterID" => $char['charID']);
-				if($lastkill) {
+				$args = array("characterID" => $char['characterID']);
+				if ($lastkill) {
 					$args["beforeKillID"] = $lastkill;
 				}
-				
-				if ( $flags & KB_APIKEY_CORP ) {
-					$killLog = $this->CallAPI( "corp", "KillLog", $args, $id, $key );
-				}
-				if ( $flags & KB_APIKEY_CHAR ) {
-					$killLog = $this->CallAPI( "char", "KillLog", $args, $id, $key );
+				if ($flags & KB_APIKEY_CORP) {
+					$killLog = $this->CallAPI("corp", "KillLog", $args, $id, $key);
+				} else if ($flags & KB_APIKEY_CHAR) {
+					$killLog = $this->CallAPI("char", "KillLog", $args, $id, $key);
+				} else {
+					$output .= "<div class='block-header2'>Key does not have access to KillLog</div>";
+					break;
 				}
 
 				if ($this->getError() === null) {
 					// Get oldest kill
 					$currentkill = 0;
-					$sxe = simplexml_load_string($this->pheal->xml);					
-					foreach($sxe->result->rowset->row as $row) {
-						if($currentkill < (int)$row['killID']) {
-							$currentkill = (int)$row['killID'];
+					$sxe = simplexml_load_string($this->pheal->xml);
+					foreach ($sxe->result->rowset->row as $row) {
+						if ($currentkill < (int) $row['killID']) {
+							$currentkill = (int) $row['killID'];
 						}
 					}
 				}
@@ -74,10 +75,10 @@ class API_KillLog extends API
 						// Check if we just need to skip back a few kills
 						// i.e. first page of kills is already fetched.
 						$pos = strpos($this->pheal->xml, "Expected beforeKillID [");
-						if($pos) {
+						if ($pos) {
 							$pos += 23;
 							$pos2 = strpos($this->pheal->xml, "]", $pos);
-							$currentkill = (int)substr($this->pheal->xml, $pos, $pos2 - $pos);
+							$currentkill = (int) substr($this->pheal->xml, $pos, $pos2 - $pos);
 						}
 					} else if (!$posted && !$skipped) {
 						// Something went wrong and no kills were found.
@@ -85,35 +86,37 @@ class API_KillLog extends API
 						$logtype = "Cron Job";
 
 						$qry->execute("insert into kb3_apilog	values( '".KB_SITE."', '"
-								.addslashes($name)."',"
-								."0, "
-								."0, "
-								."0, "
-								."0, "
-								."0, '"
-								."Error','"
-								."Cron Job','"
-								. $this->getError() . "', "
-								."UTC_TIMESTAMP() )");						
-						return $output;
+										.addslashes($name)."',"
+										."0, "
+										."0, "
+										."0, "
+										."0, "
+										."0, '"
+										."Error','"
+										."Cron Job','"
+										.$this->getError()."', "
+										."UTC_TIMESTAMP() )");
+						$output .= "<div class='block-header2'>".$this->getMessage()
+										."</div>";
+						break;
 					} else {
 						// We found kills!
 						$qry = DBFactory::getDBQuery();
 						$logtype = "Cron Job";
 
 						$qry->execute("insert into kb3_apilog values( '".KB_SITE."', '"
-								.addslashes($name)."',"
-								.count($posted).","
-								."0 ,"
-								.count($skipped).","
-								."0 ,"
-								.(count($posted) + count($skipped)).",'"
-								."New XML','"
-								."Cron Job','"
-								. ($this->getError() == 119 ? 0: $this->getError()) . "', "
-								."UTC_TIMESTAMP() )");
+										.addslashes($name)."',"
+										.count($posted).","
+										."0 ,"
+										.count($skipped).","
+										."0 ,"
+										.(count($posted) + count($skipped)).",'"
+										."New XML','"
+										."Cron Job','"
+										.($this->getError() == 119 ? 0 : $this->getError())."', "
+										."UTC_TIMESTAMP() )");
 
-						return $output;
+						break;
 					}
 				}
 
@@ -125,18 +128,20 @@ class API_KillLog extends API
 
 				$posted = array_merge($posted, $feedfetch->getPosted());
 				$skipped = array_merge($skipped, $feedfetch->getSkipped());
- 
+
 				$output .= "<div class='block-header2'>"
-						.count($posted)." kill".(count($posted) == 1 ? "" : "s")." posted, "
-						.count($skipped)." skipped from feed: ".$id.".<br></div>";
-				$output .= "<div class='block-header2'>Posted</div>\n";
-				foreach($posted as $killid) {
-					$output .= "<div><a href='"
-							.edkURI::page('kill_detail', $killid[2], 'kll_id')
-							."'>Kill ".$killid[0]."</a></div>";
+								.count($posted)." kill".(count($posted) == 1 ? "" : "s")." posted, "
+								.count($skipped)." skipped from feed: ".$id.".<br></div>";
+				if (count($posted)) {
+					$output .= "<div class='block-header2'>Posted</div>\n";
+					foreach ($posted as $killid) {
+						$output .= "<div><a href='"
+										.edkURI::page('kill_detail', $killid[2], 'kll_id')
+										."'>Kill ".$killid[0]."</a></div>";
+					}
 				}
 			}
-		}			
-        return $output;
-    }
+		}
+		return $output;
+	}
 }
