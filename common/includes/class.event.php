0ed49f51bf61fba28b0bfd0d33f63f7812838ffa
Kovell <kovellia@gmail.com>
2011-12-17 14:00:56 +1100

    Modify event class to be more efficient and warn of errors earlier.

--- common/includes/class.event.php
+++ common/includes/class.event.php
@@ -13,56 +13,60 @@ class event
 {
 	private static $events = array();
 
-	function event()
-	{
-		trigger_error('The class "event" may only be invoked statically.', E_USER_ERROR);
-	}
-
+	/**
+	 * Register a callback for an event. Callbacks are of the form
+	 * array('Class', 'Method');
+	 *
+	 * @param string $event Event name.
+	 * @param array $callback Callback to be called when the event is triggered
+	 *
+	 * @return boolean True on success, false on failure. 
+	 */
 	public static function register($event, $callback)
 	{
-		if (is_array($callback))
-		{
-			if (is_object($callback[0]))
-			{
-				trigger_error('The supplied callback has to point to a static method.', E_USER_WARNING);
-				return;
+		if (is_array($callback)) {
+			if (is_object($callback[0])) {
+				trigger_error('The supplied callback has to point to a static method.',
+								E_USER_WARNING);
+				return false;
 			}
-
-			// store callbacks as 'object::function'
-			$callback = $callback[0].'::'.$callback[1];
+		} else if (!strpos($callback, '::')) {
+			trigger_error('The supplied callback "'.$callback.'" has to point to a static method.',
+							E_USER_WARNING);
+			return false;
+		} else {
+			// Accept callback in string form 'Class::method' for legacy events.
+			$callback = explode('::', $callback);
 		}
-		if (!strpos($callback, '::'))
-		{
-			trigger_error('The supplied callback "'.$callback.'" has to point to a static method.', E_USER_WARNING);
-			return;
+		if (!is_callable($callback, false, $callable_name)) {
+			trigger_error('The stored event handler "'.$event
+					.'" is not callable (CB: "'.join("::", $callback).'").',
+							E_USER_WARNING);
+			return false;
 		}
 
-		// we store the event callbacks reverse so you need one function for every event
-		event::_put($callback, $event);
+		event::_put($event, $callback);
+		return true;
 	}
 
+	/**
+	 * Call an event. Trigger any callbacks registered for this event.
+	 *
+	 * @param string $event event name.
+	 * @param mixed $object Object to pass to the callback.
+	 */
 	public static function call($event, &$object)
 	{
-		foreach (self::$events as $callback => $c_event)
-		{
-			// if the callback registered to the calling event we'll try to use his callback
-			if ($event == $c_event)
-			{
-				$cb = explode('::', $callback);
-				if (is_callable($cb))
-				{
-					call_user_func_array($cb, array(&$object));
-				}
-				else
-				{
-					trigger_error('The stored event handler "'.$c_event.'" is not callable (CB: "'.$callback.'").', E_USER_WARNING);
-				}
+		if (isset(self::$events[$event])) {
+			foreach (self::$events[$event] as $callback){
+				// if the callback registered to the calling event we'll try to use his callback
+				call_user_func_array($callback, array(&$object));
 			}
 		}
 	}
 
 	private static function _put($key, $data)
 	{
-		self::$events[$key] = $data;
+		self::$events[$key][] = $data;
 	}
 }
