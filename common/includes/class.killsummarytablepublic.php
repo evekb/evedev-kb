b9b563440224dcb70dcc7786a86a12715d9dd072
Kovell <kovellia@gmail.com>
2012-01-22 13:19:40 +1100

    Update formatting. Tabs and spaces were mixed. :(

--- common/includes/class.killsummarytablepublic.php
+++ common/includes/class.killsummarytablepublic.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * $Date$
  * $Revision$
@@ -11,110 +12,107 @@
  */
 class KillSummaryTablePublic extends KillSummaryTable
 {
-    function KillSummaryTablePublic($klist = null)
-    {
-        $this->klist = $klist;
-		$this->verbose = true;
-    }
 
-    // do it faster, baby!
-    function getkills()
-    {
-        $this->entry = array();
-        // as there is no way to do this elegant in sql
-        // i'll keep it in php
-        $sql = "select scl_id, scl_class from kb3_ship_classes
+	function KillSummaryTablePublic($klist = null)
+	{
+		$this->klist = $klist;
+		$this->verbose = true;
+	}
+
+	// do it faster, baby!
+	function getkills()
+	{
+		$this->entry = array();
+		// as there is no way to do this elegant in sql
+		// i'll keep it in php
+		$sql = "select scl_id, scl_class from kb3_ship_classes
                where scl_class not in ('Drone','Unknown') order by scl_class";
 
-        $qry = DBFactory::getDBQuery();
-        $qry->execute($sql);
-        while ($row = $qry->getRow())
-        {
-            $this->entry[$row['scl_class']] = array('id' => $row['scl_id'],
-                                                     'kills' => 0, 'kills_isk' => 0);
-        }
-		$startdate = makeStartDate($this->weekno, $this->yearno, $this->monthno, $this->startweekno, $this->startDate);
-		$enddate = makeEndDate($this->weekno, $this->yearno, $this->monthno, $this->endDate);
+		$qry = DBFactory::getDBQuery();
+		$qry->execute($sql);
+		while ($row = $qry->getRow()) {
+			$this->entry[$row['scl_class']] = array('id' => $row['scl_id'],
+				'kills' => 0, 'kills_isk' => 0);
+		}
+		$startdate = makeStartDate($this->weekno, $this->yearno, $this->monthno,
+				$this->startweekno, $this->startDate);
+		$enddate = makeEndDate($this->weekno, $this->yearno, $this->monthno,
+				$this->endDate);
 
-        $sql = 'SELECT count(kll.kll_id) AS knb, scl_id, scl_class,';
+		$sql = 'SELECT count(kll.kll_id) AS knb, scl_id, scl_class,';
 
 		$sql .= ' sum(kll_isk_loss) AS kisk FROM kb3_kills kll
                     INNER JOIN kb3_ships shp ON ( shp.shp_id = kll.kll_ship_id )';
 
-        $sql .= ' INNER JOIN kb3_ship_classes scl ON ( scl.scl_id = shp.shp_class )';
+		$sql .= ' INNER JOIN kb3_ship_classes scl ON ( scl.scl_id = shp.shp_class )';
 
-        if ($this->inv_crp)
-        {
-            $sql .= ' inner join kb3_inv_detail ind on ( ind.ind_crp_id in ( '.join(',', $this->inv_crp).' ) and kll.kll_id = ind.ind_kll_id ) ';
-        }
-        elseif ($this->inv_all)
-        {
-            $sql .= ' inner join kb3_inv_detail ind on ( ind.ind_all_id in ( '.join(',', $this->inv_all).' ) and kll.kll_id = ind.ind_kll_id ) ';
-        }
+		if ($this->inv_crp) {
+			$sql .= ' inner join kb3_inv_detail ind on ( ind.ind_crp_id in ( '.join(',',
+							$this->inv_crp).' ) and kll.kll_id = ind.ind_kll_id ) ';
+		} else if ($this->inv_all) {
+			$sql .= ' inner join kb3_inv_detail ind on ( ind.ind_all_id in ( '.join(',',
+							$this->inv_all).' ) and kll.kll_id = ind.ind_kll_id ) ';
+		}
 		$sqlop = " WHERE ";
-		if($this->system)
-		{
+		if ($this->system) {
 			$sql .= $sqlop." kll.kll_system_id in ".join(',', $this->system)." ";
 		}
-		if($startdate)
-		{
-			$sql .= $sqlop." kll.kll_timestamp >= '".gmdate('Y-m-d H:i',$startdate)."' ";
+		if ($startdate) {
+			$sql .= $sqlop." kll.kll_timestamp >= '".gmdate('Y-m-d H:i', $startdate)."' ";
 			$sqlop = " AND ";
 		}
-		if($enddate) $sql .= $sqlop." kll.kll_timestamp <= '".gmdate('Y-m-d H:i',$enddate)."' ";
-        $sql .= 'GROUP BY shp.shp_class';
-		if($this->inv_crp || $this->inv_all) $sql .= ', kll.kll_id';
+		if ($enddate) {
+			$sql .= $sqlop." kll.kll_timestamp <= '".gmdate('Y-m-d H:i', $enddate)."' ";
+		}
+		$sql .= 'GROUP BY shp.shp_class';
+		if ($this->inv_crp || $this->inv_all) {
+			$sql .= ', kll.kll_id';
+		}
 		$sql .= ' order by scl.scl_class';
 
-        $qry = DBFactory::getDBQuery();
-        $qry->execute($sql);
-        while ($row = $qry->getRow())
-        {
-            $this->entry[$row['scl_class']]['kills'] = $row['knb'];
-            $this->entry[$row['scl_class']]['kills_isk'] = $row['kisk'];
-            $this->tkcount += $row['knb'];
-            $this->tkisk += $row['kisk'];
-        }
-    }
+		$qry = DBFactory::getDBQuery();
+		$qry->execute($sql);
+		while ($row = $qry->getRow()) {
+			$this->entry[$row['scl_class']]['kills'] = $row['knb'];
+			$this->entry[$row['scl_class']]['kills_isk'] = $row['kisk'];
+			$this->tkcount += $row['knb'];
+			$this->tkisk += $row['kisk'];
+		}
+	}
 
-    function generate()
-    {
-        if ($this->klist)
-        {
-            $entry = array();
-            // build array
-            $sql = "select scl_id, scl_class
+	function generate()
+	{
+		if ($this->klist) {
+			$entry = array();
+			// build array
+			$sql = "select scl_id, scl_class
                     from kb3_ship_classes
                    where scl_class not in ( 'Drone', 'Unknown' )
                   order by scl_class";
 
-            $qry = DBFactory::getDBQuery();
-            $qry->execute($sql) or die($qry->getErrorMsg());
-            while ($row = $qry->getRow())
-            {
-                if (!$row['scl_id']) {
-                    continue;
+			$qry = DBFactory::getDBQuery();
+			$qry->execute($sql) or die($qry->getErrorMsg());
+			while ($row = $qry->getRow()) {
+				if (!$row['scl_id']) {
+					continue;
 				}
 
-                $entry[$row['scl_class']]['id'] = $row['scl_id'];
-                $entry[$row['scl_class']]['kills'] = 0;
-                $entry[$row['scl_class']]['kills_isk'] = 0;
-            }
+				$entry[$row['scl_class']]['id'] = $row['scl_id'];
+				$entry[$row['scl_class']]['kills'] = 0;
+				$entry[$row['scl_class']]['kills_isk'] = 0;
+			}
 
-            while ($kill = $this->klist->getKill())
-            {
-                $classname = $kill->getVictimShipClassName();
-                $entry[$classname]['kills']++;
-                $entry[$classname]['kills_isk'] += $kill->getISKLoss();
-                $this->tkcount++;
-                $this->tkisk += $kill->getISKLoss();
-            }
-        }
-        else
-        {
-            $this->getkills();
-            $entry = &$this->entry;
-        }
+			while ($kill = $this->klist->getKill()) {
+				$classname = $kill->getVictimShipClassName();
+				$entry[$classname]['kills']++;
+				$entry[$classname]['kills_isk'] += $kill->getISKLoss();
+				$this->tkcount++;
+				$this->tkisk += $kill->getISKLoss();
+			}
+		} else {
+			$this->getkills();
+			$entry = &$this->entry;
+		}
 
 		// Don't count noobships.
 		$num = count($entry) - 1;
@@ -138,14 +136,18 @@ class KillSummaryTablePublic extends KillSummaryTable
 			$qrystring .= "&amp;";
 		}
 
-		foreach ($entry as $k => $v)
-		{
-			if($v['id'] == 3) continue;
+		foreach ($entry as $k => $v) {
+			if ($v['id'] == 3) {
+				continue;
+			}
 			$v['break'] = 0;
-			if($_GET['scl_id'] && $_GET['scl_id'] == $v['id']) $v['hl'] = 1;
-			else $v['hl'] = 0;
+			if ($_GET['scl_id'] && $_GET['scl_id'] == $v['id']) {
+				$v['hl'] = 1;
+			} else {
+				$v['hl'] = 0;
+			}
 			$v['qry'] = $qrystring;
-			$v['kisk'] = round($v['kills_isk']/1000000, 2);
+			$v['kisk'] = round($v['kills_isk'] / 1000000, 2);
 			$v['name'] = $k;
 
 			$summary[] = $v;
@@ -161,21 +163,21 @@ class KillSummaryTablePublic extends KillSummaryTable
 		$smarty->assign('verbose', $this->verbose);
 		$smarty->assign('filter', $this->filter);
 
-		if (config::get('summarytable_summary'))
-		{
+		if (config::get('summarytable_summary')) {
 			$smarty->assign('summarysummary', 1);
 			$smarty->assign('efficiency', 0);
-			$smarty->assign('kiskB', round($this->tkisk/1000000000, 2));
-			$smarty->assign('kiskM', round($this->tkisk/1000000, 2));
+			$smarty->assign('kiskB', round($this->tkisk / 1000000000, 2));
+			$smarty->assign('kiskM', round($this->tkisk / 1000000, 2));
 			$smarty->assign('kcount', $this->tkcount);
 		}
 
-		if (edkURI::getArg('scl_id')){
-			$smarty->assign('clearfilter',$clearfilter);
+		if (edkURI::getArg('scl_id')) {
+			$smarty->assign('clearfilter', $clearfilter);
 		}
 
 		$html .= $smarty->fetch(get_tpl('summarytable'));
 
-        return $html;
-    }
+		return $html;
+	}
+
 }
