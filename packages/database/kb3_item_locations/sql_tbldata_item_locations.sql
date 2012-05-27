51c6cac752c19b02470cb947ef86468955235b51
Paul Richards <paul@mantisforge.org>
2012-05-27 11:58:27 +0100

    Store the database update files uncompressed in git - we can always gzip them in a release.

---
+++ packages/database/kb3_item_locations/sql_tbldata_item_locations.sql
@@ -0,0 +1,2 @@
+TRUNCATE `kb3_item_locations`;
+INSERT IGNORE INTO `kb3_item_locations` VALUES('1', 'Fitted - High slot'), ('2', 'Fitted - Medium slot'), ('3', 'Fitted - Low slot'), ('4', 'Cargo'), ('5', 'Rig Slot'), ('6', 'Drone Bay'), ('7', 'Subsystem Slot'), ('8', 'Implant'), ('9', 'Copy');
