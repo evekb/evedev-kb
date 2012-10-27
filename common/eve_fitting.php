c6e6f2822a77aaac37546b8e893ecafad122b057
Paul Richards <paul@mantisforge.org>
2012-10-27 22:22:13 +0100

    remove commented out code

--- common/eve_fitting.php
+++ common/eve_fitting.php
@@ -101,51 +101,6 @@ $slots = array(3 => "low slot",
 	7 => "subsystem slot",
 	6 => "drone bay");
 
-
-// Some tools require xml formatted with indents.
-// So let's do this the ugly way
-/*
-$xml = "<?xml version=\"1.0\" ?>
-	<fittings>
-	</fittings>\n";
-
-$sxe = new SimpleXMLElement($xml);
-$fittingxml = $sxe->addChild('fitting');
-$fittingxml->addAttribute('name', $killtitle);
-$desc = $fittingxml->addChild('description');
-$desc->addAttribute("value", "From ".KB_HOST."?a=kill_detail&amp;kll_id=".$kll_id);
-$shiptype = $fittingxml->addChild('shipType');
-$shiptype->addAttribute('value', $shipname);
-
-foreach ($slots as $i => $empty)
-{
-	if (!empty($fitting_array[$i]))
-	{
-		$usedslots = 0;
-		foreach ($fitting_array[$i] as $k => $a_item)
-		{
-			$item = $a_item['Name'];
-			$hardware = $fittingxml->addChild('hardware');
-			if($i == 6)
-			{
-				$hardware->addAttribute('slot', $slots[$i]);
-				$hardware->addAttribute('type', $a_item['Name']);
-				$hardware->addAttribute('quantity', '1');
-			}
-			else
-			{
-				$hardware->addAttribute('slot', $slots[$i].' '.$usedslots);
-				$hardware->addAttribute('type', $a_item['Name']);
-			}
-
-			$usedslots++;
-		}
-	}
-}
-
-echo $sxe->asXML();
-*/
-
 $xml = "<?xml version=\"1.0\" ?>
 	<fittings>\n";
 
