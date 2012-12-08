07111db04cc43f8065cd78954c34666a73857741
Paul Richards <paul@mantisforge.org>
2012-12-08 01:09:39 +0000

    Fix schema update to run on db's with latest datadump

--- update/038/update.php
+++ update/038/update.php
@@ -14,9 +14,13 @@ function update038()
 		$sql = 'ALTER TABLE `kb3_dgmtypeattributes` CHANGE `typeID` `typeID` INT( 11 ) NOT NULL DEFAULT \'0\';';
 		$qry->execute($sql);
 
-		$sql = 'ALTER TABLE `kb3_moons` CHANGE `itemID` `moo_id` INT( 11 ) NOT NULL ,
+		$qry->execute("SHOW COLUMNS FROM kb3_moons LIKE 'moo_id'");
+		if(!$qry->recordCount())
+		{
+			$sql = 'ALTER TABLE `kb3_moons` CHANGE `itemID` `moo_id` INT( 11 ) NOT NULL ,
 					CHANGE `itemName` `moo_name` VARCHAR( 127 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;';
-		$qry->execute($sql);
+			$qry->execute($sql);
+		}
 		
 		config::set("DBUpdate", "038");
 		$qry->execute("INSERT INTO kb3_config (cfg_site, cfg_key, cfg_value) SELECT cfg_site, 'DBUpdate', '038' FROM kb3_config GROUP BY cfg_site ON DUPLICATE KEY UPDATE cfg_value = '038'");
