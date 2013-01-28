a89bc5f294867566929165ab410a5c3724d97c36
idun <idun@theexecutioners.org>
2013-01-27 19:09:07 -0600

    Convert kill_mail popup to template system Add event hook for kill_mail rendering

--- common/kill_mail.php
+++ common/kill_mail.php
@@ -1,27 +1,12 @@
-<?php
+popup| <?php
 /**
  * @package EDK
  */
-
 $kll_id = (int)edkURI::getArg('kll_id', 1);
 $kill = Cacheable::factory('Kill', $kll_id);
+$html = $kill->getRawMail();
+
+event::call("killmail_popup", $html);
+$smarty->assign('rawMail', $html);
+echo $smarty->fetch(get_tpl("kill_mail"));
 ?>
-popup|<form>
-<table class="popup-table" height="100%" width="355px">
-<tr>
-	<td align="center"><strong>Original Killmail</strong></td>
-</tr>
-<tr>
-	<td align="center"><input type="button" value="Close" onClick="ReverseContentDisplay('popup');"></td>
-</tr>
-<tr>
-<td valign="top" align="center">
-<textarea class="killmail" name="killmail" cols="60" rows="30" readonly="readonly">
-<?php echo $kill->getRawMail();?></textarea></td></tr>
-<tr><td align="center"><input type="button" value="Select All" onClick="this.form.killmail.select();this.form.killmail.focus(); document.execCommand('Copy')">&nbsp;<input type="button" value="Close" onClick="ReverseContentDisplay('popup');"></td>
-</tr>
-
-</table>
-</form>
-
-
