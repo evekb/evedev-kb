4e9e39ec3df22ce6dbe21e7cfa73dd02ff224118
Salvoxia <Salvoxia@blindfish.info>
2014-09-27 01:53:43 +0200

    Updated CCP DB to Oceanus 1.0 (105658)
    Updated the CCP DB package to Oceans 1.0 build 105658.
    Updated the killboard version to 4.2.4.0 (Oceanus 1.0)

--- common/includes/constants.php
+++ common/includes/constants.php
@@ -21,10 +21,10 @@ define('IMG_SERVER', "https://image.eveonline.com");
 
 // current version: major.minor.sub.ccpDBupdateNo
 // even numbers for minor = development version
-define('KB_VERSION', '4.2.3.0');
-define('KB_RELEASE', '(Hyperion 1.0)');
-define('KB_CCP_DB_VERSION', '101505');
-define('KB_CCP_DB_DATE', 'Aug 26, 2014');
+define('KB_VERSION', '4.2.4.0');
+define('KB_RELEASE', '(Oceanus 1.0)');
+define('KB_CCP_DB_VERSION', '105658');
+define('KB_CCP_DB_DATE', 'Sep 30, 2014');
 define('ID_FEED_VERSION', 1.20);
 define('ZKB_FETCH_VERSION', 1.0);
 define('KB_APIKEY_LEGACY', 1);
