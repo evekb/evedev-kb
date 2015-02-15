4a65fae5f9315e4001ea7dfe0ba99322b3f6bc7d
Salvoxia <Salvoxia@blindfish.info>
2015-02-15 17:03:01 +0100

    Updated KB release to Tiamat 1.0

--- common/includes/constants.php
+++ common/includes/constants.php
@@ -21,7 +21,7 @@ define('IMG_SERVER', "https://image.eveonline.com");
 // current version: major.minor.sub.ccpDBupdateNo
 // even numbers for minor = development version
 define('KB_VERSION', '4.2.8.0');
-define('KB_RELEASE', '(Proteus 1.0)');
+define('KB_RELEASE', '(Tiamat 1.0)');
 define('KB_CCP_DB_VERSION', '109795');
 define('KB_CCP_DB_DATE', 'Feb 17, 2015');
 define('ID_FEED_VERSION', 1.30);
