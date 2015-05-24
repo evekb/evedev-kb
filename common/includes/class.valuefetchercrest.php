c4cf23e8c2195110a3930d7b844fb259e37396a8
Salvoxia <Salvoxia@blindfish.info>
2015-05-24 16:42:05 +0200

    Enhancement: Special Item Prices
    Added handling of prices for special items, for which their actual value
    are not nearly met by the CREST endpoint (e.g. Tournament Ships or Super
    Capitals).

--- common/includes/class.valuefetchercrest.php
+++ common/includes/class.valuefetchercrest.php
@@ -71,6 +71,7 @@ class ValueFetcherCrest
             {
                 $itemPrice = @(float)$item->adjustedPrice;
             }
+            
 
             // Make sure we still have data
             if (!$itemPrice || !$typeId) 
@@ -78,6 +79,9 @@ class ValueFetcherCrest
                 $numberOfItemsSkipped++;
                 continue;
             }
+            
+            // handle item values not correctly represented by market/adjustedPrice, e.g. super capitals
+            $itemPrice = self::handleSpecialItemValues($typeId, $itemPrice);
 
 
             // Insert new values into the database and update the old
@@ -102,5 +106,132 @@ class ValueFetcherCrest
         $qry->execute($querytext);
         config::set('lastfetch', time());
         return $numberOfItemsUpdated;
+    }
+    
+    /**
+     *  handle item values not correctly represented by market/adjustedPrice, e.g. super capitals
+     * @param int $typeId the typeID for the item
+     * @param double $itemPrice the item price returned by CREST
+     */
+    static function handleSpecialItemValues($typeId, $itemPrice)
+    {
+        // Supercarrier => 20b
+        if(in_array($typeId, array(3628, 22852, 23913, 23917, 23919)))
+        {
+           return "20000000000"; 
+        }
+        
+        // Revenant => 100b
+        else if($typeId == 3514)
+        {
+            return "100000000000";
+        }
+        
+        // Titans => 100b
+        else if(in_array($typeId, array(671, 3764, 11567, 23773)))
+        {
+            return "100000000000";
+        }
+
+        // Source: http://www.reddit.com/r/Eve/comments/1znr1l/etana_still_keeps_its_value/cfvc9yu
+        // Utu => 50b
+        else if($typeId == 2834)
+        {
+            return "50000000000";
+        }
+        
+        // Malice => 75b
+        else if($typeId == 3516)
+        {
+            return "75000000000";
+        }
+        
+        // Erinye => 75b
+        else if($typeId == 11375)
+        {
+            return "75000000000";
+        }
+        
+        // Chremoas => 120b
+        else if($typeId == 33397)
+        {
+            return "120000000000";
+        }
+        
+        // Cambion => 105b
+        else if($typeId == 32788)
+        {
+            return "105000000000";
+        }
+        
+        // Adrestia => 100b
+        else if($typeId == 2836)
+        {
+            return "100000000000";
+        }
+        
+        // Vangel => 60b
+        else if($typeId == 3518)
+        {
+            return "60000000000";
+        }
+        
+        // Etana => 70b
+        else if($typeId == 32790)
+        {
+            return "70000000000";
+        }
+        
+        // Moracha => 100b
+        else if($typeId == 33395)
+        {
+            return "100000000000";
+        }
+        
+        // Mimir => 80b
+        else if($typeId == 32209)
+        {
+            return "80000000000";
+        }
+        
+        // Chameleon => 120b
+        else if($typeId == 33675)
+        {
+            return "120000000000";
+        }
+        
+        // Whiptail => 100b
+        else if($typeId == 33673)
+        {
+            return "100000000000";
+        }
+        
+        // Freki => 70b
+        else if($typeId == 32207)
+        {
+            return "70000000000";
+        }
+        
+        // Polaris => 1t
+        else if($typeId == 9860)
+        {
+            return "1000000000000";
+        }
+        
+        // Cockroach => 1t
+        else if($typeId == 11019)
+        {
+            return "1000000000000";
+        }
+        
+        // Federate/State/Imperial/Tribal issue Battleships => 750b
+        else if(in_array($typeId, array(13202, 26840, 11936, 11938, 26842)))
+        {
+            return "750000000000";
+        }
+        
+        
+        // default:
+        return $itemPrice;       
     }
 }
