ded5a95b244f6049c605c274afb9f3846431067d
Salvoxia <Salvoxia@blindfish.info>
2015-08-25 08:57:58 +0200

    Updated EDK version to 4.2.14.0 for Galatea 1.0 and CCP DB to 114618
    Also updated the image server constant to use CCP's new CDN for the image
    server at https://imageserver.eveonline.com

--- common/includes/constants.php
+++ common/includes/constants.php
@@ -16,15 +16,15 @@ define('KB_MAILCACHEDIR', KB_CACHEDIR . '/mails');
 define('KB_QUERYCACHEDIR', KB_CACHEDIR . '/SQL');
 define('KB_UPDATE_URL', 'http://evekb.org/downloads');
 define('API_SERVER', "https://api.eveonline.com");
-define('IMG_SERVER', "https://image.eveonline.com");
+define('IMG_SERVER', "https://imageserver.eveonline.com");
 define('CREST_PUBLIC_URL', 'https://public-crest.eveonline.com');
 
 // current version: major.minor.sub.ccpDBupdateNo
 // even numbers for minor = development version
-define('KB_VERSION', '4.2.13.0');
-define('KB_RELEASE', '(Aegis 1.1.1)');
-define('KB_CCP_DB_VERSION', '114255');
-define('KB_CCP_DB_DATE', 'Jul 10, 2015');
+define('KB_VERSION', '4.2.14.0');
+define('KB_RELEASE', '(Galatea 1.0)');
+define('KB_CCP_DB_VERSION', '114618');
+define('KB_CCP_DB_DATE', 'Aug 25, 2015');
 define('ID_FEED_VERSION', 1.30);
 define('ZKB_FETCH_VERSION', 1.0);
 define('KB_APIKEY_LEGACY', 1);
