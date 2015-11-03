bcb963da5dc389bf0a7ea2150b8e772d3589d992
Salvoxia <Salvoxia@blindfish.info>
2015-11-03 21:43:31 +0100

    Updated EDK to 4.2.16.0 and CCP DB to Parallax 1.0.115480

--- common/includes/constants.php
+++ common/includes/constants.php
@@ -21,10 +21,10 @@ define('CREST_PUBLIC_URL', 'https://public-crest.eveonline.com');
 
 // current version: major.minor.sub.ccpDBupdateNo
 // even numbers for minor = development version
-define('KB_VERSION', '4.2.15.0');
-define('KB_RELEASE', '(Vanguard 1.0)');
-define('KB_CCP_DB_VERSION', '114986');
-define('KB_CCP_DB_DATE', 'Nov 3, 2015');
+define('KB_VERSION', '4.2.16.0');
+define('KB_RELEASE', '(Parallax 1.0)');
+define('KB_CCP_DB_VERSION', '115480');
+define('KB_CCP_DB_DATE', 'Nov 4, 2015');
 define('ID_FEED_VERSION', 1.40);
 define('ZKB_FETCH_VERSION', 1.0);
 define('KB_APIKEY_LEGACY', 1);
