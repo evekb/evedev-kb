adb2a5694e8ec26cad6300e345ab8ff05020042b
Salvoxia <Salvoxia@blindfish.info>
2015-12-10 16:44:49 +0100

    Made update 038 more robust
    Made update 038 more robust so applying it multiple times doesn't lead to
    an error.

--- update/038/update.php
+++ update/038/update.php
@@ -19,12 +19,21 @@ function update038()
 	//Checking if this Update already done
 	if (CURRENT_DB_UPDATE < $DB_UPDATE) {
 		$qry = DBFactory::getDBQuery(true);
-
-		$sql = 'ALTER TABLE kb3_kills '
+                $sql = "SELECT *
+                        FROM information_schema.COLUMNS
+                        WHERE TABLE_SCHEMA = '".DB_NAME."'
+                            AND TABLE_NAME = 'kb3_kills'
+                            AND COLUMN_NAME = 'kll_x'";
+                $qry->execute($sql);
+                if(!$qry->getRow())
+                {
+                    $sql = 'ALTER TABLE kb3_kills '
                         . 'ADD COLUMN `kll_x` double NOT NULL DEFAULT 0,'
                         . 'ADD COLUMN `kll_y` double NOT NULL DEFAULT 0,'
                         . 'ADD COLUMN `kll_z` double NOT NULL DEFAULT 0';
-		$qry->execute($sql);
+                    $qry->execute($sql);
+                }
+		
 		
                 
                 $sql = 'CREATE TABLE IF NOT EXISTS `kb3_mapdenormalize` (
@@ -49,7 +58,7 @@ function update038()
                 $qry->execute($sql);
                 
 		config::set("DBUpdate", "$DB_UPDATE");
-		$qry->execute("INSERT INTO kb3_config (cfg_site, cfg_key, cfg_value) SELECT cfg_site, 'DBUpdate', '029' FROM kb3_config GROUP BY cfg_site ON DUPLICATE KEY UPDATE cfg_value = '$DB_UPDATE'");
+		$qry->execute("INSERT INTO kb3_config (cfg_site, cfg_key, cfg_value) SELECT cfg_site, 'DBUpdate', '$DB_UPDATE' FROM kb3_config GROUP BY cfg_site ON DUPLICATE KEY UPDATE cfg_value = '$DB_UPDATE'");
 		config::del($DB_UPDATE."updatestatus");
 
 		$smarty->assign('refresh', 1);
