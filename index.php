80af31afc6f7da9afd8089e503007d1f0f4a4d88
Salvoxia <Salvoxia@blindfish.info>
2016-01-28 18:45:17 +0100

    removed PHP version check from index file

--- index.php
+++ index.php
@@ -10,13 +10,11 @@ require_once ('common/includes/class.edkerror.php');
 
 set_error_handler(array('EDKError', 'handler'), E_ALL & ~(E_STRICT | E_NOTICE | E_USER_NOTICE) );
 @error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_USER_NOTICE));
-// Check we're using PHP5
-if(phpversion() < '5') die("PHP5 is required to run. You have PHP ".phpversion());
 
 // Set up include paths.
 if (!substr_compare(PHP_OS, 'win', 0, 3, true))
 {
-	@set_include_path(get_include_path() . PATH_SEPARATOR . '.\\common\\includes');
+	@set_include_path(get_include_path() . \PATH_SEPARATOR . '.\\common\\includes');
 }
 else
 {
