22c1de673976fbdd7bc653aac2029a112e87cf9f
Salvoxia <Salvoxia@blindfish.info>
2016-01-28 18:45:36 +0100

    API Import: Made more robust against API errors
    Added a check for success after fetching from the API before assuming a
    properly formatted response is available.

--- common/includes/api/class.killlog.php
+++ common/includes/api/class.killlog.php
@@ -36,6 +36,12 @@ class API_KillLog extends API
 		$posted = array();
 		$skipped = array();
 
+                if(!is_null($accts->getError()))
+                {
+                    $output .= $accts->getMessage();
+                    return $output;
+                }
+                
                 // get maximum number of API calls per key
                 $numberOfCallsMax = config::get('apikillmails_numberofcalls');
                 if(!$numberOfCallsMax)
