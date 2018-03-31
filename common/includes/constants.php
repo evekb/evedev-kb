4986f6918e3f83ab47b58076c696e7bb05beda95
Salvoxia <Salvoxia@blindfish.info>
2018-03-31 10:51:56 +0200

    pdated CCPDB to YC-120-3 1.0.20180323

--- common/includes/constants.php
+++ common/includes/constants.php
@@ -33,11 +33,11 @@ define('CREST_PUBLIC_URL', 'https://crest-tq.eveonline.com');
  */
 define('KB_VERSION', '4.2.37.0');
 /** release name */
-define('KB_RELEASE', '(YC-120-2 1.0)');
+define('KB_RELEASE', '(YC-120-3 1.0)');
 /** version of the SDE used to produce the current static database */
-define('KB_CCP_DB_VERSION', '20180213');
+define('KB_CCP_DB_VERSION', '20180323');
 /** release date of the SDE used to produce the current static database */
-define('KB_CCP_DB_DATE', 'Feb 13, 2018');
+define('KB_CCP_DB_DATE', 'Mar 23, 2018');
 /** the version of IDFeed used by this killboard, gets reported to clients */
 define('ID_FEED_VERSION', 1.50);
 /** the version of zKBFetch used by this killboard */
